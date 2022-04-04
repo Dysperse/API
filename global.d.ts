c3476ff3924e08ee222459b6a3e50f4e255bf051
Manu <manuthecoder@protonmail.com>
2022-04-04 11:31:16 -0700

    Update types

--- global.d.ts
+++ global.d.ts
@@ -1,5 +1,29 @@
 export {};
 
 declare global {
-  var ACCOUNT_DATA: Object;
+  var ACCOUNT_DATA: {
+    accessToken: string;
+    email: string;
+    name: string;
+    financePlan: string;
+    image: string;
+    notificationMin: number;
+    budget: number;
+    onboarding: number;
+    verifiedEmail: number;
+    purpose: string;
+    defaultPage: string;
+    studentMode: string;
+    financeToken: string;
+    familyCount: number;
+    houseName: string;
+    currency: string;
+    theme: {
+      dark: Array;
+      original: Array;
+      light: Array;
+      tint: Array;
+    };
+    preferences: { homePage: string };
+  };
 }
