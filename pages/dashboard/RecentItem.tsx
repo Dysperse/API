cdb8f4b7c84c4059fa8bb7929ceb2863cd4bdf2f
Manu <manuthecoder@protonmail.com>
2022-04-04 11:45:22 -0700

    Fix DayJS errors

--- pages/dashboard/RecentItem.tsx
+++ pages/dashboard/RecentItem.tsx
@@ -1,15 +1,10 @@
 import * as React from "react";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
-import * as dayjs from "dayjs";
+import dayjs from "dayjs";
 import ItemPopup from "../ItemPopup";
 
 export function RecentItem(props: any) {
-  const capitalizeFirstLetter = (
-    [first, ...rest]: any,
-    locale = navigator.language
-  ) => first.toLocaleUpperCase(locale) + rest.join("");
-
   return (
     <ItemPopup data={props.data}>
       <ListItemButton sx={{ py: "4px" }}>
