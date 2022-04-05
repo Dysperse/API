e646d65d6437766f4c75ebba1056cb96b512e4bd
Manu <manuthecoder@protonmail.com>
2022-04-04 17:26:37 -0700

    Enhanced skeleton screens

--- pages/trash.tsx
+++ pages/trash.tsx
@@ -20,13 +20,20 @@ function Items() {
 
   return isLoading ? (
     <>
-      {[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0].map((_) => (
-        <Skeleton
-          height={100}
-          animation="wave"
-          sx={{ mb: 2, borderRadius: 2 }}
-        />
-      ))}
+      {[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0].map((_) => {
+        let height = Math.random() * 400;
+        if (height < 100) height = 100;
+        return (
+          <Paper key={Math.random().toString()} sx={{ p: 0 }} elevation={0}>
+            <Skeleton
+              variant="rectangular"
+              height={height}
+              animation="wave"
+              sx={{ mb: 1, borderRadius: "5px" }}
+            />
+          </Paper>
+        );
+      })}
     </>
   ) : (
     <>
