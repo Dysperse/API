b25f5a99f8d48c74dcc0f4f7bb72320751bf16ec
Manu <manuthecoder@protonmail.com>
2022-04-15 00:31:25 -0700

    Add lists feature

--- layout/AddPopup.tsx
+++ layout/AddPopup.tsx
@@ -5,7 +5,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 import { grey } from "@mui/material/colors";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import Drawer from "@mui/material/Drawer";
+import SwipeableDrawer from "@mui/material/SwipeableDrawer";
 
 import List from "@mui/material/List";
 import ListItemButton from "@mui/material/ListItemButton";
@@ -17,69 +17,81 @@ import LabelIcon from "@mui/icons-material/Label";
 const drawerBleeding = 10;
 
 const Root = styled("div")(({ theme }) => ({
-  height: "100%"
+	height: "100%"
 }));
 
 function AddItemOption({ icon, title }: any): JSX.Element {
-  return (
-    <CreateItemModal room={title}>
-      <ListItemButton>
-        <ListItemIcon>{icon}</ListItemIcon>
-        <ListItemText primary={title} />
-      </ListItemButton>
-    </CreateItemModal>
-  );
+	return (
+		<CreateItemModal room={title}>
+			<ListItemButton>
+				<ListItemIcon>{icon}</ListItemIcon>
+				<ListItemText primary={title} />
+			</ListItemButton>
+		</CreateItemModal>
+	);
 }
 function Content({ toggleDrawer }: any) {
-  return (
-    <List
-      sx={{ width: "100%", bgcolor: "background.paper" }}
-      onClick={toggleDrawer()}
-    >
-      <AddItemOption title="Kitchen" icon={<LabelIcon />} />
-      <AddItemOption title="Bathroom" icon={<LabelIcon />} />
-      <AddItemOption title="Bedroom" icon={<LabelIcon />} />
-      <AddItemOption title="Garage" icon={<LabelIcon />} />
-      <AddItemOption title="Living room" icon={<LabelIcon />} />
-      <AddItemOption title="Dining room" icon={<LabelIcon />} />
-      <AddItemOption title="Laundry room" icon={<LabelIcon />} />
-      <AddItemOption title="Storage room" icon={<LabelIcon />} />
-    </List>
-  );
+	return (
+		<List
+			sx={{ width: "100%", bgcolor: "background.paper" }}
+			onClick={toggleDrawer()}
+		>
+			<AddItemOption title="Kitchen" icon={<LabelIcon />} />
+			<AddItemOption title="Bathroom" icon={<LabelIcon />} />
+			<AddItemOption title="Bedroom" icon={<LabelIcon />} />
+			<AddItemOption title="Garage" icon={<LabelIcon />} />
+			<AddItemOption title="Living room" icon={<LabelIcon />} />
+			<AddItemOption title="Dining room" icon={<LabelIcon />} />
+			<AddItemOption title="Laundry room" icon={<LabelIcon />} />
+			<AddItemOption title="Storage room" icon={<LabelIcon />} />
+		</List>
+	);
 }
 
 export default function AddPopup(props: any) {
-  const { window } = props;
-  const [open, setOpen] = React.useState(false);
+	const { window } = props;
+	const [open, setOpen] = React.useState(false);
 
-  const toggleDrawer = (newOpen: boolean) => () => {
-    setOpen(newOpen);
-  };
+	const toggleDrawer = (newOpen: boolean) => () => {
+		setOpen(newOpen);
+	};
 
-  return (
-    <Root>
-      <CssBaseline />
-      <Global
-        styles={{
-          ".MuiDrawer-root > .MuiPaper-root": {
-            height: "auto",
-            overflow: "visible"
-          }
-        }}
-      />
-      <Box onClick={toggleDrawer(true)}>{props.children}</Box>
+	return (
+		<Root>
+			<CssBaseline />
+			<Global
+				styles={{
+					".MuiDrawer-root > .MuiPaper-root": {
+						height: "auto",
+						overflow: "visible"
+					}
+				}}
+			/>
+			<Box onClick={toggleDrawer(true)}>{props.children}</Box>
 
-      <Drawer
-        anchor="bottom"
-        open={open}
-        onClose={toggleDrawer(false)}
-        ModalProps={{
-          keepMounted: true
-        }}
-      >
-        <Typography sx={{ p: 2, color: "text.secondary" }}>Create</Typography>
-        <Content toggleDrawer={toggleDrawer} />
-      </Drawer>
-    </Root>
-  );
+			<SwipeableDrawer
+				anchor="bottom"
+				PaperProps={{
+					sx: {
+						width: {
+							sm: "50vw"
+						},
+						borderRadius: "40px 40px 0 0",
+						mx: "auto"
+					}
+				}}
+				open={open}
+				onClose={toggleDrawer(false)}
+				onOpen={toggleDrawer(true)}
+				ModalProps={{
+					keepMounted: true
+				}}
+			>
+				<Typography sx={{ textAlign: "center", p: 2, color: "text.secondary" }}>
+					Create
+				</Typography>
+				<Content toggleDrawer={toggleDrawer} />
+			</SwipeableDrawer>
+		</Root>
+	);
 }
--- layout/dashboard/List.tsx
+++ layout/dashboard/List.tsx
@@ -1,30 +1,166 @@
-import * as React from "react";
+import React, { useState } from "react";
 import Typography from "@mui/material/Typography";
 import Card from "@mui/material/Card";
 import CardActionArea from "@mui/material/CardActionArea";
 import CardContent from "@mui/material/CardContent";
+import SwipeableDrawer from "@mui/material/SwipeableDrawer";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import { Suspense } from "react";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Skeleton from "@mui/material/Skeleton";
+import Collapse from "@mui/material/Collapse";
+
+function ListItem({ item }: any) {
+	const [deleted, setDeleted] = useState(false);
+
+	return (
+		<Collapse in={!deleted}>
+			<FormControlLabel
+				control={
+					<Checkbox
+						disabled={deleted}
+						onInput={() => {
+							setDeleted(true);
+						}}
+					/>
+				}
+				label={item.title}
+				sx={{ m: 0, display: "block" }}
+			/>
+		</Collapse>
+	);
+}
+
+function ListPopup({ listItems, title, id, drawerState, setDrawerState }: any) {
+	return (
+		<SwipeableDrawer
+			anchor="bottom"
+			swipeAreaWidth={0}
+			disableSwipeToOpen={true}
+			PaperProps={{
+				sx: {
+					width: {
+						sm: "50vw"
+					},
+					maxHeight: "80vh",
+					borderRadius: "40px 40px 0 0",
+					mx: "auto"
+				}
+			}}
+			open={drawerState}
+			onClose={() => setDrawerState(false)}
+			onOpen={() => setDrawerState(false)}
+		>
+			<div
+				style={{
+					textAlign: "center",
+					borderBottom: "1px solid #eee"
+				}}
+			>
+				<Typography
+					gutterBottom
+					variant="h5"
+					component="div"
+					sx={{ mt: 4, mb: 2 }}
+				>
+					{title}
+				</Typography>
+				<Button size="small" sx={{ mr: 1, mb: 3 }} variant="contained">
+					Create item
+				</Button>
+				<Button size="small" sx={{ mb: 3 }} variant="outlined">
+					Delete list
+				</Button>
+			</div>
+			<Box sx={{ p: 3, textAlign: "center", overflow: "scroll" }}>
+				{listItems.loading ? (
+					<>
+						<Skeleton animation="wave" />
+						<Skeleton animation="wave" />
+						<Skeleton animation="wave" />
+						<Skeleton animation="wave" />
+						<Skeleton animation="wave" />
+						<Skeleton animation="wave" />
+					</>
+				) : (
+					<div style={{ textAlign: "left", display: "block" }}>
+						{listItems.data.data.length === 0 ? (
+							<Box sx={{ textAlign: "center", opacity: ".5" }}>
+								<Typography variant="h3" sx={{ mb: 2 }}>
+									¯\_(ツ)_/¯
+								</Typography>
+								<Typography variant="h5">No items yet...</Typography>
+							</Box>
+						) : null}
+						{listItems.data.data.map((item) => (
+							<ListItem item={item} />
+						))}
+					</div>
+				)}
+			</Box>
+		</SwipeableDrawer>
+	);
+}
 
 export function List({
-  title,
-  description,
-  id
+	title,
+	description,
+	id
 }: {
-  title: string;
-  description: string;
-  id: number;
+	title: string;
+	description: string;
+	id: number;
 }) {
-  return (
-    <Card sx={{ mb: 1, width: "100%" }}>
-      <CardActionArea>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {title}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {description}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-  );
+	const [drawerState, setDrawerState] = React.useState(false);
+	const [listItems, setlistItems] = useState({
+		data: "",
+		loading: true
+	});
+
+	const getListItems = async (id: number) => {
+		const data = await fetch("https://api.smartlist.tech/v2/lists/fetch/", {
+			method: "POST",
+			body: new URLSearchParams({
+				token: ACCOUNT_DATA.accessToken,
+				parent: id.toString()
+			})
+		});
+		const e = await data.json();
+
+		setlistItems({
+			data: e,
+			loading: false
+		});
+	};
+	return (
+		<>
+			<ListPopup
+				title={title}
+				id={id}
+				listItems={listItems}
+				drawerState={drawerState}
+				setDrawerState={setDrawerState}
+			/>
+			<Card sx={{ mb: 1, width: "100%" }}>
+				<CardActionArea
+					onClick={() => {
+						setDrawerState(true);
+						getListItems(id);
+					}}
+				>
+					<CardContent>
+						<Typography gutterBottom variant="h5" component="div">
+							{title}
+						</Typography>
+						<Typography variant="body2" color="text.secondary">
+							{description}
+						</Typography>
+					</CardContent>
+				</CardActionArea>
+			</Card>
+		</>
+	);
 }
