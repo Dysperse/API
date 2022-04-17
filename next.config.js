d36555f671470153ef296c7e9eb48d4336b2acfd
Manu <manuthecoder@protonmail.com>
2022-04-16 18:38:37 -0700

    Update `~/next.config.js`

--- next.config.js
+++ next.config.js
@@ -2,17 +2,17 @@ const withPlugins = require("next-compose-plugins");
 const withPWA = require("next-pwa");
 
 module.exports = withPlugins(
-  [
-    [
-      withPWA({
-        pwa: {
-          mode: "production",
-          dest: "public"
-        }
-      })
-    ]
-  ],
-  {
-    reactStrictMode: true
-  }
+	[
+		[
+			withPWA({
+				pwa: {
+					// mode: "production",
+					dest: "public"
+				}
+			})
+		]
+	],
+	{
+		reactStrictMode: true
+	}
 );
