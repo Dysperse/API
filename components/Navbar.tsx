ff8d0b0d8ce1c699248bdb137dbc54c203ab9e11
Manu <manuthecoder@protonmail.com>
2022-04-23 12:25:22 -0700

    Fix skeleton screen

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -15,134 +15,134 @@ import { blue, blueGrey } from "@mui/material/colors";
 import useScrollTrigger from "@mui/material/useScrollTrigger";
 
 function ElevationScroll(props: any) {
-	const { children, window } = props;
-	// Note that you normally won't need to set the window ref as useScrollTrigger
-	// will default to window.
-	// This is only being set here because the demo is in an iframe.
-	const trigger = useScrollTrigger({
-		disableHysteresis: true,
-		threshold: 0,
-		target: window ? window() : undefined
-	});
-	useEffect(() => {
-		if (document && document.querySelector(`meta[name="theme-color"]`)) {
-			document
-				.querySelector(`meta[name="theme-color"]`)!
-				.setAttribute("content", trigger ? blue[100] : "#fff");
-		}
-	});
-	return React.cloneElement(children, {
-		sx: trigger
-			? {
-					color: "black",
-					py: {
-						sm: 1,
-						xs: 0.5
-					},
-					transition: "all .2s",
-					background: "rgba(187, 222, 251, .7)",
-					backdropFilter: "blur(20px)"
-			  }
-			: {
-					color: "#000",
-					py: {
-						sm: 1,
-						xs: 0.5
-					},
-					transition: "all .2s",
-					background: "rgba(255,255,255,.5)",
-					backdropFilter: "blur(10px)"
-			  }
-	});
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+    target: window ? window() : undefined
+  });
+  useEffect(() => {
+    if (document && document.querySelector(`meta[name="theme-color"]`)) {
+      document
+        .querySelector(`meta[name="theme-color"]`)!
+        .setAttribute("content", trigger ? blue[100] : "#fff");
+    }
+  });
+  return React.cloneElement(children, {
+    sx: trigger
+      ? {
+          color: "black",
+          py: {
+            sm: 1,
+            xs: 0.5
+          },
+          transition: "all .2s",
+          background: "rgba(187, 222, 251, .7)",
+          backdropFilter: "blur(20px)"
+        }
+      : {
+          color: "#000",
+          py: {
+            sm: 1,
+            xs: 0.5
+          },
+          transition: "all .2s",
+          background: "rgba(255,255,255,.5)",
+          backdropFilter: "blur(10px)"
+        }
+  });
 }
 
 export function Navbar({ handleDrawerToggle }: any): JSX.Element {
-	return (
-		<ElevationScroll>
-			<AppBar elevation={0} position="fixed">
-				<Toolbar>
-					<Tooltip title="Menu" placement="bottom-start">
-						{global.session ? (
-							<IconButton
-								color="inherit"
-								aria-label="open drawer."
-								edge="start"
-								size="large"
-								onClick={handleDrawerToggle}
-								sx={{ mr: 2, ml: -0.5, display: { sm: "none" } }}
-							>
-								<span className="material-symbols-rounded">menu</span>
-							</IconButton>
-						) : (
-							<Skeleton
-								sx={{ mr: 2 }}
-								variant="circular"
-								width={40}
-								height={40}
-								animation="wave"
-							/>
-						)}
-					</Tooltip>
+  return (
+    <ElevationScroll>
+      <AppBar elevation={0} position="fixed">
+        <Toolbar>
+          <Tooltip title="Menu" placement="bottom-start">
+            {global.session ? (
+              <IconButton
+                color="inherit"
+                aria-label="open drawer."
+                edge="start"
+                size="large"
+                onClick={handleDrawerToggle}
+                sx={{ mr: 2, ml: -0.5, display: { sm: "none" } }}
+              >
+                <span className="material-symbols-rounded">menu</span>
+              </IconButton>
+            ) : (
+              <Skeleton
+                sx={{ mr: 2 }}
+                variant="circular"
+                width={40}
+                height={40}
+                animation="wave"
+              />
+            )}
+          </Tooltip>
 
-					<Typography variant="h6" sx={{ flexGrow: 1 }} noWrap>
-						{global.session ? (
-							global.session.user.houseName
-						) : (
-							<Skeleton width={200} />
-						)}
-					</Typography>
-					<NotificationsMenu>
-						<Tooltip title="Notifications">
-							{global.session ? (
-								<IconButton
-									color="inherit"
-									edge="end"
-									size="large"
-									sx={{ mr: 0.8 }}
-								>
-									<span className="material-symbols-rounded">
-										notifications
-									</span>
-								</IconButton>
-							) : (
-								<Skeleton
-									variant="circular"
-									width={40}
-									sx={{ mr: 2 }}
-									height={40}
-									animation="wave"
-								/>
-							)}
-						</Tooltip>
-					</NotificationsMenu>
-					<Box sx={{ display: { sm: "block", xs: "none" } }}>
-						<Tooltip title="Search">
-							{global.session ? (
-								<IconButton
-									color="inherit"
-									edge="end"
-									size="large"
-									sx={{ mr: 0.8 }}
-								>
-									<span className="material-symbols-rounded">search</span>
-								</IconButton>
-							) : (
-								<Skeleton
-									sx={{ mr: 2 }}
-									variant="circular"
-									width={40}
-									height={40}
-									animation="wave"
-								/>
-							)}
-						</Tooltip>
-					</Box>
-					<Box sx={{ display: { sm: "block", xs: "none" } }}>
-						<AppsMenu />
-					</Box>
-					<ProfileMenu />
-				</Toolbar>
-			</AppBar>
-		</ElevationScroll>
-	);
+          <Typography variant="h6" sx={{ flexGrow: 1 }} noWrap>
+            {global.session ? (
+              global.session.user.houseName
+            ) : (
+              <Skeleton width={200} sx={{ maxWidth: "20vw" }} />
+            )}
+          </Typography>
+          <NotificationsMenu>
+            <Tooltip title="Notifications">
+              {global.session ? (
+                <IconButton
+                  color="inherit"
+                  edge="end"
+                  size="large"
+                  sx={{ mr: 0.8 }}
+                >
+                  <span className="material-symbols-rounded">
+                    notifications
+                  </span>
+                </IconButton>
+              ) : (
+                <Skeleton
+                  variant="circular"
+                  width={40}
+                  sx={{ mr: 2 }}
+                  height={40}
+                  animation="wave"
+                />
+              )}
+            </Tooltip>
+          </NotificationsMenu>
+          <Box sx={{ display: { sm: "block", xs: "none" } }}>
+            <Tooltip title="Search">
+              {global.session ? (
+                <IconButton
+                  color="inherit"
+                  edge="end"
+                  size="large"
+                  sx={{ mr: 0.8 }}
+                >
+                  <span className="material-symbols-rounded">search</span>
+                </IconButton>
+              ) : (
+                <Skeleton
+                  sx={{ mr: 2 }}
+                  variant="circular"
+                  width={40}
+                  height={40}
+                  animation="wave"
+                />
+              )}
+            </Tooltip>
+          </Box>
+          <Box sx={{ display: { sm: "block", xs: "none" } }}>
+            <AppsMenu />
+          </Box>
+          <ProfileMenu />
+        </Toolbar>
+      </AppBar>
+    </ElevationScroll>
+  );
 }
