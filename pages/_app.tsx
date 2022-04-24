c159650cf0e257b782cffb6e1ba5190cdc959422
Manu <manuthecoder@protonmail.com>
2022-04-23 17:46:48 -0700

    Add icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -50,6 +50,51 @@ function SmartlistApp({ user, Component, pageProps }: any): JSX.Element {
 	return (
 		<>
 			<Head>
+				<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+				<link
+					rel="apple-touch-icon"
+					href="https://smartlist.tech/app/img/logo/apple-touch-icon//apple-touch-icon.png"
+				/>
+				<link
+					rel="apple-touch-icon"
+					sizes="57x57"
+					href="https://smartlist.tech/app/img/logo/apple-touch-icon//apple-touch-icon-57x57.png"
+				/>
+				<link
+					rel="apple-touch-icon"
+					sizes="72x72"
+					href="https://smartlist.tech/app/img/logo/apple-touch-icon//apple-touch-icon-72x72.png"
+				/>
+				<link
+					rel="apple-touch-icon"
+					sizes="76x76"
+					href="https://smartlist.tech/app/img/logo/apple-touch-icon//apple-touch-icon-76x76.png"
+				/>
+				<link
+					rel="apple-touch-icon"
+					sizes="114x114"
+					href="https://smartlist.tech/app/img/logo/apple-touch-icon//apple-touch-icon-114x114.png"
+				/>
+				<link
+					rel="apple-touch-icon"
+					sizes="120x120"
+					href="https://smartlist.tech/app/img/logo/apple-touch-icon//apple-touch-icon-120x120.png"
+				/>
+				<link
+					rel="apple-touch-icon"
+					sizes="144x144"
+					href="https://smartlist.tech/app/img/logo/apple-touch-icon//apple-touch-icon-144x144.png"
+				/>
+				<link
+					rel="apple-touch-icon"
+					sizes="152x152"
+					href="https://smartlist.tech/app/img/logo/apple-touch-icon//apple-touch-icon-152x152.png"
+				/>
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="https://smartlist.tech/app/img/logo/apple-touch-icon//apple-touch-icon-180x180.png"
+				/>
 				<meta name="apple-mobile-web-app-capable" content="yes" />
 				<meta name="viewport" content="width=device-width,initial-scale=1" />
 				<meta name="theme-color" content={blue[50]} />
