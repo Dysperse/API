17ed94fd49293f9c621a16a6d38b627f6ae7ce3f
Manu <manuthecoder@protonmail.com>
2022-05-01 21:25:50 -0700

    Add meals stuff

--- pages/meals.tsx
+++ pages/meals.tsx
@@ -1,9 +1,10 @@
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 export default function Meals() {
-	return (
-		<Box sx={{ p: 3 }}>
-			<Typography variant="h5">Meals</Typography>
-		</Box>
-	);
+  return (
+    <Box sx={{ p: 3 }}>
+      <Typography variant="h5">Meals</Typography>
+      <Typography variant="h6">Coming soon!</Typography>
+    </Box>
+  );
 }
