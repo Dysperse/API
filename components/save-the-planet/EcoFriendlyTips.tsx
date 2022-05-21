266c0d3b4b4a9373a964f91081c48aeb22e4481d
Manu <manuthecoder@protonmail.com>
2022-05-20 21:46:39 -0700

    Ad modal for eco friendly tips

--- components/save-the-planet/EcoFriendlyTips.tsx
+++ components/save-the-planet/EcoFriendlyTips.tsx
@@ -49,63 +49,75 @@ export function EcoFriendlyTips() {
         }}
       >
         <TipCard
+          modalContent={<>Test</>}
           tipOfTheDay
           name="Bike more often!"
           funFact="Did you know you can save 69 tons of carbon just by riding your bike to work?"
           icon="directions_bike"
         />
         <TipCard
+          modalContent={<>Test</>}
           highlySuggested
           name="Use less water"
           funFact="Did you know you can save 49 gallons of water just by reducing the amount of water in your laundry machine?"
           icon="local_laundry_service"
         />
         <TipCard
+          modalContent={<>Test</>}
           name="Save more electricity"
           funFact="Running your appliances outside the hours 4AM to 9PM helps conserve energy - and reduce your energy bill?"
           icon="electric_meter"
         />
         <TipCard
+          modalContent={<>Test</>}
           name="Use less water"
           funFact="Did you know you can save up to 69420 gallons of water by limiting your sprinklers to 1 time a day?"
           icon="sprinkler"
         />
         <TipCard
+          modalContent={<>Test</>}
           name="Buy an eco-friendly thermostat"
           funFact="Buying an eco friendly thermostat helps you save the environment, and some electricity providers discount your bills if you have one!"
           icon="energy_program_saving"
         />
         <TipCard
+          modalContent={<>Test</>}
           name="Add attic insulation"
           funFact="Did you know that insulating your attic can reduce your electricity bill by 10-50%?!"
           icon="airware"
         />
         <TipCard
+          modalContent={<>Test</>}
           name="Upgrade to an electric car"
           funFact="Upgrading to an electric car can significantly improve the environment, plus, it's worth the purchase"
           icon="electric_car"
         />
         <TipCard
+          modalContent={<>Test</>}
           name="Switch to LEDs"
           funFact="LEDs are much safer for the environment, and could reduce your electricity bill by $215"
           icon="light"
         />
         <TipCard
+          modalContent={<>Test</>}
           name="Upgrade to an electric fireplace"
           funFact="An electric furnace is much more safe for the environment, compared to a gas fireplace"
           icon="fireplace"
         />
         <TipCard
+          modalContent={<>Test</>}
           name="Unplug unused devices, especially while traveling"
           funFact="This will help you greatly reduce your electricity bill"
           icon="outlet"
         />
         <TipCard
+          modalContent={<>Test</>}
           name="Don't set your heater above 68°"
           funFact="Lowering your thermostat 10-15 degrees for eight hours can reduce your heating bill by 5-15%."
           icon="mode_heat"
         />
         <TipCard
+          modalContent={<>Test</>}
           name="Don't set your AC below 78°"
           funFact="78 degrees is the sweet spot between staying cool and keeping your energy bill low"
           icon="mode_cool"
--- components/save-the-planet/TipCard.tsx
+++ components/save-the-planet/TipCard.tsx
@@ -1,138 +1,190 @@
+import { useState } from "react";
+import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Tab from "@mui/material/Tab";
 import { green, orange } from "@mui/material/colors";
+import SwipeableDrawer from "@mui/material/SwipeableDrawer";
+
 export function TipCard({
   name,
+  modalContent,
   funFact,
   tipOfTheDay = false,
   highlySuggested = false,
   icon = "lightbulb"
 }: any) {
+  const [open, setOpen] = useState(false);
   return (
-    <Tab
-      icon={
-        <>
-          {tipOfTheDay && (
-            <div
-              className="badge"
-              style={{
-                fontSize: "13px",
-                display: "inline-flex",
-                alignItems: "center",
-                position: "absolute",
-                top: "15px",
-                right: "15px",
-                justifyContent: "center",
-                gap: "10px",
-                background: green["A700"],
-                color: "white",
-                borderRadius: "99px",
-                fontWeight: "500",
-                padding: "3px 10px"
-              }}
-            >
-              <span
-                className="material-symbols-rounded"
-                style={{ fontSize: "14px", color: "white" }}
-              >
-                tips_and_updates
-              </span>{" "}
-              Tip of the day
-            </div>
-          )}
-          {highlySuggested && (
-            <div
-              className="badge"
-              style={{
-                fontSize: "13px",
-                display: "inline-flex",
-                alignItems: "center",
-                position: "absolute",
-                top: "15px",
-                right: "15px",
-                justifyContent: "center",
-                gap: "10px",
-                background: orange["A700"],
-                color: "white",
-                borderRadius: "99px",
-                fontWeight: "500",
-                padding: "3px 10px"
-              }}
-            >
-              <span
-                className="material-symbols-rounded"
-                style={{ fontSize: "14px", color: "white" }}
-              >
-                auto_awesome
-              </span>{" "}
-              Highly suggested
-            </div>
-          )}
-          <Typography sx={{ float: "left", fontWeight: "800" }} variant="h6">
-            <span
-              className="material-symbols-rounded"
-              style={{
-                fontSize: "28px",
-                display: "block",
-                marginTop: "10px",
-                marginBottom: "10px"
-              }}
-            >
-              {icon}
-            </span>
-            {name}
-          </Typography>
-        </>
-      }
-      label={
-        <Typography
-          variant="body2"
+    <>
+      <SwipeableDrawer
+        anchor="bottom"
+        swipeAreaWidth={0}
+        onOpen={() => setOpen(true)}
+        open={open}
+        sx={{
+          display: "flex",
+          alignItems: { xs: "end", sm: "center" },
+          height: "100vh",
+          justifyContent: "center"
+        }}
+        PaperProps={{
+          sx: {
+            borderRadius: "28px",
+            borderBottomLeftRadius: { xs: 0, sm: "28px!important" },
+            borderBottomRightRadius: { xs: 0, sm: "28px!important" },
+            position: "unset",
+            mx: "auto",
+            maxWidth: { sm: "70vw", xs: "100vw" },
+            overflow: "hidden"
+          }
+        }}
+        onClose={() => setOpen(false)}
+      >
+        <Box
           sx={{
-            fontSize: "12px",
-            fontWeight: "600"
+            p: 3,
+            py: 6,
+            px: 7,
+            borderRadius: 5,
+            mt: "-1px",
+            borderBottomLeftRadius: 0,
+            borderBottomRightRadius: 0,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat"
           }}
         >
-          {funFact}
-        </Typography>
-      }
-      disableRipple
-      sx={{
-        mr: 1,
-        px: 3,
-        py: 1,
-        pb: 2,
-        textAlign: "left!important",
-        alignItems: "start",
-        width: "90vw",
-        opacity: 1,
-        "& *": {
+          <Typography variant="h4" gutterBottom sx={{ fontWeight: "600" }}>
+            {name}
+          </Typography>
+          <Typography variant="h6">{funFact}</Typography>
+        </Box>
+      </SwipeableDrawer>
+      <Tab
+        onClick={() => setOpen(true)}
+        icon={
+          <>
+            {tipOfTheDay && (
+              <div
+                className="badge"
+                style={{
+                  fontSize: "13px",
+                  display: "inline-flex",
+                  alignItems: "center",
+                  position: "absolute",
+                  top: "15px",
+                  right: "15px",
+                  justifyContent: "center",
+                  gap: "10px",
+                  background: green["A700"],
+                  color: "white",
+                  borderRadius: "99px",
+                  fontWeight: "500",
+                  padding: "3px 10px"
+                }}
+              >
+                <span
+                  className="material-symbols-rounded"
+                  style={{ fontSize: "14px", color: "white" }}
+                >
+                  tips_and_updates
+                </span>{" "}
+                Tip of the day
+              </div>
+            )}
+            {highlySuggested && (
+              <div
+                className="badge"
+                style={{
+                  fontSize: "13px",
+                  display: "inline-flex",
+                  alignItems: "center",
+                  position: "absolute",
+                  top: "15px",
+                  right: "15px",
+                  justifyContent: "center",
+                  gap: "10px",
+                  background: orange["A700"],
+                  color: "white",
+                  borderRadius: "99px",
+                  fontWeight: "500",
+                  padding: "3px 10px"
+                }}
+              >
+                <span
+                  className="material-symbols-rounded"
+                  style={{ fontSize: "14px", color: "white" }}
+                >
+                  auto_awesome
+                </span>{" "}
+                Highly suggested
+              </div>
+            )}
+            <Typography sx={{ float: "left", fontWeight: "800" }} variant="h6">
+              <span
+                className="material-symbols-rounded"
+                style={{
+                  fontSize: "28px",
+                  display: "block",
+                  marginTop: "10px",
+                  marginBottom: "10px"
+                }}
+              >
+                {icon}
+              </span>
+              {name}
+            </Typography>
+          </>
+        }
+        label={
+          <Typography
+            variant="body2"
+            sx={{
+              fontSize: "12px",
+              fontWeight: "600"
+            }}
+          >
+            {funFact}
+          </Typography>
+        }
+        disableRipple
+        sx={{
+          mr: 1,
+          px: 3,
+          py: 1,
+          pb: 2,
+          textAlign: "left!important",
+          alignItems: "start",
+          width: "90vw",
           opacity: 1,
-          color: "#101010"
-        },
-        background: tipOfTheDay
-          ? green["A100"]
-          : highlySuggested
-          ? orange["A100"]
-          : "rgba(200,200,200,.3)",
-        transition: "color .2s",
-        "&:active": {
-          opacity: 1,
-          "& .badge": {
-            filter: "brightness(90%)"
-          },
-          background: tipOfTheDay
-            ? green["A200"]
-            : highlySuggested
-            ? orange["A200"]
-            : "rgba(200,200,200,.5)",
           "& *": {
             opacity: 1,
-            color: "#000"
-          }
-        },
-        textTransform: "none",
-        borderRadius: 5
-      }}
-    />
+            color: "#101010"
+          },
+          background: tipOfTheDay
+            ? green["A100"]
+            : highlySuggested
+            ? orange["A100"]
+            : "rgba(200,200,200,.3)",
+          transition: "color .2s",
+          "&:active": {
+            opacity: 1,
+            "& .badge": {
+              filter: "brightness(90%)"
+            },
+            background: tipOfTheDay
+              ? green["A200"]
+              : highlySuggested
+              ? orange["A200"]
+              : "rgba(200,200,200,.5)",
+            "& *": {
+              opacity: 1,
+              color: "#000"
+            }
+          },
+          textTransform: "none",
+          borderRadius: 5
+        }}
+      />
+    </>
   );
 }
