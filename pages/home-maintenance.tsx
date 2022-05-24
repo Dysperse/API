aec7e888f5c0f67af8591a139989a148371afb89
Manu <manuthecoder@protonmail.com>
2022-05-24 05:56:42 +0000

    Add coming soon for home maintenance

--- pages/home-maintenance.tsx
+++ pages/home-maintenance.tsx
@@ -1,13 +1,19 @@
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
 
-export default function Meals() {
+export default function Render() {
   return (
-    <Box sx={{ p: 3 }}>
-      <Typography variant="h5" gutterBottom>
-        Home maintenance
-      </Typography>
-      <Typography variant="body2">Coming soon!</Typography>
+    <Box
+      sx={{
+        p: 3,
+        mt: 5,
+        mx: "auto",
+        maxWidth: "calc(100vw - 50px)",
+        borderRadius: 5,
+        width: "500px",
+        background: "rgba(200,200,200,.3)",
+      }}
+    >
+      Coming soon!
     </Box>
   );
 }
