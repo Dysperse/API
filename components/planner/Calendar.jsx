620eea89ec11a2b1c6e09e8b4bfaed7858aad588
Manu <manuthecoder@protonmail.com>
2022-06-03 19:00:36 +0000

    Make calendar daygrid instead of timegrid

--- components/planner/Calendar.jsx
+++ components/planner/Calendar.jsx
@@ -1,17 +1,22 @@
 import FullCalendar from "@fullcalendar/react";
 import interactionPlugin from "@fullcalendar/interaction";
-import timeGridPlugin from "@fullcalendar/timegrid";
+import dayGridPlugin from "@fullcalendar/daygrid";
 import { useRef } from "react";
+import Box from "@mui/material/Box";
 
 const Calendar = () => {
   const calendarRef = useRef(null);
   return (
+    <Box sx={{ py: 5 }}>
       <FullCalendar
         innerRef={calendarRef}
-        plugins={[timeGridPlugin, interactionPlugin]}
+        plugins={[dayGridPlugin, interactionPlugin]}
         editable
+        initialView="dayGridWeek"
         selectable
+        height="500px"
       />
+    </Box>
   );
 };
 
--- components/planner/CreatePlanner.tsx
+++ components/planner/CreatePlanner.tsx
@@ -13,22 +13,41 @@ import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 import * as colors from "@mui/material/colors";
+import { useFormik } from "formik";
+import LoadingButton from "@mui/lab/LoadingButton";
+import toast from "react-hot-toast";
 
 export function CreatePlanner() {
   const [open, setOpen] = useState(false);
-  const [value, setValue] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setValue((event.target as HTMLInputElement).value);
-  };
-  const [mealType, setMealType] = useState("breakfast");
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      type: "meal",
+      field1: "breakfast",
+    },
+    onSubmit: async (values: {
+      title: string;
+      type: string;
+      field1: string;
+    }) => {
+      alert(JSON.stringify(values));
+      // fetch("https://api.smartlist.tech/v2/items/create/", {
+      //   method: "POST",
+      //   body: new URLSearchParams({
+      //     token: session && session.accessToken,
+      //   }),
+      // })
+      //   .then((res) => res.json())
+      //   .then((res) => {
+      //     toast("Created item!");
+      //     setLoading(false);
+      //     formik.resetForm();
+      //   });
+    },
+  });
 
-  const setMealTypeEvent = (event: SelectChangeEvent) => {
-    setMealType(event.target.value as string);
-  };
-  useEffect(() => {
-    if (open) setTimeout(() => setValue("meal"), 1000);
-  }, [open]);
   return (
     <>
       <SwipeableDrawer
@@ -56,113 +75,123 @@ export function CreatePlanner() {
         }}
       >
         <Box sx={{ py: 6, px: 7 }}>
-          <Typography variant="h4" gutterBottom sx={{ fontWeight: "600" }}>
-            Create
-          </Typography>
+          <form onSubmit={formik.handleSubmit}>
+            <Typography variant="h4" gutterBottom sx={{ fontWeight: "600" }}>
+              Create
+            </Typography>
 
-          <FormControl sx={{ my: 2 }}>
-            <FormLabel id="demo-controlled-radio-buttons-group">
-              What are you planning?
-            </FormLabel>
-            <RadioGroup
-              aria-labelledby="demo-controlled-radio-buttons-group"
-              name="controlled-radio-buttons-group"
-              value={value}
-              onChange={handleChange}
-            >
-              <FormControlLabel
-                value="meal"
-                control={<Radio />}
-                label="a meal"
-              />
-              <FormControlLabel
-                value="workout"
-                control={<Radio />}
-                label="a workout"
-              />
-              <FormControlLabel
-                value="chores"
-                control={<Radio />}
-                label="chores"
-              />
-              <FormControlLabel
-                value="something else"
-                control={<Radio />}
-                label="something else"
-              />
-            </RadioGroup>
-          </FormControl>
-          {value === "meal" ? (
-            <>
-              <TextField
-                autoFocus
-                label="What are you cooking?"
-                variant="filled"
-                fullWidth
-              />
-              <FormControl fullWidth sx={{ mt: 2 }}>
-                <InputLabel variant="filled" id="demo-simple-select-label">
-                  Type
-                </InputLabel>
-                <Select
-                  MenuProps={{
-                    BackdropProps: {
-                      sx: { opacity: "0!important" },
-                    },
-                    anchorOrigin: {
-                      vertical: "top",
-                      horizontal: "center",
-                    },
-                    transformOrigin: {
-                      vertical: "center",
-                      horizontal: "center",
-                    },
-                    PaperProps: {
-                      sx: {
-                        px: 1,
-                        borderRadius: 3,
-                        background:
-                          colors[themeColor][
-                            global.theme === "dark" ? 900 : 100
-                          ],
+            <FormControl sx={{ my: 2 }}>
+              <FormLabel id="demo-controlled-radio-buttons-group">
+                What are you planning?
+              </FormLabel>
+              <RadioGroup
+                aria-labelledby="demo-controlled-radio-buttons-group"
+                name="controlled-radio-buttons-group"
+                value={formik.values.type}
+                onChange={(_e, v) => formik.setFieldValue("type", v)}
+              >
+                <FormControlLabel
+                  value="meal"
+                  control={<Radio />}
+                  label="a meal"
+                />
+                <FormControlLabel
+                  value="workout"
+                  control={<Radio />}
+                  label="a workout"
+                />
+                <FormControlLabel
+                  value="chores"
+                  control={<Radio />}
+                  label="chores"
+                />
+                <FormControlLabel
+                  value="something else"
+                  control={<Radio />}
+                  label="something else"
+                />
+              </RadioGroup>
+            </FormControl>
+            {formik.values.type === "meal" ? (
+              <>
+                <TextField
+                  autoFocus
+                  value={formik.values.title}
+                  onChange={(e) =>
+                    formik.setFieldValue("title", e.target.value)
+                  }
+                  label="What are you cooking?"
+                  variant="filled"
+                  fullWidth
+                />
+                <FormControl fullWidth sx={{ mt: 2 }}>
+                  <InputLabel variant="filled" id="demo-simple-select-label">
+                    Type
+                  </InputLabel>
+                  <Select
+                    MenuProps={{
+                      BackdropProps: {
+                        sx: { opacity: "0!important" },
                       },
-                    },
-                    sx: {
-                      "& .MuiMenuItem-root ": {
-                        borderRadius: 3,
-                        px: 3,
-                        mb: 0.1,
-                        py: 1.5,
-                        "&:hover": {
-                          backgroundColor: "rgba(200,200,200,.1)",
+                      anchorOrigin: {
+                        vertical: "top",
+                        horizontal: "center",
+                      },
+                      transformOrigin: {
+                        vertical: "center",
+                        horizontal: "center",
+                      },
+                      PaperProps: {
+                        sx: {
+                          px: 1,
+                          borderRadius: 3,
+                          background:
+                            colors[themeColor][
+                              global.theme === "dark" ? 900 : 100
+                            ],
                         },
                       },
-                      "& .Mui-selected": { pointerEvents: "none" },
-                    },
-                  }}
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  value={mealType}
-                  variant="filled"
-                  label="Age"
-                  onChange={setMealTypeEvent}
-                >
-                  <MenuItem value={"breakfast"}>Breakfast</MenuItem>
-                  <MenuItem value={"lunch"}>Lunch</MenuItem>
-                  <MenuItem value={"dinner"}>Dinner</MenuItem>
-                </Select>
-              </FormControl>
-            </>
-          ) : (
-            <></>
-          )}
-          <Button
-            variant="contained"
-            size="large"
-            sx={{ float: "right", mt: 3, borderRadius: 4, boxShadow: "none" }}
-          >
-            Create
-          </Button>
+                      sx: {
+                        "& .MuiMenuItem-root ": {
+                          borderRadius: 3,
+                          px: 3,
+                          mb: 0.1,
+                          py: 1.5,
+                          "&:hover": {
+                            backgroundColor: "rgba(200,200,200,.1)",
+                          },
+                        },
+                        "& .Mui-selected": { pointerEvents: "none" },
+                      },
+                    }}
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={formik.values.field1}
+                    onChange={(_e, v) =>
+                      formik.setFieldValue("field1", _e.target.value)
+                    }
+                    variant="filled"
+                    label="Age"
+                  >
+                    <MenuItem value={"breakfast"}>Breakfast</MenuItem>
+                    <MenuItem value={"lunch"}>Lunch</MenuItem>
+                    <MenuItem value={"dinner"}>Dinner</MenuItem>
+                  </Select>
+                </FormControl>
+              </>
+            ) : (
+              <></>
+            )}
+            <LoadingButton
+              loading={loading}
+              variant="contained"
+              size="large"
+              type="submit"
+              sx={{ float: "right", mt: 3, borderRadius: 4, boxShadow: "none" }}
+            >
+              Create
+            </LoadingButton>
+          </form>
         </Box>
       </SwipeableDrawer>
       <Button
