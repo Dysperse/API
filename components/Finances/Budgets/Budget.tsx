61d0d9d488d4137149c46d21fe3e87e4af0e4b1b
ManuTheCoder <manuthecoder@pm.me>
2022-06-16 18:11:21 +0000

    Update Budget.tsx

--- components/Finances/Budgets/Budget.tsx
+++ components/Finances/Budgets/Budget.tsx
@@ -4,6 +4,7 @@ import Typography from "@mui/material/Typography";
 import LinearProgress from "@mui/material/LinearProgress";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import IconButton from "@mui/material/IconButton";
+import { useState } from "react"
 
 export function Budget({
   category,
@@ -14,9 +15,17 @@ export function Budget({
   amount: string,
   type: "weekly" | "monthly",
 }) {
+const [open, setOpen] = useState<boolean>(false)
   return (
-    <ListItem sx={{ px: 1,pr:0 }}>
+    <ListItem sx={{ px: 1,pr:0, 
+        borderRadius:5,    
+    ...open && {
+        background: global.theme === "dark" ? "hsl(240, 11%, 20%)": "rgba(200,200,200,.2)",
+        p: 2,
+        py: 1,
+    } }}>
       <ListItemText
+      sx={{pr:8}}
         primary={
           <>
             <Typography variant="body2" sx={{ float: "right" }}>
@@ -26,15 +35,34 @@ export function Budget({
             <LinearProgress
               variant="determinate"
               value={(50 / parseInt(amount)) * 100}
-              sx={{ width: "100%", borderRadius: "4px", my: 1, height: 10 }}
+              sx={{ width: "100%", borderRadius: "4px", my: 1, height: 10,
+                  position:"relative",
+              
+              ...open && {
+                  ml: 3,
+                  mt: -3,
+                  transform: "scale(.4) translateY(-30px)"
+              } }}
             />
+            
           </>
         }
-        secondary={""}
+        secondary={open && (
+                <>
+                {...new Array(20).map(() => (
+                    <>
+                    </>
+                ))}
+                </>
+            )}
       />
-      <ListItemIcon sx={{ml:2,mr:-2}}>
-        <IconButton edge="end" aria-label="next" sx={{transition:"none"}}>
-          <span className="material-symbols-rounded">chevron_right</span>
+      <ListItemIcon sx={{ml:2,mr:-2,position:"absolute",top:19,right:4}}>
+        <IconButton onClick={() => setOpen(!open)} edge="end" aria-label="next" sx={{
+            transition:"none!important", 
+            ...open && {
+            transform: "rotate(180deg)"
+        }}}>
+          <span className="material-symbols-rounded">expand_more</span>
         </IconButton>
       </ListItemIcon>
     </ListItem>
