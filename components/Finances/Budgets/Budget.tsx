7a0899f00a756f9b75ad5f5ecbeff43ed29e6d93
Manu <manuthecoder@protonmail.com>
2022-06-17 19:54:14 +0000

    E

--- components/Finances/Budgets/Budget.tsx
+++ components/Finances/Budgets/Budget.tsx
@@ -7,47 +7,50 @@ import Skeleton from "@mui/material/Skeleton";
 import Box from "@mui/material/Box";
 import IconButton from "@mui/material/IconButton";
 import Button from "@mui/material/Button";
-import { useState } from "react"
-import SwipeableDrawer from "@mui/material/SwipeableDrawer"
-import { Puller } from "../../Puller"
+import { useState } from "react";
+import SwipeableDrawer from "@mui/material/SwipeableDrawer";
+import { Puller } from "../../Puller";
 
 export function Budget({
   category,
   amount,
   type,
 }: {
-  category: string,
-  amount: string,
-  type: "weekly" | "monthly",
+  category: string;
+  amount: string;
+  type: "weekly" | "monthly";
 }) {
-const [open, setOpen] = useState<boolean>(false)
-  return (<>
-    <ListItem sx={{ px: 2, mt: 1, pr:0, 
-    borderRadius: {sm: 5} }}>
-      <ListItemText
-        primary={
-          <>
-            <Typography variant="body2" sx={{ float: "right" }}>
-              50 / ${amount}
-            </Typography>
-            <Typography>{category}</Typography>
-            <LinearProgress
-              variant="determinate"
-              value={(50 / parseInt(amount)) * 100}
-              sx={{ width: "100%", borderRadius: "4px", my: 1, height: 10,}}
-            />
-            
-          </>
-        }
-      />
-      <ListItemIcon sx={{ml:2,mr:-2}}>
-        <IconButton onClick={() => setOpen(true)} edge="end" aria-label="next" sx={{ borderRadius: 4, "&, & *": {transition: "none!important"} }}>
-          <span className="material-symbols-rounded">chevron_right</span>
-        </IconButton>
-        
-      </ListItemIcon>
-    </ListItem>
-     <SwipeableDrawer
+  const [open, setOpen] = useState<boolean>(false);
+  return (
+    <>
+      <ListItem sx={{ px: 2, mt: 1, pr: 0, borderRadius: { sm: 5 } }}>
+        <ListItemText
+          primary={
+            <>
+              <Typography variant="body2" sx={{ float: "right" }}>
+                50 / ${amount}
+              </Typography>
+              <Typography>{category}</Typography>
+              <LinearProgress
+                variant="determinate"
+                value={(50 / parseInt(amount)) * 100}
+                sx={{ width: "100%", borderRadius: "4px", my: 1, height: 10 }}
+              />
+            </>
+          }
+        />
+        <ListItemIcon sx={{ ml: 2, mr: -2 }}>
+          <IconButton
+            onClick={() => setOpen(true)}
+            edge="end"
+            aria-label="next"
+            sx={{ borderRadius: 4, "&, & *": { transition: "none!important" } }}
+          >
+            <span className="material-symbols-rounded">chevron_right</span>
+          </IconButton>
+        </ListItemIcon>
+      </ListItem>
+      <SwipeableDrawer
         anchor="bottom"
         swipeAreaWidth={0}
         onOpen={() => setOpen(true)}
@@ -56,7 +59,7 @@ const [open, setOpen] = useState<boolean>(false)
           display: "flex",
           alignItems: { xs: "end", sm: "center" },
           height: "100vh",
-          justifyContent: "center"
+          justifyContent: "center",
         }}
         PaperProps={{
           sx: {
@@ -65,41 +68,62 @@ const [open, setOpen] = useState<boolean>(false)
             borderBottomRightRadius: { xs: 0, sm: "28px!important" },
             position: "unset",
             mx: "auto",
-            ...global.theme==="dark" && {
-                background: "hsl(240, 11%, 17%)"
-            },
+            ...(global.theme === "dark" && {
+              background: "hsl(240, 11%, 17%)",
+            }),
             maxWidth: { sm: "70vw", xs: "100vw" },
-            overflow: "hidden"
-          }
+            overflow: "hidden",
+          },
         }}
         onClose={() => setOpen(false)}
       >
-          <Box sx={{ p: 1, display: {sm: "none"} }}>
-           <Puller />
-          </Box>
-          <Box sx={{p:4}}>
-            <Typography variant="h4" gutterBottom sx={{fontWeight:"800"}}>{category}</Typography>
-            <Typography variant="h5" sx={{mb:2}}>
-              50 / ${amount}
-            </Typography>
-            <LinearProgress
-              variant="determinate"
-              value={(50 / parseInt(amount)) * 100}
-              sx={{ width: "100%", borderRadius: "4px", my: 1, height: 10,}}
-            />
-            <Typography variant="body2" sx={{mt:3}}>
-              Expenses on this budget in the past month
-            </Typography>
-            <Box sx={{whiteSpace:"nowrap", overflowX:"scroll",mt: 2, px: 1 }}>
-                {
-                    [...new Array(10)].map(() => (
-                        <Skeleton variant="rectangular" height={100} width={150} sx={{mr:1,display: "inline-block",borderRadius:5}} animation="wave" />
-                    ))
-                }
-            </Box>
-            <Button variant="outlined" sx={{borderWidth:"2px!important", "&, & *": {transition:"none",animationDuration: "0s!important"},mt:2,float:"right",borderRadius:5}} size="large">Delete</Button>
+        <Box sx={{ p: 1, display: { sm: "none" } }}>
+          <Puller />
         </Box>
-          </SwipeableDrawer>
+        <Box sx={{ p: 4 }}>
+          <Typography variant="h4" gutterBottom sx={{ fontWeight: "800" }}>
+            {category}
+          </Typography>
+          <Typography variant="h5" sx={{ mb: 2 }}>
+            50 / ${amount}
+          </Typography>
+          <LinearProgress
+            variant="determinate"
+            value={(50 / parseInt(amount)) * 100}
+            sx={{ width: "100%", borderRadius: "4px", my: 1, height: 10 }}
+          />
+          <Typography variant="body2" sx={{ mt: 3 }}>
+            Expenses on this budget in the past month
+          </Typography>
+          <Box sx={{ whiteSpace: "nowrap", overflowX: "scroll", mt: 2, px: 1 }}>
+            {[...new Array(10)].map(() => (
+              <Skeleton
+                variant="rectangular"
+                height={100}
+                width={150}
+                sx={{ mr: 1, display: "inline-block", borderRadius: 5 }}
+                animation="wave"
+              />
+            ))}
+          </Box>
+          <Button
+            variant="outlined"
+            sx={{
+              borderWidth: "2px!important",
+              "&, & *": {
+                transition: "none",
+                animationDuration: "0s!important",
+              },
+              mt: 2,
+              float: "right",
+              borderRadius: 5,
+            }}
+            size="large"
+          >
+            Delete
+          </Button>
+        </Box>
+      </SwipeableDrawer>
     </>
   );
 }
--- components/Finances/Budgets/index.tsx
+++ components/Finances/Budgets/index.tsx
@@ -4,28 +4,36 @@ import CardContent from "@mui/material/CardContent";
 import { Budget } from "./Budget";
 
 export function Budgets() {
-  return (<>
- 
-    <Card
-      sx={{
-        background:
-          global.theme === "dark"
-            ? "hsl(240, 11%, 13%)"
-            : "rgba(200, 200, 200, .3)",
-        borderRadius: 5,
-        boxShadow: 0,
-        p: 1,
-        mt: 2,
-      }}
-    >
-      <Typography sx={{ fontWeight: "600", my: 2, ml: 2, mb: 0 }} variant="h5">
-        Budgets
-      </Typography>
-      <CardContent>
-        <Typography sx={{ mt:-1, fontSize:"13px", textTransform:"uppercase"}}>This month</Typography>
-        <Budget category="Food and Drink" amount="1000" type="monthly" />
-        <Budget category="Sporting Goods" amount="500" type="monthly" />
-      </CardContent>
-    </Card></>
+  return (
+    <>
+      <Card
+        sx={{
+          background:
+            global.theme === "dark"
+              ? "hsl(240, 11%, 13%)"
+              : "rgba(200, 200, 200, .3)",
+          borderRadius: 5,
+          boxShadow: 0,
+          p: 1,
+          mt: 2,
+        }}
+      >
+        <Typography
+          sx={{ fontWeight: "600", my: 2, ml: 2, mb: 0 }}
+          variant="h5"
+        >
+          Budgets
+        </Typography>
+        <CardContent>
+          <Typography
+            sx={{ mt: -1, fontSize: "13px", textTransform: "uppercase" }}
+          >
+            This month
+          </Typography>
+          <Budget category="Food and Drink" amount="1000" type="monthly" />
+          <Budget category="Sporting Goods" amount="500" type="monthly" />
+        </CardContent>
+      </Card>
+    </>
   );
 }
