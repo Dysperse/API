f603808c88e468898a92758ad4bfae16e33c1163
Manu <manuthecoder@protonmail.com>
2022-06-17 20:33:32 +0000

    pwa not working

--- next.config.js
+++ next.config.js
@@ -11,19 +11,14 @@ const withTM = require("next-transpile-modules")([
 
 const withPlugins = require("next-compose-plugins");
 const withPWA = require("next-pwa");
-const runtimeCaching = require('next-pwa/cache')
 
 module.exports = withPlugins(
   [
     [
-      withTM({
-        // your custom config goes here
-      }),
+      withTM(),
       withPWA({
         pwa: {
-          // disable: process.env.NODE_ENV === "development",
-          dest: "public",
-          runtimeCaching
+          dest: "public"
         }
       })
     ]
