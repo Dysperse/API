adeab49ae4ae8e38bae2ef5b258767711a0d59c2
Manu <manuthecoder@protonmail.com>
2022-06-25 15:50:46 +0000

    Fix sign out bugs

--- components/Settings/index.tsx
+++ components/Settings/index.tsx
@@ -66,7 +66,6 @@ function Logout() {
             }}
             onClick={() => {
               setOpen(false);
-              window.location.href = "/api/logout";
             }}
           >
             Cancel
