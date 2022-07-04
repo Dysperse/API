f3683fd029633a1a52585b332578a8b6c78da6b6
Manu <manuthecoder@protonmail.com>
2022-07-04 07:29:15 +0000

    E

--- pages/share/[index].tsx
+++ pages/share/[index].tsx
@@ -1,9 +1,32 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
 export default function Share() {
   return (
-    <Head>
-      <title>Share</title>
-    </Head>
+    <>
+      <Head>
+        <title>Share</title>
+      </Head>
+      <Box
+        sx={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          background: "#C4B5B5",
+        }}
+      >
+        <picture>
+          <img
+            src="https://cdn.jsdelivr.net/gh/Smartlist-App/Assets@master/v2/rounded.png"
+            alt="Carbon"
+          />
+        </picture>
+        <Typography>Carbon</Typography>
+      </Box>
+    </>
   );
 }
