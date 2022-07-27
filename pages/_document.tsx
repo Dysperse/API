56174dda717d0b0d5e3936d41390e8d2c2a2b8e1
Manu <manuthecoder@protonmail.com>
2022-07-27 20:11:27 +0000

    update _document.tsx and manifest.json

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -39,7 +39,7 @@ export default function Document() {
           rel="stylesheet"
         />
         <meta name="description" content="Carbon user dashboard" />
-        <meta name="theme-color" content="#C4B5B5" />
+        <meta name="theme-color" content="#d7ccc8" />
         <link
           rel="apple-touch-icon"
           href="https://cdn.jsdelivr.net/gh/Smartlist-App/Assets@master/img/logo/apple-touch-icon/apple-touch-icon.png"
