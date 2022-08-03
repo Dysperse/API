f4dd7b831f0b39638ab442c4ee7512c6a6818eda
Manu <manuthecoder@protonmail.com>
2022-08-03 20:25:22 +0000

    update AppsMenu.tsx

--- components/Layout/AppsMenu.tsx
+++ components/Layout/AppsMenu.tsx
@@ -289,7 +289,7 @@ export function AppsMenu() {
         )}
       </Tooltip>
       <Menu
-        BackdropProps={{ sx: { opacity: "0!important" } }}
+        BackdropProps={{ sx: { opacity: { sm: "0!important" } } }}
         id="demo-positioned-menu"
         aria-labelledby="demo-positioned-button"
         anchorEl={anchorEl}
