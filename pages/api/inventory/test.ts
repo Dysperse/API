8e17495cc5fcd43d61ef26be2a74e820e93b7ef5
Manu <manuthecoder@protonmail.com>
2022-08-15 17:24:18 +0000

    update test.ts

--- pages/api/inventory/test.ts
+++
@@ -1,9 +0,0 @@
-import CryptoJS from "crypto-js";
-
-export default function handler(req, res) {
-  const decryptedAmount = CryptoJS.AES.decrypt(
-    "U2FsdGVkX19ywHDXomuPm5X+lSfgKJmJZHPrqqROGOQ=",
-    process.env.INVENTORY_ENCRYPTION_KEY
-  ).toString(CryptoJS.enc.Utf8);
-  res.json(decryptedAmount);
-}
