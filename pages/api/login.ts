113fb38f2ed767e652afc1ad9ba193a16dc66523
manuthecoder <manuthecoder@protonmail.com>
2022-08-23 10:22:28 -0700

    Update login.ts

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -2,7 +2,7 @@ import { serialize } from "cookie";
 import jwt from "jsonwebtoken";
 
 export default function handler(req, res) {
-  const encoded = jwt.sign(req.query.token, process.env.SECRET_COOKIE_PASSWORD);
+  const encoded = jwt.sign(req.query.token, process.env.SECRET_COOKIE_PASSWORD, { expiresIn: "7d" });
   res.setHeader("Set-Cookie", serialize("token", encoded, { path: "/" }));
   // res.json({ success: true, key: encoded });
   res.redirect("/");
