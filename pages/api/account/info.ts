d8cc40407f854ecc9c5bb9e115e89fa757f0124b
manuthecoder <manuthecoder@protonmail.com>
2022-09-02 04:03:55 +0000

    update info.ts

--- pages/api/account/info.ts
+++ pages/api/account/info.ts
@@ -32,13 +32,16 @@ export const getUserData = async (token: string) => {
 };
 
 const handler = async (req: any, res: any) => {
-  res.send(process.env.DATABASE_URL);
-  return;
-  const session = await getUserData(req.query.token);
-  if (session) {
-    res.json(session);
-  } else {
-    res.status(401).json({ message: "Invalid token" });
+  try {
+    const session = await getUserData(req.query.token);
+    if (session) {
+      res.json(session);
+    } else {
+      res.status(401).json({ message: "Invalid token" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.send(err);
   }
 };
 export default handler;
