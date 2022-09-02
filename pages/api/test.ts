67609a98064001cf1db6654a8337efe69479ae5a
manuthecoder <manuthecoder@protonmail.com>
2022-09-02 04:22:50 +0000

    update test.ts

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -1,6 +1,6 @@
 import { getAllUsers } from "../../prisma/user";
 
-const handler = async (req, res) => {
+const handler = async (req: any, res: any) => {
   const users = await getAllUsers();
   res.json(users);
 };
