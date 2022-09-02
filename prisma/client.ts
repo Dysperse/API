1cee735acf79af4d1a8fcfd9a5824fd87252faac
manuthecoder <manuthecoder@protonmail.com>
2022-09-02 04:14:04 +0000

    update client.ts

--- prisma/client.ts
+++ prisma/client.ts
@@ -1,2 +1,16 @@
 import { PrismaClient } from "@prisma/client";
-export const prisma: any = new PrismaClient();
+
+// Avoid instantiating too many instances of Prisma in development
+// https://www.prisma.io/docs/support/help-articles/nextjs-prisma-client-dev-practices#problem
+let prisma: any;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export { prisma };
