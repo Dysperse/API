ac0db01e468e9804bc7a1a2ae457cda245c95c17
manuthecoder <manuthecoder@protonmail.com>
2022-09-02 03:15:08 +0000

    update info.ts and schema.prisma

--- pages/api/account/info.ts
+++ pages/api/account/info.ts
@@ -1,64 +1,20 @@
-import executeQuery from "../../../lib/db";
-import { ExchangeToken } from "../../../lib/exchange-token";
-import { validatePerms } from "../../../lib/check-permissions";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
-export const getInfo = async (token: string) => {
-  const userId = await ExchangeToken(token);
-  const result = await executeQuery({
-    query: "SELECT * FROM Accounts WHERE id = ?",
-    values: [userId ?? false],
-  });
-
-  if (!result[0]) {
-    return {
-      data: false,
-    };
+function exclude(user: any, ...keys: any[]) {
+  for (let key of keys) {
+    delete user[key];
   }
+  return user;
+}
 
-  const property = await executeQuery({
-    query: "SELECT * FROM SyncTokens WHERE propertyToken = ? AND email = ?",
-    values: [result[0].SyncToken, result[0].email],
+// Query returns User or null
+const handler = async (req: any, res: any) => {
+  const getUser: any | null = await prisma.userTokens.findUnique({
+    where: {
+      id: 1,
+    },
   });
-
-  // const perms = await validatePerms(
-  //   property[0].propertyToken,
-  //   property[0].accessToken
-  // );
-
-  return {
-    // perms: perms,
-    account: result.map((data) => {
-      return {
-        ...data,
-        id: undefined,
-        password: undefined,
-        SyncToken: undefined,
-        FinanceToken: undefined,
-        onboarding: parseInt(data.onboarding),
-        notificationMin: parseInt(data.notificationMin),
-        verifiedEmail: parseInt(data.verifiedEmail),
-        darkMode: JSON.parse(data.darkMode),
-        accessToken: token,
-        financeToken: data.FinanceToken,
-      };
-    })[0],
-    property: property.map((data) => {
-      return {
-        ...data,
-        id: undefined,
-        accepted: JSON.parse(data.accepted),
-      };
-    })[0],
-  };
+  res.json(exclude(getUser));
 };
-
-const handler = async (req, res) => {
-  const data = await getInfo(req.query.token);
-  if (data.data === false) {
-    res.status(500).send('{ "error": true }');
-    return;
-  }
-  res.json(data);
-};
-
 export default handler;
