e76a254a6e66276faac53576fc20ab12689b1044
manuthecoder <manuthecoder@protonmail.com>
2022-09-02 04:23:05 +0000

    update user.ts

--- prisma/user.ts
+++ prisma/user.ts
@@ -1,6 +1,6 @@
 import prisma from "./prisma";
 
 export const getAllUsers = async () => {
-  const users = await prisma.user.findMany({});
+  const users = await prisma.session.findMany({});
   return users;
 };
