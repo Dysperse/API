edb33071f0268d7bda490efd281b0da90c7c872f
manuthecoder <manuthecoder@protonmail.com>
2022-09-03 22:36:29 +0000

    update dashboard.tsx

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -3,10 +3,18 @@ import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Paper from "@mui/material/Paper";
+import Chip from "@mui/material/Chip";
 import Head from "next/head";
 import { RecentItems } from "../components/dashboard/RecentItems";
 import { Lists } from "../components/dashboard/Lists";
+import * as colors from "@mui/material/colors";
+
 export default function Dashboard() {
+  const styles = {
+    mr: 1,
+    px: 0.7,
+    boxShadow: "0!important",
+  };
   return (
     <>
       <Head>
@@ -17,10 +25,22 @@ export default function Dashboard() {
         </title>
       </Head>
       <Container sx={{ mt: 4 }}>
-        <Box sx={{ p: 1 }}>
+        <Box sx={{ pb: 3, pl: 1 }}>
           <Typography variant="h4" sx={{ mb: 3 }}>
             Hey, {global.user.name.split(" ")[0]}.
           </Typography>
+          <Chip label="Productivity" onClick={() => true} sx={styles} />
+          <Chip
+            label="Tasks"
+            onClick={() => true}
+            sx={{
+              ...styles,
+              background: colors[themeColor]["800"] + "!important",
+              color: "#fff",
+            }}
+          />
+          <Chip label="Recent" onClick={() => true} sx={styles} />
+          <Chip label="Tips" onClick={() => true} sx={styles} />
         </Box>
         <Box sx={{ mr: -2 }}>
           <Masonry columns={{ xs: 1, sm: 2 }} spacing={2}>
