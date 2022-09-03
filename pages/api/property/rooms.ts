d412074d01733af1693333fd3a625bc8f3be4d36
manuthecoder <manuthecoder@protonmail.com>
2022-09-03 05:03:11 +0000

    update rooms.ts

--- pages/api/property/rooms.ts
+++ pages/api/property/rooms.ts
@@ -4,6 +4,10 @@ const handler = async (req: any, res: any) => {
   const data: any | null = await prisma.customRoom.findMany({
     where: {
       propertyId: req.query.propertyId,
+      Property: {
+        id: req.query.propertyId,
+        accessToken: req.query.accessToken,
+      },
     },
   });
   res.json(data);
