7a4ad0fd349c04b685fd470cc4b1698bbca91b28
manuthecoder <manuthecoder@protonmail.com>
2022-09-04 20:15:57 +0000

    update cookie.ts

--- pages/api/cookie.ts
+++ pages/api/cookie.ts
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 const handler = async (req, res) => {
   const { accessToken } = jwt.verify(
-    req.query.token,
+    req.cookies.token,
     process.env.SECRET_COOKIE_PASSWORD
   );
   res.json(accessToken);
