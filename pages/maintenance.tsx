334e7d97a9ccd4c16ae96c1482b54ec567d8a70d
manuthecoder <manuthecoder@protonmail.com>
2022-09-05 18:43:47 +0000

    update maintenance.tsx

--- pages/maintenance.tsx
+++ pages/maintenance.tsx
@@ -1,29 +1,126 @@
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardActionArea from "@mui/material/CardActionArea";
+import CardContent from "@mui/material/CardContent";
 import * as colors from "@mui/material/colors";
-import { useState, useEffect } from "react";
-import { useRouter } from "next/router";
-import { useTheme } from "@mui/material/styles";
-import useMediaQuery from "@mui/material/useMediaQuery";
+import Skeleton from "@mui/material/Skeleton";
+import SwipeableDrawer from "@mui/material/SwipeableDrawer";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import dayjs from "dayjs";
+import { useState } from "react";
 import useSWR from "swr";
 import { ErrorHandler } from "../components/ErrorHandler";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardActionArea from "@mui/material/CardActionArea";
-import Skeleton from "@mui/material/Skeleton";
-import dayjs from "dayjs";
-function Header() {
-  // Decrease header size on scroll
-  const [scroll, setScroll] = useState(false);
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+import { Puller } from "../components/Puller";
 
-  useEffect(() => {
-    window.addEventListener("scroll", () => {
-      setScroll(window.scrollY > 10);
-    });
-  }, []);
+function Reminder({ reminder }: any) {
+  const [open, setOpen] = useState<boolean>(false);
 
+  return (
+    <>
+      <SwipeableDrawer
+        anchor="bottom"
+        open={open}
+        onClose={() => setOpen(false)}
+        onOpen={() => setOpen(true)}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            background: colors[themeColor]["50"],
+            borderRadius: "30px 30px 0 0",
+          },
+        }}
+      >
+        <Puller />
+        <Box sx={{ p: 4, mt: 2, textAlign: "center" }}>
+          <Typography variant="h4" sx={{ mb: 1, fontWeight: "600" }}>
+            {reminder.name}
+          </Typography>
+          <Typography sx={{ mb: 3 }}>
+            <span style={{ textTransform: "capitalize" }}>
+              {reminder.frequency}
+            </span>{" "}
+            &bull; Due {dayjs(reminder.nextDue).fromNow()}
+          </Typography>
+          <TextField
+            multiline
+            fullWidth
+            onBlur={(e) => {
+              e.target.placeholder = "Click to add note";
+              e.target.spellcheck = false;
+            }}
+            onKeyUp={(e: any) => {
+              if (e.code === "Enter" && !e.shiftKey) {
+                e.preventDefault();
+                e.target.value = e.target.value.trim();
+                e.target.blur();
+              }
+            }}
+            InputProps={{
+              disableUnderline: true,
+              sx: {
+                background:
+                  (global.theme === "dark"
+                    ? "hsl(240, 11%, 24%)"
+                    : colors[themeColor][100]) + "!important",
+                cursor: "pointer",
+                p: 2.5,
+                borderRadius: "15px",
+                display: "block",
+                minHeight: "100px",
+              },
+            }}
+            spellCheck={false}
+            variant="filled"
+            defaultValue={reminder.note}
+            maxRows={4}
+            onFocus={(e) => {
+              e.target.placeholder = "SHIFT+ENTER for new lines";
+              e.target.spellcheck = true;
+            }}
+            disabled={global.property.role === "read-only"}
+            placeholder={
+              global.property.role !== "read-only"
+                ? "Click to add note"
+                : "You do not have permission to edit this item"
+            }
+          />
+          <Button
+            fullWidth
+            size="large"
+            variant="outlined"
+            sx={{ mt: 2, borderWidth: "2px!important", borderRadius: 999 }}
+          >
+            Delete
+          </Button>
+        </Box>
+      </SwipeableDrawer>
+      <Card
+        sx={{
+          mb: 2,
+          background: "rgba(200,200,200,.3)",
+          borderRadius: 5,
+        }}
+      >
+        <CardActionArea onClick={() => setOpen(true)}>
+          <CardContent sx={{ px: 3.5, py: 3 }}>
+            <Typography variant="h6">{reminder.name}</Typography>
+            <Typography variant="h6">{reminder.note}</Typography>
+            <Typography variant="body2">
+              <span style={{ textTransform: "capitalize" }}>
+                {reminder.frequency}
+              </span>{" "}
+              &bull; Due {dayjs(reminder.nextDue).fromNow()}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </>
+  );
+}
+
+function Header({ count }) {
   return (
     <Box sx={{ p: { sm: 3 } }}>
       <Box
@@ -44,7 +141,7 @@ function Header() {
           justifyContent: "center",
         }}
       >
-        <Typography variant="h1">7</Typography>
+        <Typography variant="h1">{count}</Typography>
         <Typography variant="h6">tasks left for this month</Typography>
       </Box>
     </Box>
@@ -62,7 +159,7 @@ export default function Maintenance(req, res) {
 
   return (
     <Box sx={{ mb: 4 }}>
-      <Header />
+      <Header count={data ? data.length : 0} />
       <Box sx={{ p: 4 }}>
         <Typography variant="h5" sx={{ fontWeight: "600", mb: 3 }}>
           Upcoming
@@ -70,26 +167,7 @@ export default function Maintenance(req, res) {
         {data ? (
           <>
             {data.map((reminder) => (
-              <Card
-                key={reminder.id.toString()}
-                sx={{
-                  background: "rgba(200,200,200,.3)",
-                  borderRadius: 5,
-                }}
-              >
-                <CardActionArea>
-                  <CardContent sx={{ px: 3.5, py: 3 }}>
-                    <Typography variant="h6">{reminder.name}</Typography>
-                    <Typography variant="h6">{reminder.note}</Typography>
-                    <Typography variant="body2">
-                      <span style={{ textTransform: "capitalize" }}>
-                        {reminder.frequency}
-                      </span>{" "}
-                      &bull; Due {dayjs(reminder.nextDue).fromNow()}
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
+              <Reminder reminder={reminder} key={reminder.id.toString()} />
             ))}
           </>
         ) : (
