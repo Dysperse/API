6a87ab456b8828ee95156f3e08bedd7f704dadc1
manuthecoder <manuthecoder@protonmail.com>
2022-09-05 22:48:21 +0000

    feat: create updateNote.ts

---
+++ pages/api/property/inventory/updateNote.ts
@@ -0,0 +1,32 @@
+import { prisma } from "../../../../lib/client";
+import CryptoJS from "crypto-js";
+import { validatePermissions } from "../../../../lib/validatePermissions";
+
+const handler = async (req: any, res: any) => {
+  const permissions = await validatePermissions(
+    req.query.property,
+    req.query.accessToken
+  );
+  console.log(1);
+  if (!permissions || permissions === "read-only") {
+    res.status(401).json({ error: "Unauthorized" });
+    return;
+  }
+  //   Update the note on an item
+  const data: any | null = await prisma.item.update({
+    where: {
+      id: parseInt(req.query.id),
+    },
+    data: {
+      note:
+        CryptoJS.AES.encrypt(
+          req.query.note,
+          process.env.INVENTORY_ENCRYPTION_KEY
+        ).toString() ?? "",
+    },
+  });
+
+  res.json(data);
+};
+
+export default handler;
