aeea908000f61a215defc4c467e3c5600ebc3dc2
manuthecoder <manuthecoder@protonmail.com>
2022-09-07 07:57:53 +0000

    update list.ts and recent.ts

--- pages/api/property/inventory/list.ts
+++ pages/api/property/inventory/list.ts
@@ -1,13 +1,22 @@
 import { prisma } from "../../../../lib/client";
 import CryptoJS from "crypto-js";
+import { validatePermissions } from "../../../../lib/validatePermissions";
 
 const handler = async (req: any, res: any) => {
+  const permissions = await validatePermissions(
+    req.query.property,
+    req.query.accessToken
+  );
+  if (!permissions) {
+    res.status(401).json({ error: "Unauthorized" });
+    return;
+  }
+
   const data: any | null = await prisma.item.findMany({
     where: {
       room: req.query.room,
       property: {
         id: req.query.property,
-        accessToken: req.query.accessToken,
       },
     },
   });
--- pages/api/property/inventory/recent.ts
+++ pages/api/property/inventory/recent.ts
@@ -1,13 +1,21 @@
 import { prisma } from "../../../../lib/client";
 import CryptoJS from "crypto-js";
+import { validatePermissions } from "../../../../lib/validatePermissions";
 
 const handler = async (req: any, res: any) => {
+  const permissions = await validatePermissions(
+    req.query.property,
+    req.query.accessToken
+  );
+  if (!permissions) {
+    res.status(401).json({ error: "Unauthorized" });
+    return;
+  }
+
   const data: any | null = await prisma.item.findMany({
     where: {
-      property: req.query.property,
       property: {
         id: req.query.property,
-        accessToken: req.query.accessToken,
       },
     },
     orderBy: {
