c99aebc53b78f0577fd16caa39090ef21e61bb63
manuthecoder <manuthecoder@protonmail.com>
2022-10-04 22:01:20 +0000

    build(deps): delete 1 file and update 3 files

--- components/Tidy/Header.tsx
+++ components/Tidy/Header.tsx
@@ -1,51 +1,91 @@
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import Stepper from "@mui/material/Stepper";
+import { styled } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
 import { colors } from "../../lib/colors";
-import { Calendar as CalendarList } from "./Calendar";
 
-/**
- * Select frequency settiong
- * @param {any} {name
- * @param {any} formik}
- * @returns {any}
- */
-function FrequencySetting({ name, formik }: { name: string; formik }) {
+import StepConnector, {
+  stepConnectorClasses,
+} from "@mui/material/StepConnector";
+import { StepIconProps } from "@mui/material/StepIcon";
+
+const QontoConnector = styled(StepConnector)(({ theme }) => ({
+  [`&.${stepConnectorClasses.alternativeLabel}`]: {
+    top: 10,
+    left: "calc(-50% + 16px)",
+    right: "calc(50% + 16px)",
+  },
+  [`&.${stepConnectorClasses.active}`]: {
+    [`& .${stepConnectorClasses.line}`]: {
+      borderColor: `${colors[themeColor][50]}`,
+    },
+  },
+  [`&.${stepConnectorClasses.completed}`]: {
+    [`& .${stepConnectorClasses.line}`]: {
+      borderColor: `${colors[themeColor][50]}`,
+    },
+  },
+  [`& .${stepConnectorClasses.line}`]: {
+    borderColor:
+      theme.palette.mode === "dark"
+        ? theme.palette.grey[800]
+        : `${colors[themeColor][700]}`,
+    borderTopWidth: 3,
+    borderRadius: 1,
+  },
+}));
+
+const QontoStepIconRoot = styled("div")<{ ownerState: { active?: boolean } }>(
+  ({ theme, ownerState }) => ({
+    color:
+      theme.palette.mode === "dark"
+        ? theme.palette.grey[700]
+        : `${colors[themeColor][300]}`,
+    display: "flex",
+    height: 22,
+    alignItems: "center",
+    ...(ownerState.active && {
+      color: `${colors[themeColor][50]}`,
+    }),
+    "& .QontoStepIcon-completedIcon": {
+      color: `${colors[themeColor][50]}`,
+      zIndex: 1,
+      fontSize: 18,
+    },
+    "& .QontoStepIcon-circle": {
+      width: 8,
+      height: 8,
+      borderRadius: "50%",
+      backgroundColor: "currentColor",
+    },
+  })
+);
+
+function QontoStepIcon(props: StepIconProps) {
+  const { active, completed, className } = props;
+
   return (
-    <Button
-      onClick={() => formik.setFieldValue("frequency", name)}
-      sx={{
-        px: 3,
-        width: "33.9%",
-        borderRadius: 999,
-        height: 40,
-        transition: "none!important",
-        textTransform: "capitalize",
-        whiteSpace: "nowrap",
-        overflow: "hidden",
-        textOverflow: "ellipsis",
-        ...(formik.values.frequency === name && {
-          background: `${
-            colors[themeColor][global.theme !== "dark" ? 100 : 900]
-          }!important`,
-          color: `${
-            colors[themeColor][global.user.darkMode ? 50 : 900]
-          }!important`,
-        }),
-      }}
-    >
-      {formik.values.frequency === name && (
+    <QontoStepIconRoot ownerState={{ active }} className={className}>
+      {completed ? (
         <span
           className="material-symbols-rounded"
-          style={{ marginRight: "10px" }}
+          style={{
+            color: `${colors[themeColor][50]}`,
+          }}
         >
           check
         </span>
+      ) : (
+        <div className="QontoStepIcon-circle" />
       )}
-      {name}
-    </Button>
+    </QontoStepIconRoot>
   );
 }
+
+const steps = ["Declutter", "Discard", "Organize"];
 
 /**
  * Bannner for maintenance, which shows upcoming tasks the current week
@@ -66,16 +106,43 @@ export function Header({
           sx={{
             width: "100%",
             background: colors[themeColor][800],
-            color: `${colors[themeColor][100]} !important`,
+            color: `${colors[themeColor][100]}`,
             borderRadius: { sm: 5 },
             pb: 3,
             display: "block",
+            pt: 2,
             py: { sm: 3 },
-            userSelect: "none",
           }}
         >
-          <Typography></Typography>
+          {/* <Typography sx={{ fontWeight: "600" }}>Tidy</Typography> */}
+          <Stepper
+            alternativeLabel
+            activeStep={step}
+            connector={<QontoConnector />}
+          >
+            {steps.map((label) => (
+              <Step key={label} sx={{}}>
+                <StepLabel StepIconComponent={QontoStepIcon}>
+                  <span
+                    style={{
+                      // if step is completed, make text green
+                      color:
+                        step > steps.indexOf(label)
+                          ? colors[themeColor][50]
+                          : colors[themeColor][100],
+                      fontWeight:
+                        step === steps.indexOf(label) ? "bold" : "400",
+                    }}
+                  >
+                    {label}
+                  </span>
+                </StepLabel>
+              </Step>
+            ))}
+          </Stepper>
         </Box>
+        <Button onClick={() => setStep(step - 1)}>Back</Button>
+        <Button onClick={() => setStep(step + 1)}>Next</Button>
       </Box>
     </>
   );
