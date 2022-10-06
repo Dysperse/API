8c7221f798969e034e92b304a5465d369ace97a4
manuthecoder <manuthecoder@protonmail.com>
2022-10-06 03:03:07 +0000

    update tidy.tsx

--- pages/tidy.tsx
+++ pages/tidy.tsx
@@ -8,14 +8,86 @@ import { ErrorHandler } from "../components/ErrorHandler";
 import { Header } from "../components/Tidy/Header";
 import { useApi } from "../hooks/useApi";
 import type { ApiResponse } from "../types/client";
+import { Button, LinearProgress } from "@mui/material";
+import dayjs from "dayjs";
 
 function Reminder({ data }: { data: MaintenanceReminder }) {
+  const { lastDone, frequency, on } = data;
+  let nextDue = new Date(lastDone);
+  let days = 1;
+
+  switch (frequency) {
+    case "daily":
+      // Add 1 day to now variable
+      nextDue.setDate(nextDue.getDate() + 1);
+      days = 1;
+      break;
+    case "weekly":
+      // Add 7 days to now variable
+      nextDue.setDate(nextDue.getDate() + 7);
+      days = 7;
+      break;
+    case "monthly":
+      // Add 1 month to now variable
+      nextDue.setMonth(nextDue.getMonth() + 1);
+      days = 30;
+      break;
+    case "every 6 months":
+      // Add 6 months to now variable
+      nextDue.setMonth(nextDue.getMonth() + 6);
+      days = 180;
+      break;
+    case "yearly":
+      // Add 1 year to now variable
+      nextDue.setFullYear(nextDue.getFullYear() + 1);
+      days = 365;
+      break;
+  }
+
+  // Calculate days left until `nextDue` using dayjs
+  const daysLeft = dayjs(nextDue).diff(dayjs(), "day");
+  const daysLeftFromLastDone = dayjs(nextDue).diff(dayjs(lastDone), "day");
+
+  // Make lastDone and nextDue a percentage
+  const percent = (daysLeft / days) * 100;
+
   return (
-    <Card sx={{ p: 3, pt: 1 }}>
+    <Card
+      sx={{
+        background: "rgba(200,200,200,.3)",
+        mb: 2,
+        borderRadius: 5,
+        "& *": {
+          transition: "none",
+        },
+      }}
+    >
       <CardActionArea>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
+        <CardContent sx={{ px: 4, py: 2 }}>
+          <Typography gutterBottom variant="h6">
             {data.name}
+          </Typography>
+          <Typography gutterBottom variant="body1">
+            <span
+              style={{
+                textTransform: "capitalize",
+              }}
+            >
+              {data.frequency}
+            </span>{" "}
+            on {data.on}
+            <LinearProgress
+              variant="determinate"
+              value={percent}
+              sx={{ mt: 2, borderRadius: 5, height: 9 }}
+            />
+            <Button
+              onClick={() => {
+                alert(daysLeft);
+              }}
+            >
+              Button
+            </Button>
           </Typography>
         </CardContent>
       </CardActionArea>
