cb2e5757bf15785f072f98fc321b292e4005e9b9
manuthecoder <manuthecoder@protonmail.com>
2022-10-14 16:05:18 +0000

    update 5 files and delete 1 file

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
  */
 
 const withPWA = require('next-pwa')({
-  disable: process.env.NODE_ENV == "development",
+  // disable: process.env.NODE_ENV == "development",
   dest: "public",
   reloadOnOnline: false
 })
--- pages/share/[index].tsx
+++ pages/share/[index].tsx
@@ -1,137 +1,108 @@
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Skeleton from "@mui/material/Skeleton";
-import Typography from "@mui/material/Typography";
+import { useEffect, useState } from "react";
 import Head from "next/head";
-import { useRouter } from "next/router";
 
-/**
- * Query function
- * @param {any} {query}
- * @returns {any}
- */
-function Query({ query }) {
-  let of = false;
-  if (
-    query.quantity.toLowerCase().endsWith("boxes") ||
-    query.quantity.toLowerCase().endsWith("packs") ||
-    query.quantity.toLowerCase().endsWith("jars") ||
-    query.quantity.toLowerCase().endsWith("cups") ||
-    query.quantity.toLowerCase().endsWith("cups") ||
-    query.quantity.toLowerCase().endsWith("mugs") ||
-    query.quantity.toLowerCase().endsWith("📦") ||
-    query.quantity.toLowerCase().endsWith("packages")
-  ) {
-    of = true;
+const base64ToUint8Array = (base64) => {
+  const padding = "=".repeat((4 - (base64.length % 4)) % 4);
+  const b64 = (base64 + padding).replace(/-/g, "+").replace(/_/g, "/");
+
+  const rawData = window.atob(b64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
   }
-  return (
-    <Typography
-      variant="h1"
-      sx={{ color: "#232323", fontSize: { xs: "40px", sm: "50px" } }}
-    >
-      <abbr title="Person">{query.name}</abbr> has{" "}
-      <abbr title="Item name">{query.quantity}</abbr>
-      {of ? " of " : " "}
-      <abbr title="Item quantity">{query.title.toLowerCase()}</abbr> in the{" "}
-      <abbr title="Room">kitchen</abbr>
-    </Typography>
-  );
-}
+  return outputArray;
+};
 
-/**
- * Top-level component for the share page
- * @returns {any}
- */
-export default function Share() {
-  const router = useRouter();
-  const query = router && router.query.index;
+const Index = () => {
+  const [isSubscribed, setIsSubscribed] = useState(false);
+  const [subscription, setSubscription] = useState(null);
+  const [registration, setRegistration] = useState(null);
+
+  useEffect(() => {
+    if (
+      typeof window !== "undefined" &&
+      "serviceWorker" in navigator &&
+      window.workbox !== undefined
+    ) {
+      // run only in browser
+      navigator.serviceWorker.ready.then((reg) => {
+        reg.pushManager.getSubscription().then((sub) => {
+          if (
+            sub &&
+            !(
+              sub.expirationTime &&
+              Date.now() > sub.expirationTime - 5 * 60 * 1000
+            )
+          ) {
+            setSubscription(sub);
+            setIsSubscribed(true);
+          }
+        });
+        setRegistration(reg);
+      });
+    }
+  }, []);
+
+  const subscribeButtonOnClick = async (event) => {
+    event.preventDefault();
+    const sub = await registration.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: base64ToUint8Array(
+        process.env.NEXT_PUBLIC_WEB_PUSH_PUBLIC_KEY
+      ),
+    });
+    // TODO: you should call your API to save subscription data on server in order to send web push notification from server
+    setSubscription(sub);
+    setIsSubscribed(true);
+    console.log("web push subscribed!");
+    console.log(sub);
+  };
+
+  const unsubscribeButtonOnClick = async (event) => {
+    event.preventDefault();
+    await subscription.unsubscribe();
+    // TODO: you should call your API to delete or invalidate subscription data on server
+    setSubscription(null);
+    setIsSubscribed(false);
+    console.log("web push unsubscribed!");
+  };
+
+  const sendNotificationButtonOnClick = async (event) => {
+    event.preventDefault();
+    if (subscription == null) {
+      console.error("web push not subscribed");
+      return;
+    }
+
+    await fetch("/api/notification", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        subscription,
+      }),
+    });
+  };
 
   return (
     <>
       <Head>
-        <title>Share</title>
-        <meta name="theme-color" content="#C4B5B5" />
+        <title>next-pwa example</title>
       </Head>
-      <Box
-        sx={{
-          position: "fixed",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100%",
-          background: "#C4B5B5",
-          display: "flex",
-          alignItems: "center",
-        }}
-      >
-        <Box
-          sx={{
-            display: "flex",
-            p: 3,
-            alignItems: "center",
-            gap: "20px",
-            position: "fixed",
-            top: 0,
-            left: 0,
-            userSelect: "none",
-          }}
-        >
-          <picture>
-            <img
-              width="90"
-              draggable="false"
-              height="90"
-              src="https://cdn.jsdelivr.net/gh/Smartlist-App/Assets@master/v2/rounded.png"
-              alt="Carbon"
-            />
-          </picture>
-          <Typography variant="h4" sx={{ color: "#232323" }}>
-            Carbon
-          </Typography>
-        </Box>
-        <Box
-          sx={{
-            p: 4,
-            width: "100%",
-            pt: 8,
-            "& *::selection": {
-              background: "#6B4B4B",
-              color: "#fff",
-            },
-          }}
-        >
-          {query ? (
-            <Query query={query} />
-          ) : (
-            <>
-              <Skeleton
-                variant="rectangular"
-                animation="wave"
-                sx={{ width: "90%", height: "60px", borderRadius: 5 }}
-              />
-              <Skeleton
-                variant="rectangular"
-                animation="wave"
-                sx={{ width: "30%", height: "60px", borderRadius: 5, mt: 3 }}
-              />
-            </>
-          )}
-          <Button
-            variant="text"
-            size="small"
-            href="//smartlist.tech"
-            target="_blank"
-            sx={{
-              boxShadow: 0,
-              textTransform: "none",
-              mt: 4,
-              color: "#6B4B4B!important",
-            }}
-          >
-            Track your inventory for free with a Carbon account
-          </Button>
-        </Box>
-      </Box>
+      <h1>Next.js + PWA = AWESOME!</h1>
+      <button onClick={subscribeButtonOnClick} disabled={isSubscribed}>
+        Subscribe
+      </button>
+      <button onClick={unsubscribeButtonOnClick} disabled={!isSubscribed}>
+        Unsubscribe
+      </button>
+      <button onClick={sendNotificationButtonOnClick} disabled={!isSubscribed}>
+        Send Notification
+      </button>
     </>
   );
-}
+};
+
+export default Index;
