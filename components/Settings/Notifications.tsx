ce200db6dcbbe47e64f6250d211063ed3c466a8f
manuthecoder <manuthecoder@protonmail.com>
2022-10-15 01:05:53 +0000

    update Notifications.tsx, [index].tsx and worker/index.js

--- components/Settings/Notifications.tsx
+++ components/Settings/Notifications.tsx
@@ -1,21 +1,106 @@
+import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
+import { useEffect, useState } from "react";
 
+const base64ToUint8Array = (base64) => {
+  const padding = "=".repeat((4 - (base64.length % 4)) % 4);
+  const b64 = (base64 + padding).replace(/-/g, "+").replace(/_/g, "/");
+
+  const rawData = window.atob(b64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+};
 /**
  * Top-level component for the notification settings page.
  */
 export default function Notifications() {
+  const [isSubscribed, setIsSubscribed] = useState(false);
+  const [subscription, setSubscription] = useState<any>(null);
+  const [registration, setRegistration] = useState<any>(null);
+
+  useEffect(() => {
+    if (
+      typeof window !== "undefined" &&
+      "serviceWorker" in navigator &&
+      window.workbox !== undefined
+    ) {
+      // run only in browser
+      navigator.serviceWorker.ready.then((reg) => {
+        reg.pushManager.getSubscription().then((sub) => {
+          if (
+            sub &&
+            !(
+              sub.expirationTime &&
+              Date.now() > sub.expirationTime - 5 * 60 * 1000
+            )
+          ) {
+            setSubscription(sub);
+            setIsSubscribed(true);
+          }
+        });
+        setRegistration(reg);
+      });
+    }
+  }, []);
+
+  const subscribeButtonOnClick = async (event) => {
+    event.preventDefault();
+    const sub = await registration.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: base64ToUint8Array(
+        process.env.NEXT_PUBLIC_WEB_PUSH_PUBLIC_KEY
+      ),
+    });
+    // TODO: you should call your API to save subscription data on server in order to send web push notification from server
+    setSubscription(sub);
+    setIsSubscribed(true);
+    console.log("web push subscribed!");
+
+    // return sub
+  };
+
+  const unsubscribeButtonOnClick = async (event) => {
+    event.preventDefault();
+    await subscription.unsubscribe();
+    // TODO: you should call your API to delete or invalidate subscription data on server
+    setSubscription(null);
+    setIsSubscribed(false);
+    console.log("web push unsubscribed!");
+  };
+
+  const sendNotificationButtonOnClick = async (event) => {
+    event.preventDefault();
+    if (subscription == null) {
+      console.error("web push not subscribed");
+      return;
+    }
+
+    fetch("/api/notification", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        subscription,
+      }),
+    });
+  };
+
   return (
-    <Box
-      sx={{
-        display: "flex",
-        mt: -5,
-        justifyContent: "center",
-        height: "100%",
-        alignItems: "center",
-        px: 10,
-      }}
-    >
-      <div className="onesignal-customlink-container" />
+    <Box sx={{ mb: 2 }}>
+      <Button onClick={subscribeButtonOnClick} disabled={isSubscribed}>
+        Subscribe
+      </Button>
+      <Button onClick={unsubscribeButtonOnClick} disabled={!isSubscribed}>
+        Unsubscribe
+      </Button>
+      <Button onClick={sendNotificationButtonOnClick} disabled={!isSubscribed}>
+        Send Notification
+      </Button>
     </Box>
   );
 }
