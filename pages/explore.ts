2b21b08e1b38002bf54354e487cc76c6896d825b
manuthecoder <manuthecoder@protonmail.com>
2022-10-22 05:05:14 +0000

    feat: create explore.ts

---
+++ pages/explore.ts
@@ -0,0 +1,15 @@
+import Box from "@mui/material/Box";
+
+export default function Explore() {
+  return (
+    <Box
+      sx={{
+        p: 5,
+        background: "rgba(200,200,200,.3)",
+        borderRadius: 5,
+      }}
+    >
+      Coming soon!
+    </Box>
+  );
+}
