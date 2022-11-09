2786719f79d199de85d8f223b8c202720bd3202c
manuthecoder <manuthecoder@protonmail.com>
2022-11-09 18:12:57 +0000

    update Navbar.tsx and Lists.tsx

--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -270,6 +270,7 @@ export function Navbar(): JSX.Element {
       elevation={0}
       position="fixed"
       sx={{
+        zIndex: 999,
         boxShadow: trigger
           ? "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)"
           : "none",
