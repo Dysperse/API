1895486f7543e38220f8fd38b07f352fc6c0034d
_manu.codes <manuthecoder@protonmail.com>
2022-11-17 07:55:57 +0000

    update Task.tsx

--- components/Boards/Task.tsx
+++ components/Boards/Task.tsx
@@ -31,8 +31,7 @@ export const BpIcon = styled("span")(({ theme }) => ({
   "input:disabled ~ &": {
     // boxShadow: "none",
     background: "transparent",
-
-    backgroundImage: `url("data:image/svg+xml,%0A%3Csvg xmlns='http://www.w3.org/2000/svg' width='15' height='15' viewBox='0 0 24 24' fill='none' stroke='%23303030' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='feather feather-plus'%3E%3Cline x1='12' y1='5' x2='12' y2='19'%3E%3C/line%3E%3Cline x1='5' y1='12' x2='19' y2='12'%3E%3C/line%3E%3C/svg%3E");`,
+    backgroundImage: `url("data:image/svg+xml,%0A%3Csvg xmlns='http://www.w3.org/2000/svg' width='15' height='15' viewBox='0 0 24 24' fill='none' stroke='%23606060' stroke-width='3' stroke-linecap='round' stroke-linejoin='round' class='feather feather-plus'%3E%3Cline x1='12' y1='5' x2='12' y2='19'%3E%3C/line%3E%3Cline x1='5' y1='12' x2='19' y2='12'%3E%3C/line%3E%3C/svg%3E");`,
     backgroundRepeat: "no-repeat",
     backgroundPosition: "center",
   },
