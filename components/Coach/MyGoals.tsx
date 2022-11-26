339facc4d100e60556d9e815687fbbbd7231ea42
_manu.codes <manuthecoder@protonmail.com>
2022-11-26 19:47:10 +0000

    update MyGoals.tsx

--- components/Coach/MyGoals.tsx
+++ components/Coach/MyGoals.tsx
@@ -4,7 +4,7 @@ import { ExploreGoals } from "./ExploreGoals";
 
 export function MyGoals(): JSX.Element {
   return (
-    <div>
+    <>
       <div
         className="p-4 rounded-2xl flex items-center max-w-xl select-none cursor-pointer"
         style={{
@@ -13,15 +13,15 @@ export function MyGoals(): JSX.Element {
         }}
       >
         <div>
-          <Typography className="font-secondary font-bold">
+          <h3 className="font-secondary font-bold">
             Finish today&apos;s goals
-          </Typography>
-          <Typography variant="body2">7 tasks remaining</Typography>
+          </h3>
+          <h4 className="font-sm font-light">7 tasks remaining</h4>
         </div>
         <span className="material-symbols-rounded ml-auto">arrow_forward</span>
       </div>
 
       <ExploreGoals />
-    </div>
+    </>
   );
 }
