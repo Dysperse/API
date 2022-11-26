dfe6d79c4d3e69a2f1fe8b064570095a139f7ff0
_manu.codes <manuthecoder@protonmail.com>
2022-11-26 19:58:09 +0000

    update MyGoals.tsx

--- components/Coach/MyGoals.tsx
+++ components/Coach/MyGoals.tsx
@@ -1,17 +1,13 @@
 import { colors } from "../../lib/colors";
 import { ExploreGoals } from "./ExploreGoals";
-import Box from "@mui/material/Box";
 
 export function MyGoals(): JSX.Element {
   return (
     <>
-      <Box
-        className="p-4 rounded-2xl flex items-center max-w-xl select-none cursor-pointer"
-        sx={{
+      <div
+        className="p-4 rounded-2xl flex items-center max-w-xl select-none cursor-pointer active:scale-[.98] transition-transform active:transition-[0s]"
+        style={{
           background: colors[themeColor][50],
-          "&:hover": {
-            background: colors[themeColor][100],
-          },
           color: colors[themeColor][900],
         }}
       >
@@ -20,7 +16,7 @@ export function MyGoals(): JSX.Element {
           <h4 className="font-sm font-light">7 tasks remaining</h4>
         </div>
         <span className="material-symbols-rounded ml-auto">arrow_forward</span>
-      </Box>
+      </div>
 
       <ExploreGoals />
     </>
