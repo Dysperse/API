c37a2152e3e5670f9f94c8e439eec441157ea595
_manu.codes <manuthecoder@protonmail.com>
2022-11-27 00:19:01 +0000

    update MyGoals.tsx

--- components/Coach/MyGoals.tsx
+++ components/Coach/MyGoals.tsx
@@ -48,51 +48,54 @@ export function MyGoals(): JSX.Element {
               </h3>
             </div>
           )}
-          <Masonry columns={{ xs: 1, sm: 2, md: 3 }} spacing={0}>
-            {data.length == 0 && (
-              <div className="text-gray-50 w-full bg-gray-900 rounded-xl p-3 px-5 mb-4">
-                You haven&apos;t created any goals yet.
-              </div>
-            )}
-            {[
-              ...data.filter((item) => item.tasks === item.completed),
-              ...data.filter((item) => item.tasks !== item.completed),
-            ].map((goal) => (
-              <div className="bg-gray-100 rounded-3xl mb-3 sm:border-8 border-white overflow-hidden">
-                <div className="relative">
-                  <div
-                    className="bg-gray-400 absolute h-full top-0 left-0 pointer-events-none opacity-20"
-                    style={{
-                      width: (goal.completed / goal.tasks) * 100 + "%",
-                      ...(goal.completed === goal.tasks && {
-                        background: colors.green[500],
-                      }),
-                    }}
-                  />
-                  <div className="p-2 px-3">
-                    <h3 className="font-secondary font-bold mt-2">
-                      {goal.name}
-                    </h3>
-                    <h3 className="font-thin mt-1">
-                      {goal.description ?? "No description"}
-                    </h3>
-                    <div className="flex items-center mt-2 gap-5">
-                      <div className="flex items-center mt-2 gap-2">
-                        <span className="material-symbols-outlined">
-                          check_circle
-                        </span>
-                        5/10
-                      </div>
-                      <div className="flex items-center mt-2 gap-2">
-                        <span className="material-symbols-outlined">sunny</span>
-                        Every morning
+          {data.length == 0 ? (
+            <div className="text-gray-50 w-full bg-gray-900 rounded-xl p-3 px-5 mb-4">
+              You haven&apos;t created any goals yet.
+            </div>
+          ) : (
+            <Masonry columns={{ xs: 1, sm: 2, md: 3 }} spacing={0}>
+              {[
+                ...data.filter((item) => item.tasks === item.completed),
+                ...data.filter((item) => item.tasks !== item.completed),
+              ].map((goal) => (
+                <div className="bg-gray-100 rounded-3xl mb-3 sm:border-8 border-white overflow-hidden">
+                  <div className="relative">
+                    <div
+                      className="bg-gray-400 absolute h-full top-0 left-0 pointer-events-none opacity-20"
+                      style={{
+                        width: (goal.completed / goal.tasks) * 100 + "%",
+                        ...(goal.completed === goal.tasks && {
+                          background: colors.green[500],
+                        }),
+                      }}
+                    />
+                    <div className="p-2 px-3">
+                      <h3 className="font-secondary font-bold mt-2">
+                        {goal.name}
+                      </h3>
+                      <h3 className="font-thin mt-1">
+                        {goal.description ?? "No description"}
+                      </h3>
+                      <div className="flex items-center mt-2 gap-5">
+                        <div className="flex items-center mt-2 gap-2">
+                          <span className="material-symbols-outlined">
+                            check_circle
+                          </span>
+                          5/10
+                        </div>
+                        <div className="flex items-center mt-2 gap-2">
+                          <span className="material-symbols-outlined">
+                            sunny
+                          </span>
+                          Every morning
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            ))}
-          </Masonry>
+              ))}
+            </Masonry>
+          )}
         </>
       ) : error ? (
         <ErrorHandler error="An error occured while trying to fetch your routines" />
