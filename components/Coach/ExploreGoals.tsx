06725e8f7e3621461add206e5ab8d72b739e3835
_manu.codes <manuthecoder@protonmail.com>
2022-12-11 10:57:30 -0800

    Update ExploreGoals.tsx

--- components/Coach/ExploreGoals.tsx
+++ components/Coach/ExploreGoals.tsx
@@ -218,7 +218,7 @@ export function ExploreGoals({ setOpen, mutationUrl }) {
           color: "#000",
           p: 4,
           mt: "-64px",
-          minHeight: "300px",
+          minHeight: "350px",
           display: "flex",
         }}
       >
