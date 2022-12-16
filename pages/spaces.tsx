bcc29a54d8a474cc1dcf64f93cb85513cbdaf4c0
Manu <manusvathgurudath@gmail.com>
2022-12-16 19:45:25 +0000

    W

--- pages/spaces.tsx
+++ pages/spaces.tsx
@@ -96,7 +96,7 @@ function LinkModal({ value, setValue }) {
                 : colors[themeColor][900]) + "!important",
           }}
           onClick={(e) => {
-            // Check if `link` is a valid URL using regex
+            // Check if `link` is a valid URL using regex!
             if (isValidUrl(link)) {
               setValue(value + link);
               setLink("");
