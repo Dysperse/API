e5504127401c398c47322a0f6e80003e15779b40
Manu <manusvathgurudath@gmail.com>
2022-12-21 02:25:06 +0000

    W

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,11 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./providers/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   darkMode: "class",
   plugins: [],
-}
+};
