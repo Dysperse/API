763e6ba43b64f7705fcd5c7a43c32cfb7832d33e
_manu.codes <manuthecoder@protonmail.com>
2022-12-22 23:56:38 +0000

    W

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,11 +1,20 @@
+import {
+  Box,
+  Button,
+  Grow,
+  Icon,
+  LinearProgress,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
 import React from "react";
+import { colors } from "../../lib/colors";
 import { BottomNav } from "./BottomNavigation";
+import { KeyboardShortcutsModal } from "./KeyboardShortcutsModal";
 import { Navbar } from "./Navbar";
 import { Sidebar } from "./Sidebar";
-import { Box, Grow, Toolbar } from "@mui/material";
-import { KeyboardShortcutsModal } from "./KeyboardShortcutsModal";
 
 const PWAPrompt: any = dynamic(() => import("react-ios-pwa-prompt"), {
   ssr: false,
@@ -17,6 +26,132 @@ export const Transition = React.forwardRef(function Transition(
 ) {
   return <Grow in={props.open} ref={ref} {...props} />;
 });
+
+function DailyTaskSection({
+  title,
+  progress,
+  progressIndicator,
+}: {
+  title: string;
+  progress: number;
+  progressIndicator: string;
+}) {
+  return (
+    <Box
+      sx={{
+        my: 1,
+        cursor: "pointer",
+        "&:hover": {
+          background: "#eee",
+        },
+      }}
+    >
+      <Box
+        sx={{
+          px: 4,
+          py: 1.5,
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <Typography
+          sx={{
+            fontWeight: 600,
+          }}
+        >
+          {title}
+        </Typography>
+        <Typography variant="body2" sx={{ ml: "auto" }}>
+          {progressIndicator}
+        </Typography>
+      </Box>
+      <LinearProgress
+        value={progress}
+        variant="determinate"
+        sx={{ height: 3 }}
+      />
+    </Box>
+  );
+}
+function DailyTasksModal() {
+  const [expanded, setExpanded] = React.useState(false);
+
+  return (
+    <Box
+      className="shadow-2xl"
+      sx={{
+        display: { xs: "none", md: "block" },
+        position: "fixed",
+        bottom: 25,
+        overflow: "hidden",
+        right: 25,
+        zIndex: 999,
+        width: "100%",
+        maxWidth: "350px",
+        borderRadius: 3,
+        background: "#fff",
+        border: "1px solid #ccc",
+        color: colors[themeColor][900],
+      }}
+    >
+      <Box
+        onClick={() => {
+          setExpanded(!expanded);
+        }}
+      >
+        <Button
+          fullWidth
+          size="large"
+          sx={{
+            px: 2,
+            py: 1.5,
+            ...(expanded && {
+              background: "#eee !important",
+            }),
+          }}
+          disableRipple
+          color="inherit"
+        >
+          Today&apos;s tasks
+          <Icon sx={{ ml: "auto" }}>
+            {!expanded ? "expand_less" : "expand_more"}
+          </Icon>
+        </Button>
+        <LinearProgress
+          variant="determinate"
+          value={expanded ? 100 : 50}
+          sx={{
+            opacity: expanded ? 0.2 : 1,
+            height: expanded ? 1.5 : 3,
+            background: colors[themeColor][100],
+            "& .MuiLinearProgress-bar": {
+              transition: "none!important",
+              background: colors[themeColor][900],
+              strokeLinecap: "round",
+            },
+          }}
+        />
+      </Box>
+      <Box
+        sx={{
+          display: expanded ? "block" : "none",
+          animation: "completedTasks .2s forwards",
+        }}
+      >
+        <DailyTaskSection
+          title="Routines"
+          progress={75}
+          progressIndicator={"4 goals left"}
+        />
+        <DailyTaskSection
+          title="Daily task goal"
+          progress={50}
+          progressIndicator={"3/5 "}
+        />
+      </Box>
+    </Box>
+  );
+}
 
 /**
  * Drawer component
@@ -44,6 +179,7 @@ function ResponsiveDrawer({
       )}
       <Navbar />
       <KeyboardShortcutsModal />
+      <DailyTasksModal />
       <Box
         sx={{
           width: { md: "85px" },
