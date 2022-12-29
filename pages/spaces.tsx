5b76cc500ed0336a21c6bb0147333ae6e5ae2881
_manu.codes <manuthecoder@protonmail.com>
2022-12-28 19:36:45 -0800

    Update spaces.tsx

--- pages/spaces.tsx
+++ pages/spaces.tsx
@@ -947,24 +947,5 @@ export default function Spaces() {
         </Box>
       )}
     </Box>
-  ) : (
-    <div className="bg-gray-900 px-7 py-5 rounded-xl shadow-xl mt-10 max-w-lg mx-auto text-gray-50">
-      <h1 className="text-xl">Gamify your productivity</h1>
-      <h2 className="text-md mt-2 font-thin">
-        Carbon Spaces is a place to store links, images, thoughts and more. With
-        Spaces, you can organize your thoughts and ideas into a single place and
-        access them from anywhere. You can also allow family/dorm members to see
-        your posts!
-      </h2>
-      <Button
-        className="text-gray-50 border-gray-50 border-2 mt-5 w-full hover:border-2 hover:border-gray-100"
-        variant="outlined"
-        onClick={() => {
-          toast.success("You'll get an email when Spaces is released!");
-        }}
-      >
-        Join the waitlist
-      </Button>
-    </div>
-  );
+  )
 }
