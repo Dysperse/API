963fc92d0a17c7187c3525ffc1e065bf06dd4116
Manu <manusvathgurudath@gmail.com>
2023-01-11 03:14:02 +0000

    W

--- components/Layout/Achievements.tsx
+++ components/Layout/Achievements.tsx
@@ -1,6 +1,7 @@
 import {
   AppBar,
   Box,
+  Chip,
   Icon,
   IconButton,
   SwipeableDrawer,
@@ -10,8 +11,11 @@ import {
 } from "@mui/material";
 import { useState } from "react";
 
+import { achievements } from "./achievements";
+
 export function Achievements({ styles }) {
   const [open, setOpen] = useState(false);
+  const availableAchievements = achievements;
 
   return (
     <>
@@ -40,7 +44,7 @@ export function Achievements({ styles }) {
             background: "linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,0))",
           }}
         >
-          <Toolbar className="flex" sx={{ height: "70px" }}>
+          <Toolbar className="flex" sx={{ height: "var(--navbar-height)" }}>
             <IconButton
               color="inherit"
               disableRipple
@@ -60,63 +64,112 @@ export function Achievements({ styles }) {
             </IconButton>
           </Toolbar>
         </AppBar>
-        <Box
-          sx={{
-            position: "sticky",
-            mt: "-70px",
-            height: "350px",
-          }}
-        >
-          <picture>
-            <img
-              src={
-                global.user.darkMode
-                  ? "/images/stats-banner.png"
-                  : "https://cdn.dribbble.com/users/1731254/screenshots/11649852/nature_gradients_illustration_tubikarts_4x.png?resize=1400x400&compress=1"
-              }
-              alt="Achievement banner"
-              style={{
-                height: "100%",
-                width: "100%",
-                position: "absolute",
-                top: 0,
-                opacity: ".5",
-                left: 0,
-                objectFit: "cover",
-                zIndex: -1,
-              }}
-            />
-          </picture>
+        <Box>
           <Box
             sx={{
-              position: "absolute",
-              bottom: 0,
-              left: 0,
-              zIndex: 1,
-              p: 4,
+              position: "relative",
+              display: "block",
+              height: "350px",
+              mt: "calc(var(--navbar-height) * -1)",
             }}
           >
-            <Typography
-              variant="h4"
+            <picture>
+              <img
+                src={
+                  global.user.darkMode
+                    ? "/images/stats-banner.png"
+                    : "https://cdn.dribbble.com/users/1731254/screenshots/11649852/nature_gradients_illustration_tubikarts_4x.png?resize=1400x400&compress=1"
+                }
+                alt="Achievement banner"
+                style={{
+                  height: "100%",
+                  width: "100%",
+                  position: "absolute",
+                  top: 0,
+                  opacity: ".5",
+                  left: 0,
+                  objectFit: "cover",
+                  zIndex: -1,
+                }}
+              />
+            </picture>
+            <Box
               sx={{
-                mb: 2,
-                textDecoration: "underline",
-                fontWeight: "600",
+                position: "absolute",
+                bottom: 0,
+                left: 0,
+                zIndex: 1,
+                p: 4,
               }}
             >
-              Achievements coming soon!
+              <Typography
+                variant="h4"
+                sx={{
+                  mb: 2,
+                  textDecoration: "underline",
+                  fontWeight: "600",
+                }}
+              >
+                Achievements coming soon!
+              </Typography>
+              <Typography variant="body1">
+                Earn badges &amp; compare achievement by completing tasks,
+                achieving goals, and more!
+              </Typography>
+            </Box>
+          </Box>
+          <Box
+            sx={{
+              p: 5,
+            }}
+          >
+            <Typography variant="body2" sx={{ opacity: 0.7, mb: 3 }}>
+              TO BE UNLOCKED
             </Typography>
-            <Typography variant="body1">
-              Earn badges &amp; compare achievement by completing tasks,
-              achieving goals, and more!
-            </Typography>
+            {availableAchievements.map((achievement: any) => (
+              <Box
+                sx={{
+                  p: 2,
+                  px: 3,
+                  background: global.user.darkMode
+                    ? "hsl(240,11%,20%)"
+                    : "rgba(200,200,200,.3)",
+                  borderRadius: 5,
+                  mb: 2,
+                }}
+              >
+                <Typography
+                  sx={{
+                    fontWeight: "700",
+                    textDecoration: "underline",
+                  }}
+                >
+                  {achievement.name}
+                </Typography>
+                <Typography
+                  variant="body2"
+                  sx={{
+                    opacity: 0.8,
+                    mb: 1,
+                    mt: 0.5,
+                  }}
+                >
+                  {achievement.description}
+                </Typography>
+                <Chip
+                  label={achievement.type.split(":")[0]}
+                  sx={{
+                    textTransform: "capitalize",
+                    background: global.user.darkMode
+                      ? "hsl(240,11%,30%)"
+                      : "rgba(200,200,200,.3)",
+                  }}
+                  size="small"
+                />
+              </Box>
+            ))}
           </Box>
         </Box>
-        <Box
-          sx={{
-            p: 5,
-          }}
-        ></Box>
       </SwipeableDrawer>
       <Tooltip title="Achievements">
         <IconButton
---
+++ components/Layout/achievements.ts
@@ -0,0 +1,75 @@
+export const achievements = [
+  {
+    name: "Self-care Seeker",
+    description: "Achieve mental health goals",
+    type: "goal:mental health",
+    requirementsByLevel: [1, 5, 10],
+  },
+  {
+    name: "Efficiency Expert",
+    description: "Complete 10 tasks in a day",
+    type: "task",
+    requirementsByLevel: [10],
+  },
+  {
+    name: "Habit Hero",
+    description: "Break habits",
+    type: "goal:habit",
+    requirementsByLevel: [1, 3, 5],
+  },
+  {
+    name: "Dark Mode Enthusiast",
+    description: "Enable dark mode",
+    type: "feature:dark mode",
+  },
+  {
+    name: "Inventory Master",
+    description: "Add items to your home",
+    type: "feature:inventory tracking",
+    requirementsByLevel: [1, 5, 10, 25, 50],
+  },
+  {
+    name: "Goal Getter",
+    description: "Set and achieve goals",
+    type: "feature:goal setting",
+    requirementsByLevel: [1, 2, 3],
+  },
+  {
+    name: "Kanban King",
+    description: "Complete a project using a Kanban board",
+    type: "feature:kanban boards",
+  },
+  {
+    name: "Checklist Champion",
+    description: "Create and complete a checklist",
+    type: "feature:checklists",
+  },
+  {
+    name: "Memo Master",
+    description: "Write memos in spaces",
+    type: "feature:spaces",
+    requirementsByLevel: [1, 3, 5],
+  },
+  {
+    name: "Team Builder",
+    description: "Invite 3 members to your home",
+    type: "feature:invite members",
+    requirementsByLevel: [1, 2, 3],
+  },
+  {
+    name: "Designer",
+    description: "Customize the theme color of your dashboard",
+    type: "feature:customize theme",
+  },
+  {
+    name: "Security Savvy",
+    description: "Enable 2FA for your account",
+    type: "feature:2FA",
+  },
+  {
+    name: "Productivity Pro",
+    description: "Complete 15 tasks in a day",
+    type: "feature:productivity",
+    requirementsByLevel: [15, 30, 45],
+  },
+];
