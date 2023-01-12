c84ec175cf736fa4f5ffc2745458c5c5cd8675e9
Manu <manusvathgurudath@gmail.com>
2023-01-12 03:02:25 +0000

    W

--- components/Rooms/CreateItem/modal.tsx
+++ components/Rooms/CreateItem/modal.tsx
@@ -37,7 +37,7 @@ function shuffle(
     randomIndex;
 
   // While there remain elements to shuffle.
-  while (currentIndex != 0) {
+  while (currentIndex !== 0) {
     // Pick a remaining element.
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
