2d1d0a33f6f9666aae74bd0a3b59d5a13d040117
Manu <manusvathgurudath@gmail.com>
2023-01-21 22:27:13 +0000

    W

--- components/Boards/Board/Column/Task/index.tsx
+++ components/Boards/Board/Column/Task/index.tsx
@@ -255,7 +255,7 @@ export const Task = React.memo(function Task({
         <ListItem
           onClick={() => setOpen(true)}
           onContextMenu={handleContextMenu}
-          className="p-1 sm:p-0 shadow-sm border border-gray-100 dark:border-[hsl(240,11%,18%)] hover:border-gray-300 rounded-xl gap-0.5 dark:bg-transparent hover:bg-gray-100 sm:hover:bg-gray-200 active:bg-gray-300 cursor-auto select-none"
+          className="p-1 sm:p-0 shadow-sm border border-gray-100 dark:border-[hsl(240,11%,18%)] hover:border-gray-300 active:border-gray-300 rounded-xl gap-0.5 dark:bg-transparent hover:bg-gray-100 sm:hover:bg-gray-200 active:bg-gray-200 sm:active:bg-gray-200 cursor-auto select-none"
           sx={{
             color: colors[task.color][global.user.darkMode ? "A100" : "A700"],
             p: 1,
