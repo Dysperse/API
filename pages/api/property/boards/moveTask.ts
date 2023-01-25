c6b18d7ca5821b013605e61e5c4505740f9d23dd
_manu.codes <manuthecoder@protonmail.com>
2023-01-25 10:04:25 +0000

    W

---
+++ pages/api/property/boards/moveTask.ts
@@ -0,0 +1,34 @@
+import { prisma } from "../../../../lib/prismaClient";
+import { validatePermissions } from "../../../../lib/validatePermissions";
+/**
+ * API handler
+ * @param {any} req
+ * @param {any} res
+ * @returns {any}
+ */
+const handler = async (req, res) => {
+  const permissions = await validatePermissions(
+    req.query.property,
+    req.query.accessToken
+  );
+  if (!permissions) {
+    res.status(401).json({ error: "Unauthorized" });
+    return;
+  }
+
+  const data = await prisma.task.update({
+    data: {
+      columnId: parseInt(req.query.columnId),
+    },
+    where: {
+      id: parseInt(req.query.id),
+    },
+  });
+
+  res.json({
+    data,
+    success: true,
+  });
+};
+
+export default handler;
