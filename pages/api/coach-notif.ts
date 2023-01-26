0a95d8b8b8c1297660c1034956ca1bfae9a76d5b
_manu.codes <manuthecoder@protonmail.com>
2023-01-26 19:20:23 +0000

    W

--- pages/api/coach-notif.ts
+++ pages/api/coach-notif.ts
@@ -1,66 +1,69 @@
 const webPush = require("web-push");
+import { DispatchNotification } from "../../lib/notification";
 import { prisma } from "../../lib/prismaClient";
 
 const Notification = async (req, res) => {
-  if (req.method === "POST") {
-    if (
-      req.headers.authorization !== `Bearer ${process.env.COACH_CRON_API_KEY}`
-    ) {
-      res.status(401).json({
-        currentHeaders: req.headers.authorization,
-        error: "Unauthorized",
-      });
-      return;
-    }
-    // Select user's push notification subscription URL, also asking one of their incompleted goals.
-    let users = await prisma.user.findMany({
-      select: {
-        notificationSubscription: true,
-        RoutineItem: {
-          select: {
-            completed: true,
-          },
-          where: {
-            completed: true,
-          },
-          take: 1,
-        },
-      },
+  // if (req.method === "POST" || process.env.NODE_ENV !== "production") {
+  if (
+    req.headers.authorization !== `Bearer ${process.env.COACH_CRON_API_KEY}` &&
+    process.env.NODE_ENV === "production"
+  ) {
+    res.status(401).json({
+      currentHeaders: req.headers.authorization,
+      error: "Unauthorized",
     });
-
-    // Make sure that user actually has goals (which aren't completed!)
-    users = users.filter((user) => user.RoutineItem.length === 1);
-
-    webPush.setVapidDetails(
-      `mailto:${process.env.WEB_PUSH_EMAIL}`,
-      process.env.NEXT_PUBLIC_WEB_PUSH_PUBLIC_KEY,
-      process.env.WEB_PUSH_PRIVATE_KEY
-    );
-
-    // For each user
-    for (let i = 0; i < users.length; i++) {
-      const user = users[i];
-      const { notificationSubscription }: any = user;
-      //   Send notification
-      webPush
-        .sendNotification(
-          JSON.parse(notificationSubscription) as any,
-          JSON.stringify({
-            title: "Let's work on your goals!",
-            body: "Tap to start your daily routine",
-            actions: [
-              {
-                title: "⚡ Start my daily routine",
-                action: "startDailyRoutine",
-              },
-            ],
-          })
-        )
-        .catch((err) => console.log(err));
-    }
-
-    res.status(200).json({ message: "Notification sent" });
+    return;
   }
+  // Select user's push notification subscription URL, also asking one of their incompleted goals.
+  let users = await prisma.user.findMany({
+    select: {
+      notificationSubscription: true,
+      RoutineItem: {
+        select: {
+          completed: true,
+        },
+        where: {
+          completed: true,
+        },
+        take: 1,
+      },
+    },
+  });
+
+  // Make sure that user actually has goals (which aren't completed!)
+  users = users.filter((user) => user.RoutineItem.length === 1);
+
+  webPush.setVapidDetails(
+    `mailto:${process.env.WEB_PUSH_EMAIL}`,
+    process.env.NEXT_PUBLIC_WEB_PUSH_PUBLIC_KEY,
+    process.env.WEB_PUSH_PRIVATE_KEY
+  );
+
+  // For each user
+  for (let i = 0; i < users.length; i++) {
+    const user = users[i];
+    const { notificationSubscription }: any = user;
+    //   Send notification
+    try {
+      await DispatchNotification({
+        title: "Let's work on your goals!",
+        body: "Tap to start your daily routine",
+        actions: [
+          {
+            title: "⚡ Start my daily routine",
+            action: "startDailyRoutine",
+          },
+        ],
+        subscription: notificationSubscription,
+      });
+    } catch (error) {
+      // If there's an error, log it
+      console.log(error);
+    }
+  }
+
+  res.status(200).json({ message: "Notification sent" });
+  // }
 };
 
 export default Notification;
