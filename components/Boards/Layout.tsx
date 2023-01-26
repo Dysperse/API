77d054c223ab6016b6b6a7ce3fa8e3923d71bcf0
_manu.codes <manuthecoder@protonmail.com>
2023-01-26 01:16:31 +0000

    W

--- components/Boards/Layout.tsx
+++ components/Boards/Layout.tsx
@@ -3,18 +3,14 @@ import {
   Button,
   CircularProgress,
   Icon,
-  Menu,
-  MenuItem,
-  SwipeableDrawer
+  SwipeableDrawer,
 } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import toast from "react-hot-toast";
-import { mutate } from "swr";
-import { fetchApiWithoutHook, useApi } from "../../hooks/useApi";
+import { useApi } from "../../hooks/useApi";
 import { useStatusBar } from "../../hooks/useStatusBar";
 import { colors } from "../../lib/colors";
 import { toastStyles } from "../../lib/useCustomTheme";
-import { ConfirmationModal } from "../ConfirmationModal";
 import { ErrorHandler } from "../Error";
 import { Puller } from "../Puller";
 import { Board } from "./Board/Board";
@@ -46,80 +42,21 @@ function Loading() {
 }
 
 const Tab = React.memo(function Tab({
-  mutationUrl,
   styles,
   activeTab,
   setDrawerOpen,
   setActiveTab,
   board,
 }: any) {
-  const [editMode, setEditMode] = useState(false);
-  const [title, setTitle] = useState(board.name);
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    if (!editMode) event.preventDefault();
-    setAnchorEl(event.currentTarget);
-    navigator.vibrate(50);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
   return (
     <div>
-      <Menu
-        id="basic-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        MenuListProps={{
-          "aria-labelledby": "basic-button",
-        }}
-      >
-        <MenuItem
-          onClick={() => {
-            handleClose();
-            setEditMode(true);
-            setTimeout(() => {
-              document.getElementById(`renameInput${board.id}`)?.focus();
-            }, 100);
-          }}
-        >
-          <Icon>edit</Icon>
-          Rename
-        </MenuItem>
-        <ConfirmationModal
-          title="Delete board?"
-          question="Are you sure you want to delete this board? This action annot be undone"
-          callback={async () => {
-            await fetchApiWithoutHook("property/boards/deleteBoard", {
-              id: board.id,
-            });
-            await mutate(mutationUrl);
-            handleClose();
-          }}
-        >
-          <MenuItem
-            sx={{
-              display: "flex",
-              minWidth: "100px",
-            }}
-          >
-            <Icon>delete</Icon>
-            Delete board
-          </MenuItem>
-        </ConfirmationModal>
-      </Menu>
       <Button
         size="large"
         disableRipple
-        onContextMenu={handleClick}
         onClick={() => {
           setDrawerOpen(false);
           window.location.hash = board.id;
-          if (!(activeTab === board.id && !editMode)) {
-            setActiveTab(board.id);
-          }
+          setActiveTab(board.id);
         }}
         sx={{
           ...styles(activeTab === board.id),
@@ -129,84 +66,37 @@ const Tab = React.memo(function Tab({
             }),
         }}
       >
-        {!editMode ? (
-          <Box
-            sx={{
-              textAlign: "left",
-              display: "flex",
-              alignItems: "center",
-              width: "100%",
-              gap: 1,
+        <Box
+          sx={{
+            textAlign: "left",
+            display: "flex",
+            alignItems: "center",
+            width: "100%",
+            gap: 1,
+          }}
+        >
+          <Icon className={activeTab === board.id ? "rounded" : "outlined"}>
+            {board.columns.length === 1 ? "check_circle" : "view_kanban"}
+          </Icon>
+          <span
+            style={{
+              maxWidth: "calc(100% - 25px)",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+              whiteSpace: "nowrap",
             }}
           >
-            <Icon className={activeTab === board.id ? "rounded" : "outlined"}>
-              {board.columns.length === 1 ? "check_circle" : "view_kanban"}
-            </Icon>
-            <span
-              style={{
-                maxWidth: "calc(100% - 25px)",
-                overflow: "hidden",
-                textOverflow: "ellipsis",
-                whiteSpace: "nowrap",
-              }}
+            {board.name}
+          </span>
+          {board.pinned && (
+            <Icon
+              className="outlined"
+              sx={{ ml: "auto", transform: "rotate(-45deg)" }}
             >
-              {board.name}
-            </span>
-            {board.pinned && (
-              <Icon
-                className="outlined"
-                sx={{ ml: "auto", transform: "rotate(-45deg)" }}
-              >
-                push_pin
-              </Icon>
-            )}
-          </Box>
-        ) : (
-          <input
-            id={`renameInput${board.id}`}
-            onBlur={async () => {
-              setEditMode(false);
-              if (title !== board.name && title.trim() !== "") {
-                toast.promise(
-                  fetchApiWithoutHook("property/boards/renameBoard", {
-                    id: board.id,
-                    name: title,
-                  }).then(() => mutate(mutationUrl)),
-                  {
-                    loading: "Renaming...",
-                    success: "Renamed board",
-                    error: "An error occurred while renaming the board",
-                  }
-                );
-              }
-            }}
-            style={{
-              outline: 0,
-              border: 0,
-              background: colors[themeColor][600],
-              boxShadow: `0px 0px 0px 5px ${colors[themeColor][600]}`,
-              fontWeight: "500",
-              fontSize: "15px",
-              borderRadius: 9,
-              color: "#fff",
-              width: `${title.length}ch`,
-              minWidth: "100px",
-            }}
-            placeholder="Board title"
-            value={title}
-            onKeyDown={(e) => {
-              e.stopPropagation();
-              if (e.key === "Enter") {
-                e.currentTarget.blur();
-              }
-              if (e.key === "Escape") {
-                setEditMode(false);
-                setTitle(board.name);
-              }
-            }}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-        )}
+              push_pin
+            </Icon>
+          )}
+        </Box>
       </Button>
     </div>
   );
@@ -311,7 +201,6 @@ export function TasksLayout() {
             activeTab={activeTab}
             board={board}
             setActiveTab={setActiveTab}
-            mutationUrl={url}
           />
         ))}
       <Box
