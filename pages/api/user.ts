9f44d26f898fd00fee0e8bd747f3938afacc2287
_manu.codes <manuthecoder@protonmail.com>
2023-01-27 09:15:51 +0000

    MAJOR SECURITY VULNERABILITY

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -8,23 +8,23 @@ import { getUserData } from "./user/info";
  * @returns {any}
  */
 export const sessionData = async (providedToken) => {
-  const value = cacheData.get(providedToken);
+  // const value = cacheData.get(providedToken);
 
-  if (value) {
-    return value;
-  } else {
-    const { accessToken } = jwt.verify(
-      providedToken,
-      process.env.SECRET_COOKIE_PASSWORD
-    );
-    const hours = 24;
+  // if (value) {
+  // return value;
+  // } else {
+  const { accessToken } = jwt.verify(
+    providedToken,
+    process.env.SECRET_COOKIE_PASSWORD
+  );
+  const hours = 24;
 
-    const token: string = accessToken;
-    const info = await getUserData(token);
+  const token: string = accessToken;
+  const info = await getUserData(token);
 
-    cacheData.put(providedToken, info, hours * 1000 * 60 * 60);
-    return JSON.parse(JSON.stringify(info));
-  }
+  cacheData.put(providedToken, info, hours * 1000 * 60 * 60);
+  return JSON.parse(JSON.stringify(info));
+  // }
 };
 
 /**
