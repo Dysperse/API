52a4fd41d154624109b5d1899be0563179e68cd9
Manu <manusvathgurudath@gmail.com>
2023-01-30 01:43:50 +0000

    W

--- pages/api/auth/change-password.ts
+++ pages/api/auth/change-password.ts
@@ -1,5 +1,5 @@
 import argon2 from "argon2";
-import { prisma } from "../../lib/prismaClient";
+import { prisma } from "../../../lib/prismaClient";
 
 /**
  * API handler for the /api/login endpoint
