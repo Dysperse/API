9daabb7e8635e61c8f40b5fd0536b9f599625e19
Manu <manusvathgurudath@gmail.com>
2023-01-30 07:24:48 +0000

    W

--- pages/api/user/sessions/delete.ts
+++ pages/api/user/sessions/delete.ts
@@ -1,6 +1,24 @@
+import cacheData from "memory-cache";
 import { prisma } from "../../../../lib/prismaClient";
 
 const handler = async (req, res) => {
+  if (!req.query.id) {
+    const sessions = await prisma.session.findMany({
+      where: {
+        user: {
+          identifier: req.query.userIdentifier || "false",
+        },
+      },
+      select: {
+        id: true,
+      },
+    });
+    if (sessions) {
+      for (let i = 0; i < sessions.length; i++) {
+        cacheData.del(sessions[i].id);
+      }
+    }
+  }
   const session = await prisma.session.deleteMany({
     where: {
       ...(!req.query.id
@@ -24,6 +42,10 @@ const handler = async (req, res) => {
     },
   });
 
+  if (req.query.id) {
+    cacheData.del(req.query.id);
+  }
+  cacheData.del(req.query.sessionId);
   res.json(session);
 };
 export default handler;
