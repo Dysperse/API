2127a0aefefbe06b24a4a88b861657270dbe05a3
Manu <manusvathgurudath@gmail.com>
2023-02-02 05:55:10 +0000

    W

--- components/Boards/Board/Column/Task/TaskDrawer.tsx
+++ components/Boards/Board/Column/Task/TaskDrawer.tsx
@@ -315,40 +315,67 @@ export const TaskDrawer = React.memo(function TaskDrawer({
               />
             </Button>
             {view === "Details" && (
-              <HighlightWithinTextarea
-                placeholder={""}
-                value={value}
-                highlight={[
-                  {
-                    highlight: /<items:(.*?):(.*?)>/g,
-                    component: (props) => (
-                      <Tooltip
-                        title={"Linked to item"}
-                        followCursor
-                        onClick={(e) => e.stopPropagation()}
-                        placement="bottom-start"
-                      >
-                        <Chip
-                          label={props.spanText}
-                          size="small"
-                          icon={
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              height="24"
-                              width="24"
-                              fill="currentColor"
-                            >
-                              <path d="M11 17H7q-2.075 0-3.537-1.463Q2 14.075 2 12t1.463-3.538Q4.925 7 7 7h4v2H7q-1.25 0-2.125.875T4 12q0 1.25.875 2.125T7 15h4Zm-3-4v-2h8v2Zm5 4v-2h4q1.25 0 2.125-.875T20 12q0-1.25-.875-2.125T17 9h-4V7h4q2.075 0 3.538 1.462Q22 9.925 22 12q0 2.075-1.462 3.537Q19.075 17 17 17Z" />
-                            </svg>
-                          }
-                        />
-                      </Tooltip>
-                    ),
+              <Box
+                sx={{
+                  color:
+                    colors[taskData.color ?? global.themeColor ?? "brown"][
+                      global.user.darkMode ? 50 : 900
+                    ],
+                  background:
+                    colors[taskData.color ?? global.themeColor ?? "brown"][
+                      global.user.darkMode ? 800 : 100
+                    ],
+                  borderRadius: 5,
+                  p: 2,
+                  mt: 2,
+                  "&:focus-within": {
+                    background:
+                      colors[taskData.color ?? global.themeColor ?? "brown"][
+                        global.user.darkMode ? 800 : 100
+                      ],
+                    boxShadow:
+                      "0px 0px 0px 2px " +
+                      colors[taskData.color ?? global.themeColor ?? "brown"][
+                        global.user.darkMode ? 700 : 900
+                      ],
                   },
-                ]}
-                stripPastedStyles
-                onChange={(e) => setValue(e)}
-              />
+                }}
+              >
+                <HighlightWithinTextarea
+                  placeholder={""}
+                  value={value}
+                  highlight={[
+                    {
+                      highlight: /<items:(.*?):(.*?)>/g,
+                      component: (props) => (
+                        <Tooltip
+                          title={"Linked to item"}
+                          followCursor
+                          onClick={(e) => e.stopPropagation()}
+                          placement="bottom-start"
+                        >
+                          <Chip
+                            label={props.spanText}
+                            size="small"
+                            icon={
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                height="24"
+                                width="24"
+                                fill="currentColor"
+                              >
+                                <path d="M11 17H7q-2.075 0-3.537-1.463Q2 14.075 2 12t1.463-3.538Q4.925 7 7 7h4v2H7q-1.25 0-2.125.875T4 12q0 1.25.875 2.125T7 15h4Zm-3-4v-2h8v2Zm5 4v-2h4q1.25 0 2.125-.875T20 12q0-1.25-.875-2.125T17 9h-4V7h4q2.075 0 3.538 1.462Q22 9.925 22 12q0 2.075-1.462 3.537Q19.075 17 17 17Z" />
+                              </svg>
+                            }
+                          />
+                        </Tooltip>
+                      ),
+                    },
+                  ]}
+                  stripPastedStyles
+                  onChange={(e) => setValue(e)}
+                />
+              </Box>
             )}
           </Box>
         </Box>
