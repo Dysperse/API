35ad605785874ae7391566433199a6716c75ee0b
Manu <manusvathgurudath@gmail.com>
2023-02-13 07:50:07 +0000

    W

--- components/Boards/Board/Column/Task/index.tsx
+++ components/Boards/Board/Column/Task/index.tsx
@@ -1,6 +1,6 @@
 import dayjs from "dayjs";
 import hexToRgba from "hex-to-rgba";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import { fetchApiWithoutHook } from "../../../../../hooks/useApi";
 import { colors } from "../../../../../lib/colors";
@@ -9,22 +9,17 @@ import {
   Box,
   Checkbox,
   Chip,
-  CircularProgress,
   Icon,
   Link,
   ListItem,
   ListItemText,
-  Menu,
-  MenuItem,
   styled,
   Tooltip,
-  Typography,
 } from "@mui/material";
 import { mutate } from "swr";
 import { toastStyles } from "../../../../../lib/useCustomTheme";
 import Item from "../../../../ItemPopup";
 import { ImageViewer } from "./ImageViewer";
-import { SubTask } from "./SubTask";
 import { TaskDrawer } from "./TaskDrawer";
 
 // use whatever you want here
@@ -132,6 +127,7 @@ export const renderDescription = (txt: any) => {
 };
 
 export const Task = function Task({
+  isSubTask = false,
   board,
   isAgenda = false,
   columnId,
@@ -201,328 +197,187 @@ export const Task = function Task({
     },
   });
 
-  const handleDelete = React.useCallback(
-    function handleDelete(taskId) {
-      setTaskData(null);
-      fetchApiWithoutHook("property/boards/column/task/delete", {
-        id: taskId,
-      }).then(() => {
-        mutate(mutationUrl);
-      });
-    },
-    [mutationUrl]
-  );
-
-  const [loading, setLoading] = React.useState(false);
   const [checked, setChecked] = useState(taskData.completed);
-  const [open, setOpen] = useState(false);
-  const [contextMenu, setContextMenu] = React.useState<{
-    mouseX: number;
-    mouseY: number;
-  } | null>(null);
-
-  const handleContextMenu = (event: React.MouseEvent) => {
-    event.preventDefault();
-    setContextMenu(
-      contextMenu === null
-        ? {
-            mouseX: event.clientX + 2,
-            mouseY: event.clientY - 10,
-          }
-        : null
-    );
-  };
-
-  const handleClose = () => {
-    setContextMenu(null);
-  };
-
-  const handlePriorityClick = React.useCallback(async () => {
-    setTaskData((prev) => ({ ...prev, pinned: !prev.pinned }));
-    toast.promise(
-      new Promise(async (resolve, reject) => {
-        try {
-          await fetchApiWithoutHook("property/boards/togglePin", {
-            id: taskData.id,
-            pinned: !taskData.pinned ? "true" : "false",
-          }).then(() => {
-            mutate(mutationUrl);
-          });
-          await mutate(mutationUrl);
-          resolve("");
-        } catch (e) {
-          reject(e);
-        }
-      }),
-      {
-        loading: taskData.pinned
-          ? "Removing important label"
-          : "Marking important...",
-        success: taskData.pinned
-          ? "The priority has been set back to normal"
-          : "Marked as important!",
-        error: "Failed to change priority",
-      },
-      toastStyles
-    );
-  }, [taskData.pinned, taskData.id, mutationUrl]);
 
   return !taskData ? (
     <></>
   ) : (
     <>
-      <Menu
-        open={contextMenu !== null}
-        onClose={handleClose}
-        anchorReference="anchorPosition"
-        anchorPosition={
-          contextMenu !== null
-            ? { top: contextMenu.mouseY, left: contextMenu.mouseX }
-            : undefined
-        }
-      >
-        <MenuItem
-          onClick={handleClose}
+      <TaskDrawer id={taskData.id} mutationUrl={mutationUrl}>
+        <ListItem
+          tabIndex={0}
+          className="task"
           sx={{
-            pointerEvents: "none",
-            maxWidth: "300px",
+            ...(isSubTask && {
+              ml: "20px",
+              width: "calc(100% - 20px)",
+            }),
+            color:
+              colors[taskData.color][global.user.darkMode ? "A100" : "A700"],
+            p: {
+              xs: 1,
+              sm: 0,
+            },
+            cursor: "unset!important",
+            ...(global.user.darkMode && {
+              "&:hover": {
+                backgroundColor: "hsl(240,11%,19%)!important",
+              },
+              "&:active": {
+                backgroundColor: "hsl(240,11%,16%)!important",
+              },
+            }),
+            ...(!checkList && {
+              boxShadow: {
+                sm: "none!important",
+              },
+              border: {
+                sm: "none!important",
+              },
+            }),
+            gap: "10px!important",
+            mt: {
+              xs: 1.5,
+              sm: checkList ? 1.5 : taskData.pinned ? 0.5 : 0,
+            },
           }}
         >
-          <Box>
-            <Typography variant="h6">{renderText(taskData.name)}</Typography>
-            <Typography
-              variant="body2"
-              sx={{
-                textOverflow: "ellipsis",
-                overflow: "hidden",
-                whiteSpace: "hidden",
-              }}
-            >
-              {taskData.description !== ""
-                ? taskData.description
-                : "(no description provided)"}
-              {taskData.due && <>&nbsp;&bull;&nbsp;</>}
-              {taskData.due && dayjs(taskData.due).format("MMMM D, YYYY")}
-            </Typography>
-          </Box>
-        </MenuItem>
-        <MenuItem onClick={() => setOpen(true)}>
-          <Icon className="outlined">edit</Icon>Edit
-        </MenuItem>
-        <MenuItem
-          onClick={() => {
-            handlePriorityClick();
-          }}
-        >
-          <Icon className="outlined">error</Icon>Mark as{" "}
-          {taskData.pinned ? "unimportant" : "important"}
-        </MenuItem>
-        <MenuItem
-          onClick={() => {
-            setLoading(true);
-            handleDelete(taskData.id);
-          }}
-        >
-          <Box sx={{ position: "relative", width: "100%" }}>
-            {loading && (
-              <CircularProgress
-                sx={{
-                  position: "absolute",
-                  left: "50%",
-                  transform: "translateX(-50%)",
-                }}
-                size={20}
-              />
-            )}
-            <Box
-              sx={{
-                ...(loading && { filter: "blur(10px)" }),
-                display: "flex",
-                alignItems: "center",
-                gap: 2,
-              }}
-            >
-              <Icon className="outlined">delete</Icon>Delete
-            </Box>
-          </Box>
-        </MenuItem>
-      </Menu>
-      {taskData.subTasks.length >= 0 && (
-        <TaskDrawer id={taskData.id} mutationUrl={mutationUrl}>
-          <ListItem
-            tabIndex={0}
-            onClick={() => setOpen(true)}
-            onContextMenu={handleContextMenu}
-            className="task"
+          <ListItemText
             sx={{
-              color:
-                colors[taskData.color][global.user.darkMode ? "A100" : "A700"],
-              p: {
-                xs: 1,
-                sm: 0,
-              },
-              cursor: "unset!important",
-              ...(global.user.darkMode && {
-                "&:hover": {
-                  backgroundColor: "hsl(240,11%,19%)!important",
-                },
-                "&:active": {
-                  backgroundColor: "hsl(240,11%,16%)!important",
-                },
-              }),
-              ...(!checkList && {
-                boxShadow: {
-                  sm: "none!important",
-                },
-                border: {
-                  sm: "none!important",
-                },
-              }),
-              gap: "10px!important",
-              mt: {
-                xs: 1.5,
-                sm: checkList ? 1.5 : taskData.pinned ? 0.5 : 0,
-              },
+              my: 0,
             }}
-          >
-            <ListItemText
-              sx={{
-                my: 0,
-              }}
-              primary={
+            primary={
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                <Checkbox
+                  disabled={
+                    (board && board.archived) ||
+                    global.permission === "read-only"
+                  }
+                  disableRipple
+                  checked={checked}
+                  onChange={(e) => {
+                    setChecked(e.target.checked);
+                    mutate(mutationUrl);
+                    fetchApiWithoutHook("property/boards/column/task/mark", {
+                      completed: e.target.checked ? "true" : "false",
+                      id: taskData.id,
+                    }).catch(() =>
+                      toast.error(
+                        "An error occured while updating the task",
+                        toastStyles
+                      )
+                    );
+                  }}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                  }}
+                  sx={{
+                    "&:hover": { bgcolor: "transparent" },
+                  }}
+                  color="default"
+                  checkedIcon={<BpCheckedIcon />}
+                  icon={<BpIcon />}
+                />
+
                 <Box
                   sx={{
-                    display: "flex",
-                    alignItems: "center",
+                    overflow: "hidden",
+                    textOverflow: "ellipsis",
+                    whiteSpace: "nowrap",
+                    ...(checked && {
+                      textDecoration: "line-through",
+                      opacity: 0.7,
+                    }),
                   }}
                 >
-                  <Checkbox
-                    disabled={
-                      (board && board.archived) ||
-                      global.permission === "read-only"
-                    }
-                    disableRipple
-                    checked={checked}
-                    onChange={(e) => {
-                      setChecked(e.target.checked);
-                      mutate(mutationUrl);
-                      fetchApiWithoutHook("property/boards/column/task/mark", {
-                        completed: e.target.checked ? "true" : "false",
-                        id: taskData.id,
-                      }).catch(() =>
-                        toast.error(
-                          "An error occured while updating the task",
-                          toastStyles
-                        )
-                      );
-                    }}
-                    onClick={(e) => {
-                      e.stopPropagation();
-                    }}
-                    sx={{
-                      "&:hover": { bgcolor: "transparent" },
-                    }}
-                    color="default"
-                    checkedIcon={<BpCheckedIcon />}
-                    icon={<BpIcon />}
-                  />
-
-                  <Box
-                    sx={{
-                      overflow: "hidden",
-                      textOverflow: "ellipsis",
-                      whiteSpace: "nowrap",
-                      ...(checked && {
-                        textDecoration: "line-through",
-                        opacity: 0.7,
-                      }),
-                    }}
-                  >
-                    {renderText(taskData.name)}
-                    {taskData.image && (
-                      <ImageViewer trimHeight url={taskData.image} />
-                    )}
-                  </Box>
-                  {taskData.pinned && (
-                    <Tooltip title="Marked as important" placement="top">
-                      <Icon
-                        onClick={(e) => e.stopPropagation()}
-                        sx={{
-                          ml: "auto",
-                          mr: 1,
-                          color:
-                            colors.orange[
-                              global.user.darkMode ? "200" : "A400"
-                            ],
-                        }}
-                        className="outlined"
-                      >
-                        error
-                      </Icon>
-                    </Tooltip>
+                  {renderText(taskData.name)}
+                  {taskData.image && (
+                    <ImageViewer trimHeight url={taskData.image} />
                   )}
                 </Box>
-              }
-              secondary={
-                <span
-                  style={{
-                    marginLeft: "45px",
-                    display: "block",
-                    position: "relative",
-                    top: taskData.image ? "3px" : "-7px",
-                    ...(taskData.image && {
-                      marginBottom: "7px",
-                    }),
-                    whiteSpace: "nowrap",
-                    textOverflow: "ellipsis",
-                    maxWidth: "100%",
-                    overflow: "hidden",
-                  }}
-                >
-                  {renderDescription(taskData.description)}
-                  {taskData.due && !isAgenda && (
-                    <Tooltip
-                      title={dayjs(taskData.due).format("MMMM D, YYYY")}
-                      followCursor
-                      placement="bottom-start"
+                {taskData.pinned && (
+                  <Tooltip title="Marked as important" placement="top">
+                    <Icon
+                      onClick={(e) => e.stopPropagation()}
+                      sx={{
+                        ml: "auto",
+                        mr: 1,
+                        color:
+                          colors.orange[global.user.darkMode ? "200" : "A400"],
+                      }}
+                      className="outlined"
                     >
-                      <span
-                        style={{
-                          display: "flex",
-                          alignItems: "center",
-                          gap: "2px",
-                          marginTop: "3px",
-                          marginLeft: "-5px",
-                        }}
+                      error
+                    </Icon>
+                  </Tooltip>
+                )}
+              </Box>
+            }
+            secondary={
+              <span
+                style={{
+                  marginLeft: "45px",
+                  display: "block",
+                  position: "relative",
+                  top: taskData.image ? "3px" : "-7px",
+                  ...(taskData.image && {
+                    marginBottom: "7px",
+                  }),
+                  whiteSpace: "nowrap",
+                  textOverflow: "ellipsis",
+                  maxWidth: "100%",
+                  overflow: "hidden",
+                }}
+              >
+                {renderDescription(taskData.description)}
+                {taskData.due && !isAgenda && (
+                  <Tooltip
+                    title={dayjs(taskData.due).format("MMMM D, YYYY")}
+                    followCursor
+                    placement="bottom-start"
+                  >
+                    <span
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                        gap: "2px",
+                        marginTop: "3px",
+                        marginLeft: "-5px",
+                      }}
+                    >
+                      <Icon
+                        sx={{ mx: 1, transform: "scale(.9)" }}
+                        className="outlined"
                       >
-                        <Icon
-                          sx={{ mx: 1, transform: "scale(.9)" }}
-                          className="outlined"
-                        >
-                          schedule
-                        </Icon>
-                        {dayjs(taskData.due).fromNow()}
-                      </span>
-                    </Tooltip>
-                  )}
-                </span>
-              }
-            />
-          </ListItem>
-        </TaskDrawer>
-      )}
-      {taskData.subTasks.map((subtask) => (
-        <SubTask
-          board={board}
-          checkList={checkList}
-          mutationUrl={mutationUrl}
-          setOpen={setOpen}
-          key={subtask.id}
-          BpIcon={BpIcon}
-          BpCheckedIcon={BpCheckedIcon}
-          subtask={subtask}
-        />
-      ))}
+                        schedule
+                      </Icon>
+                      {dayjs(taskData.due).fromNow()}
+                    </span>
+                  </Tooltip>
+                )}
+              </span>
+            }
+          />
+        </ListItem>
+      </TaskDrawer>
+      {taskData &&
+        taskData.subTasks &&
+        taskData.subTasks.map((subtask) => (
+          <Task
+            isSubTask
+            board={board}
+            isAgenda={isAgenda}
+            columnId={columnId}
+            mutationUrl={mutationUrl}
+            task={subtask}
+            checkList={checkList}
+          />
+        ))}
     </>
   );
 };
