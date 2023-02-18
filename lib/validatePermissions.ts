9a8bf8442db9e3970947de43db398d8dc2894165
_manu.codes <manuthecoder@protonmail.com>
2023-02-18 10:58:58 -0800

    Update validatePermissions.ts

---
+++ lib/validatePermissions.ts
@@ -0,0 +1,25 @@
+import { prisma } from "./prismaClient";
+
+/**
+ * Fetches the permissions of a user
+ */
+export const validatePermissions = async (
+  property: string,
+  accessToken: string
+) => {
+  // If property or access token isn't defined
+  if (!property || !accessToken) return false;
+
+  const permissions = await prisma.propertyInvite.findFirst({
+    where: {
+      propertyId: property,
+      accessToken: accessToken,
+    },
+    select: {
+      permission: true,
+    },
+    cacheStrategy: { swr: 300, ttl: 600 },
+  });
+
+  return permissions ? permissions.permission : false;
+};
