0372c60869020aab01008a87d5e6122535b1783a
Manu <manusvathgurudath@gmail.com>
2023-03-08 00:45:23 +0000

    W

--- pages/auth/signup.tsx
+++ pages/auth/signup.tsx
@@ -227,6 +227,9 @@ export default function Prompt() {
                     name.trim() == "" ||
                     email.trim() == "" ||
                     password.length < 8 ||
+                    !/\d/.test(password) ||
+                    !/[a-z]/i.test(password) ||
+                    confirmPassword.trim().length < 8 ||
                     password !== confirmPassword
                   }
                   size="large"
