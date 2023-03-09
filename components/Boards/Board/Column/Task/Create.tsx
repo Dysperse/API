7bbac4e01015f7323596bb15f27297f4e65161e6
_manu.codes <manusvathgurudath@gmail.com>
2023-03-09 14:04:20 -0800

    Update Create.tsx

--- components/Boards/Board/Column/Task/Create.tsx
+++ components/Boards/Board/Column/Task/Create.tsx
@@ -704,7 +704,7 @@ export function CreateTask({
             display: "flex",
             width: 25,
             height: 25,
-            ml: 1,
+            ml: 0.5,
             alignItems: "center",
             justifyContent: "center",
           }}
