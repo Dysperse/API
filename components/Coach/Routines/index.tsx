9620db0e90bd7c4fd705bc0f8483802fa1efe6fc
Manu <manusvathgurudath@gmail.com>
2023-03-14 22:01:57 +0000

    W

--- components/Coach/Routines/index.tsx
+++ components/Coach/Routines/index.tsx
@@ -1,5 +1,6 @@
-import { Box, Skeleton } from "@mui/material";
+import { Box, Skeleton, Typography } from "@mui/material";
 import { useApi } from "../../../hooks/useApi";
+import { useSession } from "../../../pages/_app";
 import { ErrorHandler } from "../../Error";
 
 export function Routines() {
@@ -17,6 +18,7 @@ export function Routines() {
     >
       {[...new Array(20)].map((_, index) => (
         <Skeleton
+          key={index}
           variant="circular"
           animation="wave"
           height={65}
@@ -26,23 +28,76 @@ export function Routines() {
       ))}
     </Box>
   );
+  const session = useSession();
+
   return (
     <Box>
       {data ? (
-        <>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            overflowX: "hidden",
+            cursor: "pointer",
+            gap: 2,
+            px: 2,
+            mb: 2,
+          }}
+        >
           {data.map((goal) => (
             <Box
+              key={goal.id}
               sx={{
+                flexShrink: 0,
+                flex: "0 0 65px",
+                gap: 0.4,
                 display: "flex",
+                flexDirection: "column",
                 alignItems: "center",
-                overflowX: "hidden",
-                gap: 2,
-                px: 2,
-                mb: 2,
+                overflow: "hidden",
+                "&:hover": {
+                  background: `hsl(240, 11%, ${
+                    session?.user?.darkMode ? 10 : 90
+                  }%)`,
+                },
+                "&:active": {
+                  transform: "scale(.95)",
+                },
               }}
-            ></Box>
+            >
+              <Box
+                key={goal.id}
+                sx={{
+                  borderRadius: 9999,
+                  width: 65,
+                  height: 65,
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  background: "rgba(200,200,200,.3)",
+                }}
+              >
+                <picture>
+                  <img src={goal.emoji} width="35px" height="35px" />
+                </picture>
+              </Box>
+              <Box sx={{ width: "100%" }}>
+                <Typography
+                  variant="body2"
+                  sx={{
+                    whiteSpace: "nowrap",
+                    textAlign: "center",
+                    textOverflow: "ellipsis",
+                    fontSize: "13px",
+                    overflow: "hidden",
+                  }}
+                >
+                  {goal.name}
+                </Typography>
+              </Box>
+            </Box>
           ))}
-        </>
+        </Box>
       ) : (
         loading
       )}
