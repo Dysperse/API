895618ebfe8f93a478b10d70f65848493ee47fad
Manu <manusvathgurudath@gmail.com>
2023-03-15 16:36:01 +0000

    W

--- pages/api/user/routines/custom-routines/create.ts
+++ pages/api/user/routines/custom-routines/create.ts
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
       note: req.query.note,
       daysOfWeek: req.query.daysOfWeek,
       emoji: req.query.emoji,
-      timeOfDay: req.query.timeOfDay,
+      timeOfDay: parseInt(req.query.timeOfDay),
       user: { connect: { identifier: req.query.userIdentifier } },
     },
   });
