7cc0451cfe9a6c094179f1e03224771ca223360b
Manu <manusvathgurudath@gmail.com>
2023-03-18 23:21:09 +0000

    W

--- components/Settings/AccountSettings.tsx
+++ components/Settings/AccountSettings.tsx
@@ -1,52 +1,58 @@
-import { updateSettings } from "./updateSettings";
-
-import {
-  Alert,
-  Box,
-  Link,
-  ListItem,
-  ListItemText,
-  TextField,
-} from "@mui/material";
+import { Alert, Box, Button, Link, TextField } from "@mui/material";
+import { useCallback, useState } from "react";
 import { useSession } from "../../pages/_app";
+import { updateSettings } from "./updateSettings";
 
 /**
  * Top-level component for the account settings page.
  */
 export default function AppearanceSettings() {
   const session = useSession();
+  const [name, setName] = useState(session.user.name);
+  const handleChange = useCallback((e) => setName(e.target.value), [setName]);
+
+  const handleSubmit = () => {
+    if (name.trim() !== "") updateSettings("name", name);
+  };
 
   return (
     <Box>
-      <ListItem>
-        <ListItemText
-          primary={
-            <TextField
-              variant="filled"
-              defaultValue={session?.user && session.user.name}
-              label="Name"
-              onBlur={(e) => updateSettings("name", e.target.value)}
-              onKeyDown={(e: any) => {
-                if (e.key == "Enter") e.target.blur();
-              }}
-            />
+      <TextField
+        onKeyDown={(e) => e.stopPropagation()}
+        variant="filled"
+        label="Name"
+        fullWidth
+        value={name}
+        onChange={handleChange}
+        margin="dense"
+      />
+      <TextField
+        disabled
+        sx={{ mb: 2 }}
+        variant="filled"
+        defaultValue={session?.user && session.user.email}
+        label="Email"
+        margin="dense"
+      />
+      <Box
+        sx={{
+          display: "flex",
+        }}
+      >
+        <Button
+          onClick={handleSubmit}
+          variant="contained"
+          sx={{ ml: "auto" }}
+          disabled={
+            session.user.name.trim() === name.trim() ||
+            name.trim() === "" ||
+            name.length > 40
           }
-        />
-      </ListItem>
-      <ListItem>
-        <ListItemText
-          primary={
-            <TextField
-              disabled
-              sx={{ mb: 2 }}
-              variant="filled"
-              defaultValue={session?.user && session.user.email}
-              label="Email"
-            />
-          }
-        />
-      </ListItem>
-      <Alert sx={{ mb: 3, px: 2 }} severity="info">
+        >
+          Save
+        </Button>
+      </Box>
+      <Alert sx={{ my: 3, px: 2 }} severity="info">
         If you want to deactivate your account,{" "}
         <Link href="mailto:hello@dysperse.com" target="_blank">
           please contact us
