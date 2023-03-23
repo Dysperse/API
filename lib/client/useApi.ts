deeebe5dd6efeec69ceeab8a002f36ed0cff5cfc
Manu <manusvathgurudath@gmail.com>
2023-03-23 22:33:23 +0000

    Add JSDoc documentation for API

--- lib/client/useApi.ts
+++ lib/client/useApi.ts
@@ -28,30 +28,53 @@ const getInfo = (
   };
 };
 
+export interface ApiResponse {
+  /**
+   * Data returned from the API
+   */
+  data: any;
+  /**
+   * URL can be used for either debugging or passed as a parameter for the SWR `mutate()` function
+   */
+  url: string;
+  /**
+   * Is the request still loading?
+   */
+  loading: boolean;
+  /**
+   * Returns if there was an error in fetching the request
+   */
+  error: null | any;
+  /**
+   * Function to actually fetch the url from the API. (Required for the SWR `preload()` function)
+   */
+  fetcher: any;
+}
+
+/**
+ *
+ * @param path - The path of the API request
+ * @param initialParams - Any parameters you want to send to the server goes here
+ * @param removeDefaultParams - Change this to `true` if you want to prevent the default tokens and parameters from being passed to the server
+ * @returns @interface ApiResponse
+ */
 export function useApi(
   path: string,
   initialParams = {},
   removeDefaultParams = false
-) {
+): ApiResponse {
   let session = useSession() || { property: "", user: "" };
+  const { property, user } = session;
 
   const { url } = useMemo(
-    () =>
-      getInfo(
-        path,
-        initialParams,
-        session.property,
-        session?.user,
-        removeDefaultParams
-      ),
-    [path, initialParams, removeDefaultParams, session.property, session?.user]
+    () => getInfo(path, initialParams, property, user, removeDefaultParams),
+    [path, initialParams, property, user, removeDefaultParams]
   );
 
   const fetcher = (url: string) => fetch(url).then((res) => res.json());
-
   const { data, error } = useSWR(url, fetcher);
 
-  const [response, setResponse] = useState({
+  const [response, setResponse] = useState<ApiResponse>({
     data,
     url,
     loading: !error && !data,
@@ -72,11 +95,18 @@ export function useApi(
   return response;
 }
 
+/**
+ * Use the raw API without the SWR library
+ * @param path - The path of the API request
+ * @param initialParams - Any parameters you want to send to the server goes here
+ * @param removeDefaultParams - Change this to `true` if you want to prevent the default tokens and parameters from being passed to the server
+ * @returns Promise<ApiResponse>
+ */
 export async function useRawApi(
   path,
   initialParams = {},
   removeDefaultParams = false
-) {
+): Promise<ApiResponse> {
   const session = useSession();
 
   const { url } = getInfo(
