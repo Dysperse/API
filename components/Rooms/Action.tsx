5e0e662003c09d6a28de042d217b6e4da5fe7cd2
Manu <manusvathgurudath@gmail.com>
2023-03-23 18:36:35 +0000

    W

--- components/Rooms/Action.tsx
+++ components/Rooms/Action.tsx
@@ -13,189 +13,179 @@ import React, { useCallback } from "react";
 import { useSession } from "../../pages/_app";
 import { RoomActionMenu } from "./items//RoomActionMenu";
 
-/**
- * Room button
- * @param {string | JSX.Element} icon - The room's icon
- * @param {string | JSX.Element} primary - The room's name
- * @param {string} href - The room's link
- * @param {Function} onClick - Callback function for the room's click event
- */
-
-const Action = React.memo(
-  ({
-    count,
-    icon,
-    mutationUrl,
-    disableLoading = false,
-    primary,
-    href,
-    onClick,
-    isPrivate = false,
-    isCustom = false,
-    disabled = false,
-  }: {
-    disableLoading?: boolean;
-    count?: {
-      byRoom: {
-        [key: string]: string | number | boolean;
-      };
+interface RoomActionButtonProps {
+  disableLoading?: boolean;
+  count?: {
+    byRoom: {
+      [key: string]: string | number | boolean;
     };
-    mutationUrl: string;
-    icon: string | JSX.Element;
-    primary: string;
-    href?: string;
-    isPrivate?: boolean;
-    onClick?;
-    isCustom?: boolean;
-    disabled?: boolean;
-  }) => {
-    const router = useRouter();
-    const [loading, setLoading] = React.useState<boolean>(false);
-    const itemCount = count
+  };
+  mutationUrl: string;
+  icon: string | JSX.Element;
+  primary: string;
+  href?: string;
+  isPrivate?: boolean;
+  onClick?;
+  isCustom?: boolean;
+  disabled?: boolean;
+}
+
+const Action = React.memo(function Action({
+  count,
+  icon,
+  mutationUrl,
+  disableLoading = false,
+  primary,
+  href,
+  onClick,
+  isPrivate = false,
+  isCustom = false,
+  disabled = false,
+}: RoomActionButtonProps) {
+  const router = useRouter();
+  const [loading, setLoading] = React.useState<boolean>(false);
+  const itemCount = count
+    ? count.byRoom[primary.toLowerCase()]
       ? count.byRoom[primary.toLowerCase()]
-        ? count.byRoom[primary.toLowerCase()]
-        : -2
-      : -1;
-    const ref: any = React.useRef(null);
+      : -2
+    : -1;
+  const ref: any = React.useRef(null);
 
-    const handleContextMenu = useCallback((e) => {
-      e.preventDefault();
-      e.stopPropagation();
-      ref.current.click();
-      navigator.vibrate(50);
-    }, []);
+  const handleContextMenu = useCallback((e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    ref.current.click();
+    navigator.vibrate(50);
+  }, []);
 
-    const handleClick = useCallback(() => {
-      if (href) router.push(href).then(() => setLoading(false));
-      else {
-        onClick && onClick();
-      }
-      setLoading(true);
-    }, [href, onClick, router]);
+  const handleClick = useCallback(() => {
+    if (href) router.push(href).then(() => setLoading(false));
+    else {
+      onClick && onClick();
+    }
+    setLoading(true);
+  }, [href, onClick, router]);
 
-    const session = useSession();
-    const isActive =
-      (!isCustom &&
-        router.asPath.toLowerCase().includes(primary.toLowerCase())) ||
-      (isCustom &&
-        router.asPath.split("rooms/")[1] &&
-        decode(router.asPath.split("rooms/")[1]).includes(
-          primary.toLowerCase()
-        ));
+  const session = useSession();
+  const isActive =
+    (!isCustom &&
+      router.asPath.toLowerCase().includes(primary.toLowerCase())) ||
+    (isCustom &&
+      router.asPath.split("rooms/")[1] &&
+      decode(router.asPath.split("rooms/")[1]).includes(primary.toLowerCase()));
 
-    return (
-      <ListItemButton
-        disabled={disabled}
-        onClick={handleClick}
-        onMouseDown={handleClick}
-        onContextMenu={handleContextMenu}
-        sx={{
-          mb: 0.2,
-          "&:hover": {
-            background: {
-              sm: session.user.darkMode
-                ? "hsl(240,11%,13%)!important"
-                : "hsl(240,11%,90%)!important",
-            },
-          },
-          cursor: "unset!important",
-          "&:active": {
-            background: session.user.darkMode
+  return (
+    <ListItemButton
+      disabled={disabled}
+      onClick={handleClick}
+      onMouseDown={handleClick}
+      onContextMenu={handleContextMenu}
+      sx={{
+        mb: 0.2,
+        "&:hover": {
+          background: {
+            sm: session.user.darkMode
               ? "hsl(240,11%,13%)!important"
-              : "hsl(240,11%,90%) !important",
+              : "hsl(240,11%,90%)!important",
           },
-          borderRadius: 5,
-          transition: "none!important",
-          background: isActive
-            ? `hsl(240,11%,${session.user.darkMode ? 13 : 90}%)`
-            : "transparent!important",
+        },
+        cursor: "unset!important",
+        "&:active": {
+          background: session.user.darkMode
+            ? "hsl(240,11%,13%)!important"
+            : "hsl(240,11%,90%) !important",
+        },
+        borderRadius: 5,
+        transition: "none!important",
+        background: isActive
+          ? `hsl(240,11%,${session.user.darkMode ? 13 : 90}%)`
+          : "transparent!important",
 
-          ...(session.user.darkMode && {
-            "&:hover .MuiAvatar-root": {
-              background: "hsl(240,11%,17%)",
-            },
-          }),
-        }}
-      >
-        <ListItemAvatar>
-          <Avatar
+        ...(session.user.darkMode && {
+          "&:hover .MuiAvatar-root": {
+            background: "hsl(240,11%,17%)",
+          },
+        }),
+      }}
+    >
+      <ListItemAvatar>
+        <Avatar
+          sx={{
+            borderRadius: 4,
+            color: session.user.darkMode ? "#fff" : "#000",
+            background: session.user.darkMode
+              ? "hsl(240,11%,17%)"
+              : "hsl(240,11%,80%)",
+          }}
+        >
+          <span
+            style={{ fontSize: "20px" }}
+            className="material-symbols-rounded"
+          >
+            {icon}
+          </span>
+        </Avatar>
+      </ListItemAvatar>
+      <ListItemText
+        primary={
+          <Typography
             sx={{
-              borderRadius: 4,
-              color: session.user.darkMode ? "#fff" : "#000",
-              background: session.user.darkMode
-                ? "hsl(240,11%,17%)"
-                : "hsl(240,11%,80%)",
+              fontWeight: "500",
+              whiteSpace: "nowrap",
+              overflow: "hidden",
+              maxWidth: "calc(100% - 40px)",
+              textOverflow: "ellipsis",
             }}
           >
-            <span
-              style={{ fontSize: "20px" }}
-              className="material-symbols-rounded"
-            >
-              {icon}
-            </span>
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText
-          primary={
-            <Typography
-              sx={{
-                fontWeight: "500",
-                whiteSpace: "nowrap",
-                overflow: "hidden",
-                maxWidth: "calc(100% - 40px)",
-                textOverflow: "ellipsis",
-              }}
-            >
-              {primary}
-            </Typography>
-          }
-          secondary={
-            <Typography
-              className="sm text-sm font-normal"
-              sx={{
-                ...((isCustom || itemCount === -3) && { display: "none" }),
-                ...(itemCount == -1 && {
-                  filter: "blur(3px)!important",
-                }),
-              }}
-            >
-              {itemCount !== -2 ? itemCount : "No"} item{itemCount !== 1 && "s"}
-            </Typography>
-          }
-        />
-        <ListItemSecondaryAction>
-          {!disableLoading && loading ? (
-            <CircularProgress
-              size={15}
-              sx={{
-                color: session.user.darkMode ? "#fff" : "#000",
-                ml: "auto",
-                mt: "8px",
-                mr: 1,
-                animationDuration: ".4s",
-                transitionDuration: ".4s",
-              }}
-              disableShrink
-            />
-          ) : (
-            <RoomActionMenu
-              roomId={
-                href
-                  ? decode(
-                      href.replace("/rooms/", "").replace("?custom=true", "")
-                    ).split(",")[0]
-                  : null
-              }
-              mutationUrl={mutationUrl}
-              isCustom={isCustom}
-              isPrivate={isPrivate}
-              itemRef={ref}
-            />
-          )}
-        </ListItemSecondaryAction>
-      </ListItemButton>
-    );
-  }
-);
+            {primary}
+          </Typography>
+        }
+        secondary={
+          <Typography
+            className="sm text-sm font-normal"
+            sx={{
+              ...((isCustom || itemCount === -3) && { display: "none" }),
+              ...(itemCount == -1 && {
+                filter: "blur(3px)!important",
+              }),
+            }}
+          >
+            {itemCount !== -2 ? itemCount : "No"} item{itemCount !== 1 && "s"}
+          </Typography>
+        }
+      />
+      <ListItemSecondaryAction>
+        {!disableLoading && loading ? (
+          <CircularProgress
+            size={15}
+            sx={{
+              color: session.user.darkMode ? "#fff" : "#000",
+              ml: "auto",
+              mt: "8px",
+              mr: 1,
+              animationDuration: ".4s",
+              transitionDuration: ".4s",
+            }}
+            disableShrink
+          />
+        ) : (
+          <RoomActionMenu
+            roomId={
+              href
+                ? decode(
+                    href.replace("/rooms/", "").replace("?custom=true", "")
+                  ).split(",")[0]
+                : null
+            }
+            mutationUrl={mutationUrl}
+            isCustom={isCustom}
+            isPrivate={isPrivate}
+            itemRef={ref}
+          />
+        )}
+      </ListItemSecondaryAction>
+    </ListItemButton>
+  );
+});
 
 export default Action;
