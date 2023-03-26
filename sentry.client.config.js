e53ac8c480b1da6e18a2bc48597319a197014921
Manu <manusvathgurudath@gmail.com>
2023-03-26 02:06:10 +0000

    W

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -10,8 +10,6 @@ Sentry.init({
   // If the entire session is not sampled, use the below sample rate to sample
   // sessions when an error occurs.
   replaysOnErrorSampleRate: 1.0,
-
-  integrations: [
-    new Replay()
-  ],
+  ignoreErrors: ["ResizeObserver loop limit exceeded"],
+  integrations: [new Replay()],
 });
