1f8d7f56d38fe1886d267f2d335855a1afd5e810
_manu.codes <manusvathgurudath@gmail.com>
2023-04-05 21:52:40 -0700

    Update prevent-navigate-history.js

--- public/prevent-navigate-history.js
+++ public/prevent-navigate-history.js
@@ -1,8 +1,3 @@
 window.addEventListener("load", () => {
-  setTimeout(() => {
-    history.pushState(null, null, location.href);
-    window.onpopstate = function () {
-      history.go(1);
-    };
-  }, 2000);
+  console.log("Loaded")
 });
