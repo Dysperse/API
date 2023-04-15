725e10097e5a35b55a0e91b5123ca7ff4cf6d3d1
Manu <manusvathgurudath@gmail.com>
2023-04-15 21:53:47 +0000

    Fix stuff

--- pages/tasks.tsx
+++ pages/tasks.tsx
@@ -1,16 +1,8 @@
-import Head from "next/head";
 import { TasksLayout } from "../components/Boards/Layout";
 
 /**
  * Top-level component for the dashboard page.
  */
 export default function Dashboard() {
-  return (
-    <>
-      <Head>
-        <title>Tasks</title>
-      </Head>
-      <TasksLayout />
-    </>
-  );
+  return <TasksLayout />;
 }
