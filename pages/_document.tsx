4de1c2526deb9ef905fbe44600cdf7463a48b615
Manu <manusvathgurudath@gmail.com>
2023-04-22 23:43:26 +0000

    Fix google analytics issues

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,14 +8,18 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <Script id="google-analytics" strategy="afterInteractive" defer>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-KFJ4BEE09N"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
           {`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-PPH4TH4');
-         `}
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-KFJ4BEE09N');
+        `}
         </Script>
 
         {/* Preconnect */}
@@ -118,11 +122,6 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PPH4TH4" height="0" width="0" style="display: none; visibility: hidden;" />`,
-          }}
-        />
       </body>
     </Html>
   );
