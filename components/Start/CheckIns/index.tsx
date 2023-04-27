f93e1f43488e31821d15e4adbb50d59bd272a760
Manu <manusvathgurudath@gmail.com>
2023-04-27 18:56:58 +0000

    fix

--- components/Start/CheckIns/index.tsx
+++ components/Start/CheckIns/index.tsx
@@ -18,12 +18,16 @@ import {
 import dayjs from "dayjs";
 import useEmblaCarousel from "embla-carousel-react";
 import { WheelGesturesPlugin } from "embla-carousel-wheel-gestures";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
 import { Sparklines, SparklinesLine, SparklinesSpots } from "react-sparklines";
+import { mutate } from "swr";
 import { capitalizeFirstLetter } from "../../../lib/client/capitalizeFirstLetter";
-import { useApi } from "../../../lib/client/useApi";
+import { fetchRawApi, useApi } from "../../../lib/client/useApi";
 import { useSession } from "../../../lib/client/useSession";
+import { toastStyles } from "../../../lib/client/useTheme";
 import { colors } from "../../../lib/colors";
+import { Emoji } from "./Emoji";
 import { InfoModal } from "./InfoModal";
 export const moodOptions = ["1f601", "1f600", "1f610", "1f614", "1f62d"];
 
@@ -535,5 +539,79 @@ export function DailyCheckInDrawer({ mood }) {
         <Box sx={{ mt: 5 }} />
       </SwipeableDrawer>
     </>
+  );
+}
+
+export function DailyCheckIn() {
+  const [mood, setMood] = useState<string | null>(null);
+  const today = dayjs().startOf("day");
+  const { data, url: mutationUrl } = useApi("user/checkIns", {
+    date: today,
+  });
+
+  useEffect(() => {
+    if (data && data[0] && data[0].mood) {
+      setMood(data[0].mood);
+    } else {
+      setMood(null);
+    }
+  }, [data, mood, setMood]);
+
+  const handleMoodChange: any = useCallback(
+    async (emoji: string, reason: string) => {
+      try {
+        await fetchRawApi("user/checkIns/setMood", {
+          date: today,
+          mood: emoji,
+          reason,
+          delete: emoji === mood ? "true" : "false",
+        });
+        await mutate(mutationUrl);
+        toast.success("Updated mood!", toastStyles);
+      } catch (e) {
+        toast.error(
+          "Oh no! Something went wrong while trying to save your mood!",
+          toastStyles
+        );
+      }
+    },
+    [today, mutationUrl, mood]
+  );
+  const session = useSession();
+
+  return (
+    <Box
+      sx={{
+        background: `hsl(240,11%,${session.user.darkMode ? 10 : 100}%)`,
+        border: "1px solid",
+        borderColor: session.user.darkMode
+          ? "hsl(240, 11%, 20%)"
+          : "rgba(200, 200, 200, 0.3)",
+        borderRadius: 5,
+      }}
+    >
+      <DailyCheckInDrawer mood={mood} />
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          mt: 0.5,
+          mb: -1,
+          gap: 0.5,
+          p: 3,
+          pt: 0,
+        }}
+      >
+        {moodOptions.map((emoji) => (
+          <Emoji
+            key={emoji}
+            emoji={emoji}
+            handleMoodChange={handleMoodChange}
+            mood={mood}
+            data={data}
+          />
+        ))}
+      </Box>
+    </Box>
   );
 }
