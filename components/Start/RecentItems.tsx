ffc3130aa6ba332af1ce81c591a563132767db98
Manu <manusvathgurudath@gmail.com>
2023-04-28 18:57:40 +0000

    remove embla shit

--- components/Start/RecentItems.tsx
+++ components/Start/RecentItems.tsx
@@ -10,9 +10,6 @@ import {
   useMediaQuery,
 } from "@mui/material";
 import dayjs from "dayjs";
-import useEmblaCarousel from "embla-carousel-react";
-import { WheelGesturesPlugin } from "embla-carousel-wheel-gestures";
-import { useEffect } from "react";
 import { Twemoji } from "react-emoji-render";
 import { useApi } from "../../lib/client/useApi";
 import { useSession } from "../../lib/client/useSession";
@@ -27,19 +24,6 @@ export function RecentItems() {
     take: trigger ? 12 : 6,
   });
 
-  const [emblaRef, emblaApi] = useEmblaCarousel(
-    {
-      dragFree: trigger,
-      align: "start",
-      containScroll: "trimSnaps",
-      loop: false,
-    },
-    [WheelGesturesPlugin()]
-  );
-
-  useEffect(() => {
-    emblaApi && emblaApi?.reInit();
-  }, [data, emblaApi]);
   const session = useSession();
 
   return (
@@ -87,99 +71,95 @@ export function RecentItems() {
         Recently edited
       </Typography>
       <Box
-        className="embla"
-        ref={emblaRef}
         sx={{
           px: { xs: 2, sm: 3.5 },
           width: "100%",
           whiteSpace: "nowrap",
           overflowX: "scroll",
+          display: "flex",
           overflowY: "visible",
           mb: 2,
+          gap: 2,
+          pb: 2,
         }}
       >
-        <div
-          className="embla__container"
-          style={{ gap: "15px", paddingBottom: "10px" }}
-        >
-          {!data && (
-            <>
-              {[...new Array(6)].map((_, index) => (
-                <Skeleton
-                  variant="rectangular"
-                  height={120}
-                  animation="wave"
-                  sx={{
-                    borderRadius: 5,
-                    flex: { xs: "0 0 90%", sm: "0 0 20%" },
-                  }}
-                  key={index}
-                />
-              ))}
-            </>
-          )}
-          {data &&
-            data.map((item) => (
-              <TaskDrawer id={item.id} mutationUrl={url} key={item.id}>
-                <Card
-                  sx={{
-                    border: "1px solid",
-                    borderColor: session.user.darkMode
-                      ? "hsl(240, 11%, 20%)"
-                      : "rgba(200, 200, 200, 0.3)",
-                    width: "100%",
-                    flex: { xs: "0 0 90%", sm: "0 0 20%" },
-                    borderRadius: 5,
-                  }}
-                  variant="outlined"
-                >
-                  <CardActionArea sx={{ height: "100%" }}>
-                    <CardContent sx={{ height: "100%" }}>
-                      <Typography
-                        sx={{
-                          fontWeight: 700,
-                          mb: 1,
-                          whiteSpace: "nowrap",
-                          overflow: "hidden",
-                          textOverflow: "ellipsis",
-                          "& span img": {
-                            display: "inline-flex !important",
-                            width: "23px!important",
-                            height: "23px!important",
-                            verticalAlign: "top !important",
-                          },
-                          color:
-                            colors[item.color][
-                              session.user.darkMode ? "A400" : 700
-                            ],
-                        }}
-                      >
-                        <span>
-                          <Twemoji>{item.name || " "}</Twemoji>
-                        </span>
-                      </Typography>
-                      {item.pinned && (
-                        <Chip
-                          size="small"
-                          sx={{ mb: 0.5, mr: 0.5 }}
-                          icon={<Icon>priority_high</Icon>}
-                          label={"Urgent"}
-                        />
-                      )}
-                      {item.lastUpdated && (
-                        <Chip
-                          size="small"
-                          sx={{ mb: 0.5 }}
-                          icon={<Icon>history</Icon>}
-                          label={dayjs(item.lastUpdated).fromNow()}
-                        />
-                      )}
-                    </CardContent>
-                  </CardActionArea>
-                </Card>
-              </TaskDrawer>
+        {!data && (
+          <>
+            {[...new Array(6)].map((_, index) => (
+              <Skeleton
+                variant="rectangular"
+                height={120}
+                animation="wave"
+                sx={{
+                  borderRadius: 5,
+                  flex: { xs: "0 0 90%", sm: "0 0 20%" },
+                }}
+                key={index}
+              />
             ))}
-        </div>
+          </>
+        )}
+        {data &&
+          data.map((item) => (
+            <TaskDrawer id={item.id} mutationUrl={url} key={item.id}>
+              <Card
+                sx={{
+                  border: "1px solid",
+                  borderColor: session.user.darkMode
+                    ? "hsl(240, 11%, 20%)"
+                    : "rgba(200, 200, 200, 0.3)",
+                  width: "100%",
+                  flex: { xs: "0 0 90%", sm: "0 0 20%" },
+                  borderRadius: 5,
+                }}
+                variant="outlined"
+              >
+                <CardActionArea sx={{ height: "100%" }}>
+                  <CardContent sx={{ height: "100%" }}>
+                    <Typography
+                      sx={{
+                        fontWeight: 700,
+                        mb: 1,
+                        whiteSpace: "nowrap",
+                        overflow: "hidden",
+                        textOverflow: "ellipsis",
+                        "& span img": {
+                          display: "inline-flex !important",
+                          width: "23px!important",
+                          height: "23px!important",
+                          verticalAlign: "top !important",
+                        },
+                        color:
+                          colors[item.color][
+                            session.user.darkMode ? "A400" : 700
+                          ],
+                      }}
+                    >
+                      <span>
+                        <Twemoji>{item.name || " "}</Twemoji>
+                      </span>
+                    </Typography>
+                    {item.pinned && (
+                      <Chip
+                        size="small"
+                        sx={{ mb: 0.5, mr: 0.5 }}
+                        icon={<Icon>priority_high</Icon>}
+                        label={"Urgent"}
+                      />
+                    )}
+                    {item.lastUpdated && (
+                      <Chip
+                        size="small"
+                        sx={{ mb: 0.5 }}
+                        icon={<Icon>history</Icon>}
+                        label={dayjs(item.lastUpdated).fromNow()}
+                      />
+                    )}
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </TaskDrawer>
+          ))}
       </Box>
     </>
   );
