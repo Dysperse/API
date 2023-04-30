7206b099dab74dd18b04925f4bce76f5d4656886
Manu <manusvathgurudath@gmail.com>
2023-04-30 07:11:38 +0000

    W

--- components/Coach/Routines/Routine.tsx
+++ components/Coach/Routines/Routine.tsx
@@ -85,7 +85,7 @@ export function Routine({ isCoach = false, mutationUrl, routine }) {
                 width="35px"
                 height="35px"
                 alt="Emoji"
-                loading={lazy}
+                loading="lazy"
               />
             </picture>
             <Typography variant="h6">{routine.name}</Typography>
