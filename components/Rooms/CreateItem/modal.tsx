51123caa1773d3639ce7f9dc5ac9ca64dbf3471d
_manu.codes <manusvathgurudath@gmail.com>
2023-05-23 02:24:10 +0000

    W

--- components/Rooms/CreateItem/modal.tsx
+++ components/Rooms/CreateItem/modal.tsx
@@ -65,7 +65,7 @@ export function CreateItemModal({
       containScroll: "trimSnaps",
       loop: false,
     },
-    [WheelGesturesPlugin()]
+    [WheelGesturesPlugin() as any]
   );
 
   const handleSubmit = useCallback(() => {
