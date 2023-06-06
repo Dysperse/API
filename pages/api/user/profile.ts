a022e5588250bab143658c5c8dcc827d1b2043b9
Manu <manusvathgurudath@gmail.com>
2023-06-06 02:47:13 +0000

    W

--- pages/api/user/profile.ts
+++ pages/api/user/profile.ts
@@ -12,6 +12,8 @@ export default async function handler(req, res) {
       color: true,
       name: true,
       email: true,
+      followers: true,
+      following: true,
     },
   });
   res.json(data);
