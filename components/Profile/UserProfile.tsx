377e6402fd069158f9a60c130186119c6dbb29e4
Manu <manusvathgurudath@gmail.com>
2023-06-06 19:36:56 +0000

    add working hours

--- components/Profile/UserProfile.tsx
+++ components/Profile/UserProfile.tsx
@@ -11,12 +11,176 @@ import {
   Button,
   Chip,
   Icon,
+  IconButton,
   TextField,
   Typography,
 } from "@mui/material";
 import dayjs from "dayjs";
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { mutate } from "swr";
+
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+
+export function WorkingHours({
+  isCurrentUser,
+  editMode,
+  mutationUrl,
+  profileCardStyles,
+  profile,
+  data,
+}) {
+  const session = useSession();
+  const [workingHours, setWorkingHours] = useState(
+    JSON.parse(profile.workingHours || "[]")
+  );
+
+  const handleChange = (index, field, value) => {
+    const updatedWorkingHours = [...workingHours];
+    updatedWorkingHours[index][field] = value;
+    setWorkingHours(updatedWorkingHours);
+  };
+
+  const [edited, setEdited] = useState(false);
+
+  const handleSave = useCallback(async () => {
+    const data = await fetchRawApi("user/profile/update", {
+      email: session.user.email,
+      workingHours: JSON.stringify(workingHours),
+    });
+    console.log(data);
+    await mutate(mutationUrl);
+  }, [workingHours, mutationUrl, session.user.email]);
+
+  // Save
+  useEffect(() => {
+    if (edited) handleSave();
+  }, [workingHours, handleSave, edited]);
+
+  return (
+    <Box sx={profileCardStyles}>
+      <Typography
+        sx={{
+          mb: 2,
+          color: colors[data.color][800],
+          display: "flex",
+          alignItems: "center",
+        }}
+        variant="h6"
+      >
+        Working hours
+        <IconButton
+          sx={{ ml: "auto", ...(!editMode && { display: "none" }) }}
+          onClick={() => {
+            const updatedWorkingHours = [...workingHours];
+            updatedWorkingHours.push({
+              dayOfWeek: 1,
+              startTime: "9:00",
+              endTime: "17:00",
+            });
+            setWorkingHours(updatedWorkingHours);
+            setEdited(true);
+          }}
+          disabled={workingHours.length === 7}
+        >
+          <Icon>add</Icon>
+        </IconButton>
+      </Typography>
+      {workingHours.map((hour, index) => (
+        <Box
+          key={index}
+          sx={{ display: "flex", alignItems: "center", mb: 1, gap: 2 }}
+        >
+          <FormControl fullWidth variant="standard">
+            {editMode && <InputLabel>Day</InputLabel>}
+            <Select
+              value={hour.dayOfWeek}
+              size="small"
+              {...(!editMode && { inputProps: { IconComponent: () => null } })}
+              onChange={(e) => {
+                setEdited(true);
+                handleChange(index, "dayOfWeek", e.target.value);
+              }}
+              disabled={!editMode}
+              readOnly={!editMode}
+            >
+              {[
+                "Sunday",
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+              ].map((day, i) => (
+                <MenuItem value={i} key={i}>
+                  {day}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          {!editMode && (
+            <Typography sx={{ color: colors[data.color][800] }}>
+              from
+            </Typography>
+          )}
+          <FormControl fullWidth variant="standard">
+            {editMode && <InputLabel>Start Time</InputLabel>}
+            <Select
+              value={hour.startTime}
+              label="Start time"
+              {...(!editMode && { inputProps: { IconComponent: () => null } })}
+              size="small"
+              onChange={(e) => {
+                setEdited(true);
+                handleChange(index, "startTime", e.target.value);
+              }}
+              readOnly={!editMode}
+              disabled={!editMode}
+            >
+              {[...new Array(23)].map((_, i) => (
+                <MenuItem key={i} value={i}>{`${i + 1}:00`}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          {!editMode && (
+            <Typography sx={{ color: colors[data.color][800] }}>to</Typography>
+          )}
+          <FormControl fullWidth variant="standard">
+            {editMode && <InputLabel>End Time</InputLabel>}
+            <Select
+              label="End time"
+              size="small"
+              {...(!editMode && { inputProps: { IconComponent: () => null } })}
+              onChange={(e) => {
+                setEdited(true);
+                handleChange(index, "endTime", e.target.value);
+              }}
+              readOnly={!editMode}
+              disabled={!editMode}
+              value={hour.endTime}
+            >
+              {[...new Array(23)].map((_, i) => (
+                <MenuItem key={i} value={i}>{`${i + 1}:00`}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          {editMode && (
+            <IconButton
+              onClick={() => {
+                setEdited(true);
+                const updatedWorkingHours = [...workingHours];
+                updatedWorkingHours.splice(index, 1);
+                setWorkingHours(updatedWorkingHours);
+              }}
+            >
+              <Icon>delete</Icon>
+            </IconButton>
+          )}
+        </Box>
+      ))}
+    </Box>
+  );
+}
 
 export function UserProfile({
   setEditMode,
@@ -252,6 +416,16 @@ export function UserProfile({
             )}
           </Box>
         </Box>
+        {profile && (
+          <WorkingHours
+            editMode={editMode}
+            isCurrentUser={isCurrentUser}
+            mutationUrl={mutationUrl}
+            data={data}
+            profile={profile}
+            profileCardStyles={profileCardStyles}
+          />
+        )}
         <Box sx={profileCardStyles}>
           <Typography
             sx={{
