4e1cf0be218849153405779985c7d2f784a5d1b6
Manu <manusvathgurudath@gmail.com>
2023-06-08 08:08:24 +0000

    W

--- components/Coach/Goal/MoreOptions.tsx
+++ components/Coach/Goal/MoreOptions.tsx
@@ -6,23 +6,21 @@ import { mutate } from "swr";
 import { ConfirmationModal } from "../../ConfirmationModal";
 import { ShareGoal } from "./ShareGoal";
 
-export const exportAsImage = async (el, imageFileName) => {
-  const canvas = await html2canvas(el, { backgroundColor: null });
-  const image = canvas.toDataURL("image/png", 1.0);
-  downloadImage(image, imageFileName);
-};
 const downloadImage = (blob, fileName) => {
   const fakeLink: any = window.document.createElement("a");
   fakeLink.style = "display:none;";
   fakeLink.download = fileName;
-
   fakeLink.href = blob;
-
   document.body.appendChild(fakeLink);
   fakeLink.click();
   document.body.removeChild(fakeLink);
-
   fakeLink.remove();
+};
+
+export const exportAsImage = async (el, imageFileName) => {
+  const canvas = await html2canvas(el, { backgroundColor: null });
+  const image = canvas.toDataURL("image/png", 1.0);
+  downloadImage(image, imageFileName);
 };
 
 export function MoreOptions({ goal, mutationUrl, setOpen }): JSX.Element {
