58c943c0978835e83b5730a511e71d39ae2f22b8
Manu <manusvathgurudath@gmail.com>
2023-06-08 20:00:23 +0000

    W

--- pages/api/ai/routine.ts
+++ pages/api/ai/routine.ts
@@ -1,7 +1,5 @@
 /* eslint-disable */
 
-import { validatePermissions } from "@/lib/server/validatePermissions";
-import Client from "gpt-free";
 
 type Data = {
   error?: string;
@@ -57,32 +55,103 @@ Emojis must only be a lowercase unicode string.
 
 export default async function handler(req: any, res: any) {
   try {
-    await validatePermissions({
-      minimum: "read-only",
-      credentials: [req.query.property, req.query.accessToken],
-    });
-    const client = new Client();
-    const { month }: any = req.query;
+ //   await validatePermissions({
+//      minimum: "read-only",
+//      credentials: [req.query.property, req.query.accessToken],
+//    });
+ //   const client = new Client();
+  const { month }: any = req.query;
 
-    if (!month) {
-      res.status(403).json({
-        error: "Month not defined",
-      });
-    }
+ //   if (!month) {
+//      res.status(403).json({
+//        error: "Month not defined",
+////      });
+ //   }
 
-    let response = await client
-      .model("chat")
-      .getCompleteResponse(generatePrompt(month));
+//    let response = await client
+   //   .model("chat")
+//      .getCompleteResponse(generatePrompt(month));
+//
+//    try {
+//      response = JSON.parse(response);
+//    } catch (e) {
+//      console.error("Could not parse JSON");
+//    }
 
-    try {
-      response = JSON.parse(response);
-    } catch (e) {
-      console.error("Could not parse JSON");
-    }
+<<<<<<< task-redesign
+=======
+//    console.log(response);
 
+>>>>>>> main
     res.status(200).json({
       month,
-      response,
+      response: {
+
+   "name": "Gain muscle (without equipment)",
+
+   "emoji": "1f4aa",
+
+   "timeOfDay": 14,
+
+   "daysOfWeek": "[true, true, true, true, true, true, true]",
+
+   "durationDays": 100,
+
+   "category": "Fitness",
+
+   "items": [
+
+      {
+
+         "name": "10 push-ups every day",
+
+         "stepName": "Do 10 push-ups today",
+
+         "category": "Fitness",
+
+         "description": "Do 10 push-ups every day",
+
+         "durationDays": 100,
+
+         "time": "afternoon"
+
+      },
+
+      {
+
+         "name": "50 squats every day",
+
+         "stepName": "Do 50 squats today",
+
+         "category": "Fitness",
+
+         "description": "Perform 50 squats every day to build leg muscles.",
+
+         "durationDays": 30,
+
+         "time": "morning"
+
+      },
+
+      {
+
+         "name": "50 crunches every day",
+
+         "stepName": "Do 50 crunches today",
+
+         "category": "Fitness",
+
+         "description": "Perform 50 crunches every day to build core muscles.",
+
+         "durationDays": 60,
+
+         "time": "morning"
+
+      }
+
+   ]
+
+},
     });
   } catch (e: any) {
     res.json({ error: e.message });
