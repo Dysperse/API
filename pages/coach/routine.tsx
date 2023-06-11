bc1f9d6ae29cc6bde43feee5cdac5e2c739887c6
Manu <manusvathgurudath@gmail.com>
2023-06-11 23:45:24 +0000

    update routine.tsx

--- pages/coach/routine.tsx
+++ pages/coach/routine.tsx
@@ -13,10 +13,11 @@ import {
   Typography,
 } from "@mui/material";
 import dayjs from "dayjs";
+import { motion } from "framer-motion";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { toast } from "react-hot-toast";
-import { mutate } from "swr";
+import { useHotkeys } from "react-hotkeys-hook";
 
 function GoalTask({ goal, setSlide, mutationUrl }) {
   const session = useSession();
@@ -38,14 +39,12 @@ function GoalTask({ goal, setSlide, mutationUrl }) {
             : goal.progress + 1
           : 1,
       id: goal.id,
-    })
-      .then(() => mutate(mutationUrl))
-      .catch(() =>
-        toast.error(
-          "Yikes! Something went wrong while trying to mark your routine as done",
-          toastStyles
-        )
-      );
+    }).catch(() =>
+      toast.error(
+        "Yikes! Something went wrong while trying to mark your routine as done",
+        toastStyles
+      )
+    );
   };
 
   return (
@@ -58,10 +57,7 @@ function GoalTask({ goal, setSlide, mutationUrl }) {
         alignItems: "center",
         justifyContent: "center",
         flexDirection: "column",
-      }}
-    >
-      <Box
-        sx={{
+        "& .goal-task": {
           flexGrow: 1,
           px: 3,
           background: palette[2],
@@ -71,7 +67,14 @@ function GoalTask({ goal, setSlide, mutationUrl }) {
           display: "flex",
           flexDirection: "column",
           justifyContent: "center",
-        }}
+        },
+      }}
+    >
+      <motion.div
+        className="goal-task"
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 50 }}
       >
         <Box sx={{ mt: "auto" }}>
           <Chip label={`${goal.timeOfDay}:00`} sx={{ mb: 2 }} />
@@ -87,14 +90,14 @@ function GoalTask({ goal, setSlide, mutationUrl }) {
           </Typography>
         </Box>
         <Button
-          sx={{ mt: "auto", mb: 2, background: palette[3] }}
+          sx={{ mt: "auto", zIndex: 999999999, mb: 2, background: palette[3] }}
           variant="contained"
           onClick={handleNext}
           disabled={goal.lastCompleted === dayjs().format("YYYY-MM-DD")}
         >
           I completed this task!
         </Button>
-      </Box>
+      </motion.div>
       <Box sx={{ mt: "auto", width: "100%", p: 1, display: "flex" }}>
         <Button sx={{ color: "#fff" }} size="small">
           <Icon>local_fire_department</Icon>
@@ -123,6 +126,12 @@ export default function Routine() {
     if (data && data[0]) setSlide(0);
   }, [data]);
 
+  const handleNext = () => setSlide((s) => (data.length === s ? s : s + 1));
+  const handlePrev = () => setSlide((s) => (s === 0 ? 0 : s - 1));
+
+  useHotkeys("ArrowRight", handleNext);
+  useHotkeys("ArrowLeft", handlePrev);
+
   return (
     <Box
       sx={{
@@ -132,16 +141,16 @@ export default function Routine() {
         width: "100vw",
         height: "100vh",
         background: palette[1],
-        zIndex: 999,
         overflow: "auto",
       }}
     >
       <Box
         sx={{
-          position: "absolute",
+          position: "fixed",
           display: "flex",
           alignItems: "center",
           top: 0,
+          zIndex: 999999,
           gap: 0.5,
           p: 1,
           width: "100%",
@@ -161,7 +170,7 @@ export default function Routine() {
       <IconButton
         onClick={() => router.push("/coach")}
         sx={{
-          position: "absolute",
+          position: "fixed",
           top: 0,
           left: 0,
           m: 2,
@@ -185,9 +194,37 @@ export default function Routine() {
           <CircularProgress />
         </Box>
       )}
+      <Box
+        sx={{
+          height: "100vh",
+          width: "50vw",
+          position: "absolute",
+          right: 0,
+          zIndex: 99,
+          top: 0,
+        }}
+        onClick={handleNext}
+      />
+      <Box
+        sx={{
+          height: "100vh",
+          width: "50vw",
+          position: "absolute",
+          left: 0,
+          zIndex: 99,
+          top: 0,
+        }}
+        onClick={handlePrev}
+      />
       {data &&
         data
-          .sort((a, b) => a.timeOfDay.value - b.timeOfDay.value)
+          .sort((a, b) => a.timeOfDay - b.timeOfDay)
+          .sort(function (x, y) {
+            // true values first
+            return x.completed === y.completed ? 0 : x.completed ? -1 : 1;
+            // false values first
+            // return (x === y)? 0 : x? 1 : -1;
+          })
           .map(
             (goal, index) =>
               slide === index && (
@@ -199,6 +236,30 @@ export default function Routine() {
                 />
               )
           )}
+      {slide >= data?.length && (
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "column",
+            height: "100vh",
+          }}
+        >
+          <Typography variant="h1" sx={{ mb: 2 }} className="font-heading">
+            All done
+          </Typography>
+          <Typography>Come back tomorrow for more.</Typography>
+          <Button
+            onClick={() => router.push("/coach")}
+            sx={{ zIndex: 999 }}
+            variant="contained"
+            size="large"
+          >
+            <Icon>home</Icon>Done
+          </Button>
+        </Box>
+      )}
     </Box>
   );
 }
