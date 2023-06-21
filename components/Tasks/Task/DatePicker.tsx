68b4de5ae68674c5abfa1b1097c09b5a685bc55e
_manu.codes <manusvathgurudath@gmail.com>
2023-06-21 19:49:58 +0000

    W

--- components/Tasks/Task/DatePicker.tsx
+++ components/Tasks/Task/DatePicker.tsx
@@ -4,6 +4,8 @@ import {
   Box,
   Button,
   Icon,
+  Menu,
+  MenuItem,
   SwipeableDrawer,
   Tooltip,
   Typography,
@@ -19,8 +21,15 @@ export const SelectDateModal: any = function SelectDateModal({
   setDate,
 }: any) {
   const session = useSession();
+  const hasTime =
+    date && (dayjs(date).hour() !== 0 || dayjs(date).minute() !== 0);
   const [open, setOpen] = useState<boolean>(false);
   const today = new Date(dayjs().startOf("day").toISOString());
+
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const handleClick = (event) => setAnchorEl(event.currentTarget);
+  const handleClose = () => setAnchorEl(null);
 
   return (
     <>
@@ -36,6 +45,67 @@ export const SelectDateModal: any = function SelectDateModal({
           },
         }}
       >
+        <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <Box sx={{ gap: 1, display: "flex" }}>
+            <Box sx={{ flexGrow: 1 }}>
+              {[...Array(12).keys()].map((i) => (
+                <MenuItem
+                  selected={i === dayjs(date).hour() % 12}
+                  onClick={() => {
+                    setDate(
+                      dayjs(date)
+                        .hour(i + (dayjs(date).hour() % 12))
+                        .toDate()
+                    );
+                  }}
+                  key={i}
+                >
+                  {i % 12 || 12}
+                </MenuItem>
+              ))}
+            </Box>
+            <Box sx={{ flexGrow: 1 }}>
+              {[...Array(12).keys()].map((i) => (
+                <MenuItem
+                  selected={i * 5 === dayjs(date).minute()}
+                  onClick={() => {
+                    setDate(
+                      dayjs(date)
+                        .minute(i * 5)
+                        .toDate()
+                    );
+                  }}
+                  key={i}
+                >
+                  {(i * 5).toString().padStart(2, "0")}
+                </MenuItem>
+              ))}
+            </Box>
+            <Box sx={{ flexGrow: 1 }}>
+              {[...Array(2).keys()].map((i) => (
+                <MenuItem
+                  selected={
+                    i === 0 ? dayjs(date).hour() < 12 : dayjs(date).hour() >= 12
+                  }
+                  onClick={() => {
+                    setDate(
+                      dayjs(date)
+                        .hour(dayjs(date).hour() + (i === 0 ? -12 : 12))
+                        .toDate()
+                    );
+                  }}
+                  key={i}
+                >
+                  {i === 0 ? "AM" : "PM"}
+                </MenuItem>
+              ))}
+            </Box>
+          </Box>
+        </Menu>
         <Puller sx={{ mb: -1 }} showOnDesktop />
         <DatePicker
           value={new Date(date)}
@@ -52,15 +122,18 @@ export const SelectDateModal: any = function SelectDateModal({
             width: "100%",
           }}
         >
+          <Button fullWidth variant="contained" onClick={handleClick}>
+            <Icon>access_time</Icon>
+            {dayjs(date).format("h:mm a")}
+          </Button>
           <Button
-            fullWidth
             sx={{ borderRadius: 9 }}
             variant="contained"
             onClick={() => {
               setDate(today);
             }}
           >
-            Today
+            <Icon>refresh</Icon>
           </Button>
         </Box>
       </SwipeableDrawer>
@@ -81,11 +154,25 @@ export const SelectDateModal: any = function SelectDateModal({
           <Icon>today</Icon>
           <Typography
             sx={{
-              fontSize: "15px",
+              fontSize: hasTime ? "13px" : "15px",
               display: { xs: "none", sm: "inline-flex" },
+              textAlign: "left",
+              justifyContent: "flex-start",
+              alignItems: "flex-start",
+              flexDirection: "column",
             }}
           >
             {date && dayjs(date).format("MMMM D")}
+            {hasTime && (
+              <Typography
+                variant="body2"
+                style={{
+                  fontSize: "10px",
+                }}
+              >
+                {dayjs(date).format("h:mm a")}
+              </Typography>
+            )}
           </Typography>
         </Button>
       </Tooltip>
