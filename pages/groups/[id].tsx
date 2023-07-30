008523d89675b5702e87dd5f6f6c0bea5dca3292
_manu.codes <manusvathgurudath@gmail.com>
2023-07-30 01:41:26 +0000

    W

--- pages/groups/[id].tsx
+++ pages/groups/[id].tsx
@@ -114,8 +114,8 @@ export default function Page() {
         ) : (
           <Box
             sx={{
-              width: "100%",
-              height: "100%",
+              width: "100vw",
+              height: "100vh",
               display: "flex",
               alignItems: "center",
               justifyContent: "center",
