7575d73ecfa1d80f5060423c23ebdac01e194d73
_manu.codes <manusvathgurudath@gmail.com>
2023-08-05 09:31:14 +0000

    update redirect.ts

--- pages/api/user/spotify/redirect.ts
+++ pages/api/user/spotify/redirect.ts
@@ -1,19 +1,13 @@
-const client_id = "27b1c11438204ca6acc88ca514c22665";
-const redirect_uri =
-  "https://manuthecoder-crispy-garbanzo-w495gwjj6q4h5pp-3000.app.github.dev/api/user/spotify/auth";
-
 export default function handler(req, res) {
-  // var state = generateRandomString(16);
   const scope = "user-read-currently-playing";
 
   res.redirect(
     "https://accounts.spotify.com/authorize?" +
       new URLSearchParams({
         response_type: "code",
-        client_id: client_id,
+        client_id: process.env.SPOTIFY_CLIENT_ID as string,
         scope: scope,
-        redirect_uri: redirect_uri,
-        //   state: state,
+        redirect_uri: `https://${req.headers["x-forwarded-host"]}/api/user/spotify/auth`,
       })
   );
 }
