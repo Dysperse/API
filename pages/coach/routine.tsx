c72018851b8e1e7b547885968cd19f9d61e5c37a
Manu <manusvathgurudath@gmail.com>
2023-08-07 20:26:08 +0000

    W

--- pages/coach/routine.tsx
+++ pages/coach/routine.tsx
@@ -190,7 +190,7 @@ export default function Routine() {
       )}
       {slide >= filteredGoals?.length && (
         <motion.div
-          initial={{ opacity: 0, scale: 3 }}
+          initial={{ opacity: 0, scale: 0 }}
           animate={{ opacity: 1, scale: 1 }}
         >
           <Box
