d9e4c29c2a5563c4a68f3ef9d7ec2a8dc1c5a5b0
Manu <manusvathgurudath@gmail.com>
2023-08-07 23:52:52 +0000

    W

--- components/Tasks/Task/DatePicker.tsx
+++ components/Tasks/Task/DatePicker.tsx
@@ -1,22 +1,9 @@
-import { Puller } from "@/components/Puller";
 import { useSession } from "@/lib/client/session";
 import { useColor, useDarkMode } from "@/lib/client/useColor";
 import { toastStyles } from "@/lib/client/useTheme";
-import {
-  Box,
-  Button,
-  Icon,
-  IconButton,
-  Menu,
-  MenuItem,
-  SwipeableDrawer,
-  Tooltip,
-  Typography,
-  useMediaQuery,
-} from "@mui/material";
+import { Box, Button, Icon, SwipeableDrawer, TextField } from "@mui/material";
 import dayjs from "dayjs";
-import { AnimatePresence, motion } from "framer-motion";
-import React, { cloneElement, useMemo, useState } from "react";
+import React, { cloneElement, useState } from "react";
 import DatePicker from "react-calendar";
 import { toast } from "react-hot-toast";
 
@@ -31,86 +18,17 @@ const SelectDateModal: any = React.memo(function SelectDateModal({
   const isDark = useDarkMode(session.darkMode);
   const palette = useColor(session.themeColor, isDark);
 
-  const hasTime =
-    date && (dayjs(date).hour() !== 0 || dayjs(date).minute() !== 0);
   const [open, setOpen] = useState<boolean>(false);
+  const [timeOpen, setTimeOpen] = useState(false);
+
   const today = new Date(dayjs().startOf("day").toISOString());
 
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 
-  const handleClick = (event) => setAnchorEl(event.currentTarget);
+  const handleClick = (event) => setTimeOpen((s) => !s);
   const handleClose = () => setAnchorEl(null);
-  const isMobile = useMediaQuery("(max-width: 600px)");
 
-  let trigger = useMemo(
-    () => (
-      <Tooltip title="Date (alt • f)" placement="top">
-        <AnimatePresence mode="wait" initial={false}>
-          <motion.div
-            key={dayjs(date).format("MMMM D h:mm a")}
-            initial={{ scale: 0.5, opacity: 0 }}
-            animate={{ scale: 1, opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{
-              duration: 0.05,
-              type: "spring",
-              stiffness: 260,
-              damping: 20,
-            }}
-            style={{
-              marginLeft: "auto",
-            }}
-          >
-            <IconButton
-              id="dateModal"
-              size="small"
-              ref={ref}
-              disableRipple
-              sx={{
-                ...styles(palette, Boolean(date)),
-                gap: 1,
-                mr: 1,
-                ...(!isMobile && { borderRadius: 99, px: 1 }),
-              }}
-              onClick={() => setOpen(!open)}
-            >
-              <Icon>today</Icon>
-              <Typography
-                sx={{
-                  fontSize: hasTime ? "13px" : "15px",
-                  ...(hasTime && { mt: -0.4 }),
-                  display: { xs: "none", sm: "inline-flex" },
-                  textAlign: "left",
-                  justifyContent: "flex-start",
-                  alignItems: "flex-start",
-                  flexDirection: "column",
-                }}
-              >
-                {date && dayjs(date).format("MMMM D")}
-                {hasTime && (
-                  <Typography
-                    variant="body2"
-                    sx={{
-                      fontSize: "11px",
-                      mt: -0.5,
-                      mb: -0.5,
-                    }}
-                  >
-                    {dayjs(date).format("h:mm a")}
-                  </Typography>
-                )}
-              </Typography>
-            </IconButton>
-          </motion.div>
-        </AnimatePresence>
-      </Tooltip>
-    ),
-    [date, isMobile, open, palette, ref, styles, hasTime]
-  );
-
-  if (children) {
-    trigger = cloneElement(children, { onClick: () => setOpen(true) });
-  }
+  const trigger = cloneElement(children, { onClick: () => setOpen(true) });
 
   return (
     <>
@@ -127,88 +45,47 @@ const SelectDateModal: any = React.memo(function SelectDateModal({
           },
         }}
       >
-        <Menu
-          anchorEl={anchorEl}
-          open={Boolean(anchorEl)}
-          onClose={handleClose}
-        >
-          <Box sx={{ gap: 1, display: "flex" }}>
-            <Box sx={{ flexGrow: 1 }}>
-              {[...Array(12).keys()].map((i) => (
-                <MenuItem
-                  selected={i === dayjs(date).hour() % 12}
-                  onClick={() => {
-                    setDate(
-                      dayjs(date)
-                        .hour(i + (dayjs(date).hour() % 12))
-                        .toDate()
-                    );
-                  }}
-                  key={i}
-                >
-                  {i % 12 || 12}
-                </MenuItem>
-              ))}
-            </Box>
-            <Box sx={{ flexGrow: 1 }}>
-              {[...Array(12).keys()].map((i) => (
-                <MenuItem
-                  selected={i * 5 === dayjs(date).minute()}
-                  onClick={() => {
-                    setDate(
-                      dayjs(date)
-                        .minute(i * 5)
-                        .toDate()
-                    );
-                  }}
-                  key={i}
-                >
-                  {(i * 5).toString().padStart(2, "0")}
-                </MenuItem>
-              ))}
-            </Box>
-            <Box sx={{ flexGrow: 1 }}>
-              {[...Array(2).keys()].map((i) => (
-                <MenuItem
-                  selected={
-                    i === 0 ? dayjs(date).hour() < 12 : dayjs(date).hour() >= 12
-                  }
-                  onClick={() => {
-                    setDate(
-                      dayjs(date)
-                        .hour(dayjs(date).hour() + (i === 0 ? -12 : 12))
-                        .toDate()
-                    );
-                  }}
-                  key={i}
-                >
-                  {i === 0 ? "AM" : "PM"}
-                </MenuItem>
-              ))}
-            </Box>
+        {timeOpen ? (
+          <Box sx={{ p: 2 }}>
+            <TextField
+              type="time"
+              value={dayjs(date).format("HH:mm")}
+              onChange={(e) => {
+                const [hours, minutes] = e.target.value.split(":");
+                const roundedMinutes = Math.round(parseInt(minutes) / 5) * 5; // Round minutes to nearest 5
+                setDate(
+                  dayjs(date)
+                    .set("hour", parseInt(hours))
+                    .set("minute", roundedMinutes)
+                );
+              }}
+              inputProps={{
+                step: "600",
+              }}
+            />
           </Box>
-        </Menu>
-        <Puller sx={{ mb: -1 }} showOnDesktop />
-        <DatePicker
-          calendarType="US"
-          value={new Date(date)}
-          onChange={(e: any) => {
-            setDate(e);
-            toast(
-              <span>
-                <b>{dayjs(date).format("dddd, MMMM D")}</b>{" "}
-                {dayjs(date).format("HHmm") !== "0000" && (
-                  <>
-                    {" "}
-                    at <b>{dayjs(date).format("h:mm A")}</b>
-                  </>
-                )}
-              </span>,
-              toastStyles
-            );
-            setTimeout(() => setOpen(false), 50);
-          }}
-        />
+        ) : (
+          <DatePicker
+            calendarType="US"
+            value={new Date(date)}
+            onChange={(e: any) => {
+              setDate(e);
+              toast(
+                <span>
+                  <b>{dayjs(date).format("dddd, MMMM D")}</b>{" "}
+                  {dayjs(date).format("HHmm") !== "0000" && (
+                    <>
+                      {" "}
+                      at <b>{dayjs(date).format("h:mm A")}</b>
+                    </>
+                  )}
+                </span>,
+                toastStyles
+              );
+              setTimeout(() => setOpen(false), 50);
+            }}
+          />
+        )}
         <Box
           sx={{
             mt: -1,
