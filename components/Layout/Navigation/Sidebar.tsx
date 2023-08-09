99131fd6ae130e356e2876d47a618a4cc3b2201b
Manu <manusvathgurudath@gmail.com>
2023-08-09 07:44:53 +0000

    W

--- components/Layout/Navigation/Sidebar.tsx
+++ components/Layout/Navigation/Sidebar.tsx
@@ -317,7 +317,7 @@ export function Sidebar() {
             }
           >
             <Tooltip title={session.property.profile.name} placement="right">
-              <span className={`material-symbols-outlined`}>group</span>
+              <span className={`material-symbols-outlined`}>groups</span>
             </Tooltip>
           </Box>
         </GroupModal>
