f6fa338e898f8aff835109169ba43f04b9b40e29
Manu <manusvathgurudath@gmail.com>
2023-08-09 16:53:10 +0000

    timer 🤡🤡

--- components/Tasks/Layout/index.tsx
+++ components/Tasks/Layout/index.tsx
@@ -13,6 +13,7 @@ import {
   AppBar,
   Box,
   Button,
+  CircularProgress,
   Divider,
   Drawer,
   Fade,
@@ -21,6 +22,8 @@ import {
   IconButton,
   ListItemButton,
   ListItemText,
+  Menu,
+  MenuItem,
   SwipeableDrawer,
   TextField,
   Toolbar,
@@ -34,12 +37,14 @@ import { useRouter } from "next/router";
 import {
   createContext,
   memo,
+  useCallback,
   useContext,
   useEffect,
   useMemo,
   useRef,
   useState,
 } from "react";
+import Draggable from "react-draggable";
 import { toast } from "react-hot-toast";
 import { useHotkeys } from "react-hotkeys-hook";
 import { mutate } from "swr";
@@ -145,6 +150,257 @@ const buttonStyles = (palette, condition: boolean) => ({
         },
       }),
 });
+
+const FocusTimer = () => {
+  const session = useSession();
+  const palette = useColor(session.user.color, useDarkMode(session.darkMode));
+
+  const styles = {
+    container: {
+      display: "flex",
+      alignItems: "center",
+      flexDirection: "column",
+      justifyContent: "center",
+      padding: 2,
+      width: 200,
+      height: 200,
+      borderRadius: 999,
+      backdropFilter: "blur(10px)",
+      background: addHslAlpha(palette[4], 0.4),
+      boxShadow: `0 0 0 5px inset ${addHslAlpha(palette[8], 0.4)}`,
+    },
+    dragHandle: {
+      cursor: "move",
+    },
+  };
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [open, setOpen] = useState(true);
+  const [timeInMinutes, setTimeInMinutes] = useState(25);
+  const [isTimerPaused, setIsTimerPaused] = useState(false);
+  const [isTimerRunning, setIsTimerRunning] = useState(false);
+  const [startTime, setStartTime] = useState<any>(null);
+  const [pausedTime, setPausedTime] = useState<any>(0);
+  const [elapsedTime, setElapsedTime] = useState(0);
+  const [isTimerDone, setIsTimerDone] = useState(false);
+
+  const alarmAudio = useMemo(() => new Audio("/sfx/alarm_gentle.wav"), []);
+
+  const handleTimerComplete = useCallback(() => {
+    setIsTimerDone(true);
+    setIsTimerRunning(false);
+    setStartTime(0);
+    setElapsedTime(0);
+    setIsTimerPaused(false);
+    setPausedTime(0);
+    alarmAudio.play();
+  }, [alarmAudio]);
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleTimeSelect = (time) => {
+    setTimeInMinutes(time);
+    handleMenuClose();
+  };
+
+  const handleStartTimer = () => {
+    setStartTime(Date.now());
+    setIsTimerRunning(true);
+  };
+
+  const handlePauseTimer = () => {
+    if (isTimerRunning) {
+      setIsTimerPaused(true);
+      setPausedTime(Date.now() - startTime);
+      setIsTimerRunning(false);
+    }
+  };
+
+  const handleResumeTimer = () => {
+    if (isTimerPaused) {
+      setIsTimerPaused(false);
+      setStartTime(Date.now() - pausedTime);
+      setIsTimerRunning(true);
+    }
+  };
+  const timerValue = Math.max(0, timeInMinutes * 60 * 1000 - elapsedTime);
+  const minutes = Math.floor(timerValue / 60000);
+  const seconds = Math.floor((timerValue % 60000) / 1000);
+
+  const handleStopTimer = () => {
+    setIsTimerRunning(false);
+    setStartTime(0);
+    setElapsedTime(0);
+    setIsTimerPaused(false);
+    setPausedTime(0);
+
+    alarmAudio.pause();
+    alarmAudio.currentTime = 0;
+  };
+
+  const updateElapsedTime = useCallback(() => {
+    if (isTimerRunning) {
+      const currentTime = Date.now();
+      const diff = currentTime - startTime;
+      setElapsedTime(diff);
+
+      if (timerValue <= 0) {
+        handleTimerComplete();
+      }
+    }
+  }, [handleTimerComplete, startTime, isTimerRunning, timerValue]);
+
+  useEffect(() => {
+    if (isTimerRunning) {
+      const animationFrameId = requestAnimationFrame(updateElapsedTime);
+      return () => {
+        cancelAnimationFrame(animationFrameId);
+      };
+    }
+  }, [isTimerRunning, updateElapsedTime]);
+
+  return !open ? (
+    <></>
+  ) : (
+    <Draggable>
+      <Box
+        sx={{
+          position: "fixed",
+          display: "none",
+          ".priorityMode &": {
+            display: "block",
+          },
+          bottom: 20,
+          left: "50%",
+          transform: "translateX(-50%)",
+          zIndex: 99999,
+          "& #close": {
+            display: "none",
+          },
+          "&:hover #close": {
+            display: "flex",
+          },
+        }}
+      >
+        <IconButton
+          onClick={() => setOpen(false)}
+          id="close"
+          sx={{
+            position: "absolute",
+            top: 0,
+            background: palette[3],
+            right: 0,
+            mt: -2,
+            zIndex: 999999999999999,
+            mr: -1,
+          }}
+        >
+          <Icon>close</Icon>
+        </IconButton>
+        <Box id="focus-timer" sx={styles.container}>
+          {isTimerDone ? (
+            <>
+              <Typography className="font-heading" variant="h3">
+                Time&apos;s up!
+              </Typography>
+              <Button
+                variant="contained"
+                onClick={() => {
+                  setIsTimerDone(false);
+                }}
+              >
+                <Icon>stop</Icon>
+              </Button>
+            </>
+          ) : (
+            <>
+              <CircularProgress
+                variant="determinate"
+                value={(timerValue / (timeInMinutes * 60 * 1000)) * 100}
+                size={200}
+                thickness={1}
+                sx={{
+                  position: "absolute",
+                  top: 0,
+                  "&, & *": {
+                    transition: "none!important",
+                  },
+                  left: 0,
+                }}
+              />
+              <Typography variant="h2" className="font-heading">
+                {minutes.toString().padStart(2, "0")}:
+                {seconds.toString().padStart(2, "0")}
+              </Typography>
+              <Box sx={{ display: "flex", gap: 1 }}>
+                <Menu
+                  sx={{ zIndex: 999999 }}
+                  anchorEl={anchorEl}
+                  open={Boolean(anchorEl)}
+                  onClose={handleMenuClose}
+                >
+                  <MenuItem onClick={() => handleTimeSelect(0.1)}>
+                    1 minute
+                  </MenuItem>
+                  <MenuItem onClick={() => handleTimeSelect(1)}>
+                    1 minute
+                  </MenuItem>
+                  <MenuItem onClick={() => handleTimeSelect(25)}>
+                    25 minutes
+                  </MenuItem>
+                  <MenuItem onClick={() => handleTimeSelect(30)}>
+                    30 minutes
+                  </MenuItem>
+                  <MenuItem onClick={() => handleTimeSelect(45)}>
+                    45 minutes
+                  </MenuItem>
+                  <MenuItem onClick={() => handleTimeSelect(60)}>
+                    1 hour
+                  </MenuItem>
+                </Menu>
+                {startTime > 0 ? (
+                  <Button
+                    size="small"
+                    variant="contained"
+                    onClick={
+                      isTimerPaused ? handleResumeTimer : handlePauseTimer
+                    }
+                  >
+                    <Icon>{!isTimerPaused ? "pause" : "play_arrow"}</Icon>
+                  </Button>
+                ) : (
+                  <Button
+                    size="small"
+                    variant="contained"
+                    onClick={handleMenuOpen}
+                  >
+                    <Icon>expand_more</Icon>
+                  </Button>
+                )}
+                {(isTimerPaused || !isTimerRunning) && (
+                  <Button
+                    size="small"
+                    variant="contained"
+                    color="primary"
+                    onClick={isTimerPaused ? handleStopTimer : handleStartTimer}
+                  >
+                    <Icon>{isTimerPaused ? "stop" : "play_arrow"}</Icon>
+                  </Button>
+                )}
+              </Box>
+            </>
+          )}
+        </Box>
+      </Box>
+    </Draggable>
+  );
+};
 
 function BulkCompletion() {
   const session = useSession();
@@ -443,7 +699,7 @@ export function TasksLayout({
               </Link>
             ))}
           </Box>
-
+          <FocusTimer />
           <Box
             sx={{
               transition: "all .2s",
