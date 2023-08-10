30d5852dd6b6d02bcd259a3a5cc15e444eccca46
Manu <manusvathgurudath@gmail.com>
2023-08-10 08:41:30 +0000

    W

--- components/Tasks/Layout/widgets/Weather.tsx
+++ components/Tasks/Layout/widgets/Weather.tsx
@@ -7,6 +7,7 @@ import {
   IconButton,
   Typography,
 } from "@mui/material";
+import { motion } from "framer-motion";
 import { cloneElement, useEffect, useState } from "react";
 import Draggable from "react-draggable";
 import { Sparklines, SparklinesLine, SparklinesSpots } from "react-sparklines";
@@ -65,129 +66,165 @@ export function WeatherWidget({ children }) {
               position: "fixed",
               bottom: "40px",
               left: "40px",
-              borderRadius: 5,
-              overflow: "hidden",
-              p: 3,
-              width: "300px",
-              "& .close": {
-                display: "none",
-              },
-              "&:hover .close": {
-                display: "flex",
-              },
-              background: palette[3],
-              ...(weatherData && {
-                color:
-                  weatherCodes[weatherData.current_weather.weathercode][
-                    isNight() ? "night" : "day"
-                  ].textColor,
-                background: `linear-gradient(${
-                  weatherCodes[weatherData.current_weather.weathercode][
-                    isNight() ? "night" : "day"
-                  ].backgroundGradient[0]
-                },${
-                  weatherCodes[weatherData.current_weather.weathercode][
-                    isNight() ? "night" : "day"
-                  ].backgroundGradient[1]
-                })`,
-              }),
             }}
           >
-            <IconButton
-              onClick={() => {
-                setOpen(false);
-                setWeatherData(null);
-              }}
-              className="close"
-              sx={{
-                position: "absolute",
-                top: 0,
-                background: "rgba(255,255,255,.1)!important",
-                color: "#fff!important",
-                right: 0,
-                m: 1,
-              }}
-              size="small"
+            <motion.div
+              initial={{ scale: 0, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
             >
-              <Icon>close</Icon>
-            </IconButton>
-            <Typography sx={{ display: "flex", gap: 2, alignItems: "center" }}>
-              <Icon className="outlined">location_on</Icon>
-              <span>
-                {locationData
-                  ? `${locationData?.address?.city}, ${locationData?.address?.state}`
-                  : "Loading..."}
-              </span>
-            </Typography>
-            {!weatherData ? (
-              <CircularProgress sx={{ mt: 2 }} />
-            ) : (
-              <>
-                <Box sx={{ display: "flex" }}>
-                  <Typography
-                    variant="h1"
-                    className="font-heading"
-                    flexGrow={1}
-                  >
-                    {weatherData.current_weather.temperature}°
-                  </Typography>
-                  <picture>
-                    <img
-                      src={
-                        isNight()
-                          ? weatherCodes[
-                              weatherData.current_weather.weathercode
-                            ].night.image
-                          : weatherCodes[
-                              weatherData.current_weather.weathercode
-                            ].day.image
-                      }
-                      style={{
-                        ...(weatherCodes[
-                          weatherData.current_weather.weathercode
-                        ][isNight() ? "night" : "day"].textColor == "#000" && {
-                          filter: "invert(1)",
-                        }),
-                      }}
-                      alt="Weather Icon"
-                    />
-                  </picture>
-                </Box>
-                <Typography variant="h5">
-                  {isNight()
-                    ? weatherCodes[weatherData.current_weather.weathercode]
-                        .night.description
-                    : weatherCodes[weatherData.current_weather.weathercode].day
-                        .description}
-                </Typography>
-                <Sparklines
-                  data={weatherData.hourly.temperature_2m}
-                  style={{
-                    position: "absolute",
-                    bottom: 0,
-                    zIndex: -1,
-                    opacity: 0.2,
-                    left: 0,
-                    width: "100%",
-                  }}
-                >
-                  <SparklinesSpots style={{ display: "none" }} />
-                  <SparklinesLine
-                    style={{
-                      fill: weatherCodes[
-                        weatherData.current_weather.weathercode
-                      ][isNight() ? "night" : "day"].textColor,
-                      strokeWidth: 2,
-                    }}
-                    color={
+              <Box
+                sx={{
+                  borderRadius: 5,
+                  overflow: "hidden",
+                  position: "relative",
+                  p: 3,
+                  width: "300px",
+                  maxWidth: "300px",
+                  minWidth: "300px",
+                  "& .close": {
+                    display: "none",
+                  },
+                  "&:hover .close": {
+                    display: "flex",
+                  },
+                  background: palette[3],
+                  ...(weatherData && {
+                    color:
                       weatherCodes[weatherData.current_weather.weathercode][
                         isNight() ? "night" : "day"
-                      ].textColor
-                    }
-                  />
-                </Sparklines>
-              </>
-            )}
+                      ].textColor,
+                    background: `linear-gradient(${
+                      weatherCodes[weatherData.current_weather.weathercode][
+                        isNight() ? "night" : "day"
+                      ].backgroundGradient[0]
+                    },${
+                      weatherCodes[weatherData.current_weather.weathercode][
+                        isNight() ? "night" : "day"
+                      ].backgroundGradient[1]
+                    })`,
+                  }),
+                }}
+              >
+                <IconButton
+                  onClick={() => {
+                    setOpen(false);
+                    setWeatherData(null);
+                  }}
+                  className="close"
+                  sx={{
+                    position: "absolute",
+                    top: 0,
+                    background: "rgba(255,255,255,.1)!important",
+                    color: "#fff!important",
+                    right: 0,
+                    m: 1,
+                  }}
+                  size="small"
+                >
+                  <Icon>close</Icon>
+                </IconButton>
+                <Typography
+                  sx={{ display: "flex", gap: 2, alignItems: "center" }}
+                >
+                  <Icon className="outlined">location_on</Icon>
+                  <span>
+                    {locationData
+                      ? `${locationData?.address?.city}, ${locationData?.address?.state}`
+                      : "Loading..."}
+                  </span>
+                </Typography>
+                {!weatherData ? (
+                  <CircularProgress sx={{ mt: 2 }} />
+                ) : (
+                  <>
+                    <motion.div
+                      initial={{ y: 10, opacity: 0 }}
+                      animate={{ y: 0, opacity: 1 }}
+                      transition={{ delay: 0.5 }}
+                    >
+                      <Box sx={{ display: "flex" }}>
+                        <Typography
+                          variant="h1"
+                          className="font-heading"
+                          flexGrow={1}
+                        >
+                          {weatherData.current_weather.temperature}°
+                        </Typography>
+                        <picture>
+                          <img
+                            src={
+                              isNight()
+                                ? weatherCodes[
+                                    weatherData.current_weather.weathercode
+                                  ].night.image
+                                : weatherCodes[
+                                    weatherData.current_weather.weathercode
+                                  ].day.image
+                            }
+                            style={{
+                              ...(weatherCodes[
+                                weatherData.current_weather.weathercode
+                              ][isNight() ? "night" : "day"].textColor ==
+                                "#000" && {
+                                filter: "invert(1)",
+                              }),
+                            }}
+                            alt="Weather Icon"
+                          />
+                        </picture>
+                      </Box>
+                    </motion.div>
+                    <motion.div
+                      initial={{ y: -10, opacity: 0 }}
+                      animate={{ y: 0, opacity: 1 }}
+                      transition={{ delay: 0.8 }}
+                    >
+                      <Typography variant="h5">
+                        {isNight()
+                          ? weatherCodes[
+                              weatherData.current_weather.weathercode
+                            ].night.description
+                          : weatherCodes[
+                              weatherData.current_weather.weathercode
+                            ].day.description}
+                      </Typography>
+                    </motion.div>
+                    <motion.div
+                      initial={{ opacity: 0 }}
+                      transition={{ delay: 1 }}
+                      animate={{ opacity: 1 }}
+                    >
+                      <Sparklines
+                        data={weatherData.hourly.temperature_2m}
+                        style={{
+                          position: "absolute",
+                          left: 0,
+                          bottom: 0,
+                          zIndex: 0,
+                          opacity: 0.2,
+                          width: "100%",
+                        }}
+                      >
+                        <SparklinesSpots style={{ display: "none" }} />
+                        <SparklinesLine
+                          style={{
+                            fill: weatherCodes[
+                              weatherData.current_weather.weathercode
+                            ][isNight() ? "night" : "day"].textColor,
+                            strokeWidth: 2,
+                          }}
+                          color={
+                            weatherCodes[
+                              weatherData.current_weather.weathercode
+                            ][isNight() ? "night" : "day"].textColor
+                          }
+                        />
+                      </Sparklines>
+                    </motion.div>
+                  </>
+                )}
+              </Box>
+            </motion.div>
           </Box>
         </Draggable>
       )}
