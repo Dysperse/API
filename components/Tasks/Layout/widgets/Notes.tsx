abe46ac0bcad7d1dcc3555accea7d0d46bcfb2d1
Manu <manusvathgurudath@gmail.com>
2023-08-11 18:26:05 +0000

    W

--- components/Tasks/Layout/widgets/Notes.tsx
+++ components/Tasks/Layout/widgets/Notes.tsx
@@ -1,21 +1,43 @@
-import { Box, TextField } from "@mui/material";
+import { Box, Icon, IconButton, TextField } from "@mui/material";
 import { cloneElement, useState } from "react";
 
 interface Note {
   value: string;
   color: "#feff9c" | "#ff65a3" | "#7afcff" | "#fff740";
+  deleted: boolean;
 }
 
 export function Notes({ children }) {
   const [notes, setNotes] = useState<Note[]>([]);
-  const trigger = cloneElement(children, {
-    onClick: () => setNotes((n) => [...n, { value: "", color: "#feff9c" }]),
-  });
+
+  const addNote = () => {
+    setNotes((n) => [...n, { value: "", color: "#feff9c", deleted: false }]);
+  };
+
+  const updateNoteValue = (index, value) => {
+    const updatedNotes = [...notes];
+    updatedNotes[index].value = value;
+    setNotes(updatedNotes);
+  };
+
+  const updateNoteColor = (index, color) => {
+    const updatedNotes = [...notes];
+    updatedNotes[index].color = color;
+    setNotes(updatedNotes);
+  };
+
+  const deleteNote = (index) => {
+    const updatedNotes = [...notes];
+    updatedNotes[index].deleted = true;
+    setNotes(updatedNotes);
+  };
+
+  const filteredNotes = notes.filter((note) => !note.deleted);
 
   return (
     <>
-      {trigger}
-      {notes.map((note, index) => (
+      {cloneElement(children, { onClick: addNote })}
+      {filteredNotes.map((note, index) => (
         <Box
           key={index}
           className="drag-widget"
@@ -24,37 +46,40 @@ export function Notes({ children }) {
             top: 20,
             width: 200,
             borderRadius: 5,
-            right: 0,
+            left: 0,
             background: note.color,
           }}
         >
           <TextField
+            placeholder="Add note..."
             multiline
             variant="standard"
             InputProps={{
               disableUnderline: true,
               sx: {
                 p: 2,
+                pb: 1,
+                pt: 3,
                 color: "#000",
                 fontWeight: "900",
               },
             }}
             defaultValue={note.value}
-            onBlur={(e) => {
-              let x = notes;
-              x[index].value = e.target.value;
-              setNotes(x);
-            }}
+            onBlur={(e) => updateNoteValue(index, e.target.value)}
           />
-          <Box sx={{ display: "flex", gap: 1.5, px: 2, pb: 2 }}>
+          <Box
+            sx={{
+              display: "flex",
+              gap: 1.5,
+              px: 2,
+              pb: 2,
+              alignItems: "center",
+            }}
+          >
             {["#feff9c", "#ff65a3", "#7afcff", "#fff740"].map((color) => (
               <Box
                 key={color}
-                onClick={() => {
-                  let x = notes;
-                  x[index].color = color as Note["color"];
-                  setNotes(x);
-                }}
+                onClick={() => updateNoteColor(index, color as Note["color"])}
                 sx={{
                   width: 20,
                   height: 20,
@@ -65,6 +90,12 @@ export function Notes({ children }) {
                 }}
               />
             ))}
+            <IconButton
+              sx={{ ml: "auto", color: "#000!important" }}
+              onClick={() => deleteNote(index)}
+            >
+              <Icon>delete</Icon>
+            </IconButton>
           </Box>
         </Box>
       ))}
