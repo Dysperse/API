7fd3a6dfe0cbcb7dedac43fa0d405cdd442d29a4
Manu G <manusvathgurudath@gmail.com>
2023-08-23 18:10:15 +0000

    W

--- components/EmojiPicker/index.tsx
+++ components/EmojiPicker/index.tsx
@@ -1,16 +1,9 @@
 import { useSession } from "@/lib/client/session";
 import { useColor, useDarkMode } from "@/lib/client/useColor";
-import { Box, SwipeableDrawer, TextField, Typography } from "@mui/material";
-import { SearchIndex, init } from "emoji-mart";
-import React, {
-  cloneElement,
-  useCallback,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
-import { Puller } from "../Puller";
-import { EmojiButton } from "./button";
+import data from "@emoji-mart/data";
+import Picker from "@emoji-mart/react";
+import { Box, SwipeableDrawer } from "@mui/material";
+import React, { cloneElement, useCallback, useState } from "react";
 
 export function debounce(func: (...args: any[]) => void, delay: number) {
   let timeout;
@@ -35,28 +28,13 @@ const EmojiPicker = React.memo(function EmojiPicker({
 }) {
   const session = useSession();
   const [open, setOpen] = useState<boolean>(false);
-  const [results, setResults] = useState<Array<any>>([]);
-  const [inputValue, setInputValue] = useState("");
-  const palette = useColor(session.themeColor, useDarkMode(session.darkMode));
+  const isDark = useDarkMode(session.darkMode);
+  const palette = useColor(session.themeColor, isDark);
 
   const handleOpen = useCallback(() => setOpen(true), []);
   const handleClose = () => setOpen(false);
 
-  const handleSearch = useCallback(async function handleSearch(value: any) {
-    const data = (await import("@emoji-mart/data")).default;
-    init({ data });
-    const emojis = await SearchIndex.search(value || "face");
-    setResults(emojis);
-  }, []);
-
   const trigger = cloneElement(children, { onClick: handleOpen });
-
-  useEffect(() => {
-    if (inputValue.length === 0) {
-      handleSearch("face");
-      setInputValue("");
-    }
-  }, [inputValue.length, handleSearch]);
 
   function handleEmojiSelect(emoji) {
     const code = useNativeEmoji
@@ -65,12 +43,6 @@ const EmojiPicker = React.memo(function EmojiPicker({
     setEmoji(code);
     handleClose();
   }
-
-  const ref: any = useRef();
-
-  useEffect(() => {
-    if (open) setTimeout(() => ref?.current?.focus(), 100);
-  }, [open]);
 
   return (
     <>
@@ -82,79 +54,22 @@ const EmojiPicker = React.memo(function EmojiPicker({
         anchor="bottom"
         sx={{
           zIndex: 99999999999,
+          textAlign: "center",
+        }}
+        PaperProps={{
+          sx: {
+            width: "auto",
+            minWidth: "auto",
+            background: "transparent",
+          },
         }}
       >
-        <Puller showOnDesktop />
-        <Box sx={{ px: 2 }}>
-          <TextField
-            type="text"
-            size="small"
-            variant="standard"
-            sx={{ mb: 2 }}
-            InputProps={{
-              disableUnderline: true,
-              sx: {
-                px: 2,
-                py: 1,
-                borderRadius: 3,
-                background: palette[2],
-                "&:focus-within": {
-                  background: palette[3],
-                },
-              },
-            }}
-            autoFocus
-            placeholder="Search..."
-            inputRef={ref}
-            onChange={(e: any) => {
-              setInputValue(e.target.value);
-              handleSearch(e.target.value);
-            }}
-            value={inputValue}
+        <Box sx={{ display: "flex", justifyContent: "center", pb: 5 }}>
+          <Picker
+            data={data}
+            onEmojiSelect={handleEmojiSelect}
+            theme={isDark ? "dark" : "light"}
           />
-          <Box
-            sx={{
-              display: "flex",
-              flexWrap: "wrap",
-              maxHeight: "350px",
-              overflowY: "scroll",
-            }}
-          >
-            {(!results || (results && results.length === 0)) &&
-              inputValue.length !== 0 && (
-                <Box
-                  sx={{
-                    mb: 2,
-                    p: 3,
-                    background: "rgba(200,200,200,.2)",
-                    borderRadius: 3,
-                    width: "100%",
-                    fontSize: "14px",
-                    fontWeight: 700,
-                    textAlign: "center",
-                  }}
-                >
-                  <picture>
-                    <img
-                      src="https://cdn.jsdelivr.net/npm/emoji-datasource-apple/img/apple/64/1f62d.png"
-                      alt="Crying emoji"
-                    />
-                  </picture>
-                  <Typography sx={{ mt: 1, fontWeight: 700 }}>
-                    No results found
-                  </Typography>
-                </Box>
-              )}
-            {results &&
-              results.map((thisEmoji) => (
-                <EmojiButton
-                  handleEmojiSelect={handleEmojiSelect}
-                  selectedEmoji={emoji}
-                  emoji={thisEmoji}
-                  key={thisEmoji.id}
-                />
-              ))}
-          </Box>
         </Box>
       </SwipeableDrawer>
     </>
