4fc579bea4f8a092c9d52f82ab61bc531a0e5c46
Manu G <manusvathgurudath@gmail.com>
2023-09-10 11:04:47 -0700

    Update scan.ts

--- pages/api/property/inventory/items/scan.ts
+++ pages/api/property/inventory/items/scan.ts
@@ -12,7 +12,7 @@ async function query(url) {
   const data = new Buffer(url.split(",")[1], "base64");
 
   const response = await fetch(
-    "https://api-inference.huggingface.co/models/facebook/detr-resnet-50",
+    "https://api-inference.huggingface.co/models/google/vit-base-patch16-224",
     {
       headers: {
         Authorization: "Bearer " + process.env.AI_AUTH_TOKEN,
