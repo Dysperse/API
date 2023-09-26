7878a7ae896e8161e5b1aeb976fdf02eed7f74d5
Manu G <manusvathgurudath@gmail.com>
2023-09-26 00:39:31 +0000

    bruh

--- pages/tasks/home.tsx
+++ pages/tasks/home.tsx
@@ -1,15 +1,18 @@
 import { Navbar } from "@/components/Navbar";
 import { MenuChildren } from "@/components/Tasks/Layout";
 import { SearchTasks } from "@/components/Tasks/Layout/SearchTasks";
+import { CreateTask } from "@/components/Tasks/Task/Create";
 import { useSession } from "@/lib/client/session";
 import { useColor, useDarkMode } from "@/lib/client/useColor";
 import {
   Box,
   Icon,
+  IconButton,
   InputAdornment,
   TextField,
   Typography,
 } from "@mui/material";
+import dayjs from "dayjs";
 import { motion } from "framer-motion";
 
 export default function Home() {
@@ -42,36 +45,62 @@ export default function Home() {
           >
             Tasks
           </Typography>
-          <SearchTasks>
-            <TextField
-              variant="standard"
-              placeholder="Search..."
-              InputProps={{
-                readOnly: true,
-                disableUnderline: true,
-                sx: {
-                  background: palette[2],
-                  "&:focus-within": {
-                    background: palette[3],
+          <Box
+            sx={{
+              display: "flex",
+              gap: 2,
+              mb: 2,
+              mt: 0.5,
+              alignItems: "center",
+            }}
+          >
+            <SearchTasks>
+              <TextField
+                variant="standard"
+                placeholder="Search..."
+                InputProps={{
+                  readOnly: true,
+                  disableUnderline: true,
+                  sx: {
+                    background: palette[2],
+                    "&:focus-within": {
+                      background: palette[3],
+                    },
+                    "& *::placeholder": {
+                      color: palette[10],
+                    },
+                    transition: "all .2s",
+                    px: 2,
+                    py: 0.3,
+                    borderRadius: 3,
                   },
-                  "& *::placeholder": {
-                    color: palette[10],
-                  },
-                  transition: "all .2s",
-                  mb: 2,
-                  mt: 0.5,
-                  px: 2,
-                  py: 0.3,
-                  borderRadius: 3,
-                },
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <Icon sx={{ color: palette[9] }}>search</Icon>
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </SearchTasks>
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <Icon sx={{ color: palette[9] }}>search</Icon>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </SearchTasks>
+            {session.permission !== "read-only" && (
+              <CreateTask
+                onSuccess={() => {}}
+                defaultDate={dayjs().startOf("day").toDate()}
+              >
+                <IconButton
+                  sx={{
+                    color: palette[11],
+                    background: palette[2],
+                    "&:active": {
+                      background: palette[3],
+                    },
+                  }}
+                >
+                  <Icon>add</Icon>
+                </IconButton>
+              </CreateTask>
+            )}
+          </Box>
         </Box>
         <MenuChildren />
       </motion.div>
