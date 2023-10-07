8386fd5e53341504be5294c14ff69a27810e2e05
Manu G <manusvathgurudath@gmail.com>
2023-10-07 17:30:14 +0000

    W

--- components/Start/Friend.tsx
+++ components/Start/Friend.tsx
@@ -442,9 +442,12 @@ export const Friend = memo(function Friend({ mutate, friend }: any) {
                     </Typography>
                   </>
                 ) : (
-                  <Typography sx={{ display: "flex", gap: 2, opacity: 0.6 }}>
-                    Active {dayjs(friend.lastActive).fromNow()}
-                  </Typography>
+                  dayjs(friend.lastActive).toISOString() !==
+                    "2023-10-07T17:23:03.871Z" && (
+                    <Typography sx={{ display: "flex", gap: 2, opacity: 0.6 }}>
+                      Active {dayjs(friend.lastActive).fromNow()}
+                    </Typography>
+                  )
                 )}
               </Box>
             </CardContent>
