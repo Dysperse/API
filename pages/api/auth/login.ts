6cab0e9124425258e61ebf8830d86805953e97af
Manu G <manusvathgurudath@gmail.com>
2023-10-15 08:03:26 +0000

    W

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -53,26 +53,29 @@ export async function createSession(id: any, res, ip: any) {
  * @returns {any}
  */
 export default async function handler(req, res) {
-  // FIRST, Validate the captcha
+  if (process.env.NODE_ENV !== "development") {
+    // FIRST, Validate the captcha
 
-  const endpoint = "https://challenges.cloudflare.com/turnstile/v0/siteverify";
-  const secret: any = process.env.CAPTCHA_KEY;
-  const body = `secret=${encodeURIComponent(
-    secret
-  )}&response=${encodeURIComponent(req.body.token)}`;
+    const endpoint =
+      "https://challenges.cloudflare.com/turnstile/v0/siteverify";
+    const secret: any = process.env.CAPTCHA_KEY;
+    const body = `secret=${encodeURIComponent(
+      secret
+    )}&response=${encodeURIComponent(req.body.token)}`;
 
-  const captchaRequest = await fetch(endpoint, {
-    method: "POST",
-    body,
-    headers: {
-      "content-type": "application/x-www-form-urlencoded",
-    },
-  });
+    const captchaRequest = await fetch(endpoint, {
+      method: "POST",
+      body,
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+      },
+    });
 
-  const data = await captchaRequest.json();
+    const data = await captchaRequest.json();
 
-  if (!data.success && data.error !== "internal_error") {
-    return res.status(401).json({ message: "Invalid Captcha" });
+    if (!data.success && data.error !== "internal_error") {
+      return res.status(401).json({ message: "Invalid Captcha" });
+    }
   }
 
   // Get the user's email and password from the request body
