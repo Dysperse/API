953d51a5e6148224c53eb6772bfb97a86a719797
Manu G <manusvathgurudath@gmail.com>
2023-10-22 00:07:26 +0000

    W

--- app/(auth)/auth/client-layout.tsx
+++ app/(auth)/auth/client-layout.tsx
@@ -2,16 +2,17 @@
 
 import { useColor } from "@/lib/client/useColor";
 import { useCustomTheme } from "@/lib/client/useTheme";
-import { ThemeProvider, createTheme } from "@mui/material";
+import { ThemeProvider, createTheme, useMediaQuery } from "@mui/material";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 export function AuthClientLayout({ children }) {
   const palette = useColor("violet", true);
+  const isDark = useMediaQuery("(prefers-color-scheme: dark)");
 
   const userTheme = createTheme(
     useCustomTheme({
-      darkMode: "dark",
+      darkMode: isDark ? "dark" : "light",
       themeColor: "violet",
     })
   );
