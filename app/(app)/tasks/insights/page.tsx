e52a32c0f5315efef117416b18b8f1211cbeb0fb
Manu G <manusvathgurudath@gmail.com>
2023-10-24 07:52:59 +0000

    W

--- app/(app)/tasks/insights/page.tsx
+++ app/(app)/tasks/insights/page.tsx
@@ -2,7 +2,7 @@
 
 import { useSession } from "@/lib/client/session";
 import { useColor, useDarkMode } from "@/lib/client/useColor";
-import { Box, Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import { BarChart } from "@mui/x-charts/BarChart";
 import { LineChart } from "@mui/x-charts/LineChart";
 import { PieChart } from "@mui/x-charts/PieChart";
@@ -48,146 +48,157 @@ export default function Insights() {
       <Typography variant="h2" className="font-heading">
         Insights
       </Typography>
-      <Box>
-        <BarChart
-          xAxis={[
-            {
-              id: "tasks",
-              data: Array.from({ length: 24 }).map((_, i) => i),
-              valueFormatter: (i) =>
-                Array.from({ length: 24 }, (_, index) => {
-                  const hour = index % 12 || 12; // Convert 0 to 12 for 12:00 AM
-                  const period = index < 12 ? "AM" : "PM";
-                  return `${hour} ${period}`;
-                })[i],
-              scaleType: "band",
-              label: "Hour",
-            },
-          ]}
-          bottomAxis={{
-            axisId: "tasks",
-            // disableTicks: true,
-            // tickFontSize: ,
-          }}
-          colors={[palette[8]]}
-          series={[
-            {
-              data: Array.from(
-                { length: 24 },
-                (_, hour) =>
-                  data.filter((d) => dayjs(d.completedAt).hour() === hour)
-                    .length
-              ),
-            },
-          ]}
-          height={300}
-        />
-        <BarChart
-          xAxis={[
-            {
-              id: "tasks",
-              data: Array.from({ length: 7 }).map((_, i) => i),
-              valueFormatter: (i) =>
-                ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"][i],
-              scaleType: "band",
-              label: "Day",
-            },
-          ]}
-          bottomAxis={{
-            axisId: "tasks",
-          }}
-          colors={[palette[8]]}
-          series={[
-            {
-              data: Array.from(
-                { length: 7 },
-                (_, hour) =>
-                  data.filter((d) => dayjs(d.completedAt).day() === hour).length
-              ),
-            },
-          ]}
-          height={300}
-        />
-        <BarChart
-          xAxis={[
-            {
-              id: "tasks",
-              data: Array.from({ length: 12 }).map((_, i) => i),
-              valueFormatter: (i) =>
-                [
-                  "Jan",
-                  "Feb",
-                  "Mar",
-                  "Apr",
-                  "May",
-                  "June",
-                  "Jul",
-                  "Aug",
-                  "Sep",
-                  "Oct",
-                  "Nov",
-                  "Dec",
-                ][i],
-              scaleType: "band",
-              label: "Month",
-            },
-          ]}
-          bottomAxis={{
-            axisId: "tasks",
-          }}
-          colors={[palette[8]]}
-          series={[
-            {
-              data: Array.from(
-                { length: 12 },
-                (_, hour) =>
-                  data.filter((d) => dayjs(d.completedAt).month() === hour)
-                    .length
-              ),
-            },
-          ]}
-          height={300}
-        />
-        <LineChart
-          xAxis={[
-            {
-              data: tasksCompletedByDate.map((d, i) => i),
-              // scaleType: "time",
-              valueFormatter: (i) =>
-                dayjs(tasksCompletedByDate[i].date).format("MMM D"),
-            },
-          ]}
-          series={[
-            {
-              label: "Tasks completed",
-              showMark: false,
-              curve: "catmullRom",
-              data: tasksCompletedByDate.map((d) => d.count),
-            },
-          ]}
-          height={300}
-        />
-        <PieChart
-          series={[
-            {
-              data: [
-                {
-                  id: 0,
-                  value: data?.filter((d) => d.task.pinned).length,
-                  label: "Important",
-                },
-                {
-                  id: 1,
-                  value: data?.filter((d) => !d.task.pinned).length,
-                  label: "Other",
-                },
-              ],
-            },
-          ]}
-          width={400}
-          height={200}
-        />
-      </Box>
+      <Grid container>
+        <Grid item xs={12} sm={6}>
+          <BarChart
+            xAxis={[
+              {
+                id: "tasks",
+                data: Array.from({ length: 24 }).map((_, i) => i),
+                valueFormatter: (i) =>
+                  Array.from({ length: 24 }, (_, index) => {
+                    const hour = index % 12 || 12; // Convert 0 to 12 for 12:00 AM
+                    const period = index < 12 ? "AM" : "PM";
+                    return `${hour} ${period}`;
+                  })[i],
+                scaleType: "band",
+                label: "Hour",
+              },
+            ]}
+            bottomAxis={{
+              axisId: "tasks",
+              // disableTicks: true,
+              // tickFontSize: ,
+            }}
+            colors={[palette[8]]}
+            series={[
+              {
+                data: Array.from(
+                  { length: 24 },
+                  (_, hour) =>
+                    data.filter((d) => dayjs(d.completedAt).hour() === hour)
+                      .length
+                ),
+              },
+            ]}
+            height={300}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <BarChart
+            xAxis={[
+              {
+                id: "tasks",
+                data: Array.from({ length: 7 }).map((_, i) => i),
+                valueFormatter: (i) =>
+                  ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"][i],
+                scaleType: "band",
+                label: "Day",
+              },
+            ]}
+            bottomAxis={{
+              axisId: "tasks",
+            }}
+            colors={[palette[8]]}
+            series={[
+              {
+                data: Array.from(
+                  { length: 7 },
+                  (_, hour) =>
+                    data.filter((d) => dayjs(d.completedAt).day() === hour)
+                      .length
+                ),
+              },
+            ]}
+            height={300}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <BarChart
+            xAxis={[
+              {
+                id: "tasks",
+                data: Array.from({ length: 12 }).map((_, i) => i),
+                valueFormatter: (i) =>
+                  [
+                    "Jan",
+                    "Feb",
+                    "Mar",
+                    "Apr",
+                    "May",
+                    "June",
+                    "Jul",
+                    "Aug",
+                    "Sep",
+                    "Oct",
+                    "Nov",
+                    "Dec",
+                  ][i],
+                scaleType: "band",
+                label: "Month",
+              },
+            ]}
+            bottomAxis={{
+              axisId: "tasks",
+            }}
+            colors={[palette[8]]}
+            series={[
+              {
+                data: Array.from(
+                  { length: 12 },
+                  (_, hour) =>
+                    data.filter((d) => dayjs(d.completedAt).month() === hour)
+                      .length
+                ),
+              },
+            ]}
+            height={300}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <LineChart
+            xAxis={[
+              {
+                data: tasksCompletedByDate.map((d, i) => i),
+                // scaleType: "time",
+                valueFormatter: (i) =>
+                  dayjs(tasksCompletedByDate[i].date).format("MMM D"),
+              },
+            ]}
+            series={[
+              {
+                label: "Tasks completed",
+                showMark: false,
+                curve: "catmullRom",
+                data: tasksCompletedByDate.map((d) => d.count),
+              },
+            ]}
+            height={300}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <PieChart
+            series={[
+              {
+                data: [
+                  {
+                    id: 0,
+                    value: data?.filter((d) => d.task.pinned).length,
+                    label: "Important",
+                  },
+                  {
+                    id: 1,
+                    value: data?.filter((d) => !d.task.pinned).length,
+                    label: "Other",
+                  },
+                ],
+              },
+            ]}
+            width={400}
+            height={200}
+          />
+        </Grid>
+      </Grid>
     </Box>
   ) : (
     "Loading..."
