6c03012e3176910e522c6f91284fd11570ef34ae
Manu G <manusvathgurudath@gmail.com>
2023-10-26 19:00:14 +0000

    W

--- app/(app)/tasks/perspectives/[...view]/loading.tsx
+++ app/(app)/tasks/perspectives/[...view]/loading.tsx
@@ -26,11 +26,13 @@ export default function Loading() {
         }}
       >
         <Skeleton
+          animation="wave"
           variant="rectangular"
           sx={{
             width: "100%",
             height: "100%",
             borderRadius: 5,
+            opacity: 0.7,
           }}
         />
       </Box>
