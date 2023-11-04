59bffdb74500efc45e3fd73479aa6b0cd60479b3
Manu G <manusvathgurudath@gmail.com>
2023-11-04 06:06:10 +0000

    W

--- app/(app)/tasks/stream/[view]/page.tsx
+++ app/(app)/tasks/stream/[view]/page.tsx
@@ -53,7 +53,9 @@ export default function Page() {
         <Virtuoso
           useWindowScroll
           customScrollParent={
-            isMobile ? undefined : document.getElementById("boardContainer")
+            isMobile
+              ? undefined
+              : (document.getElementById("boardContainer") as any)
           }
           data={data}
           itemContent={(_, task) => (
