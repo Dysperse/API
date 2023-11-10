19a4be9a281a31c0190d69e908d1f91186148918
Manu G <manusvathgurudath@gmail.com>
2023-11-10 03:23:24 +0000

    W

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -17,13 +17,71 @@ export const metadata = {
     capable: true,
     title: "Dysperse",
     startupImage: [
-      { media: "2048x2732", url: "/ios-splash/2048x2732.png" },
-      { media: "1668x2224", url: "/ios-splash/1668x2224.png" },
-      { media: "1536x2048", url: "/ios-splash/1536x2048.png" },
-      { media: "1125x2436", url: "/ios-splash/1125x2436.png" },
-      { media: "1242x2208", url: "/ios-splash/1242x2208.png" },
-      { media: "750x1334", url: "/ios-splash/750x1334.png" },
-      { media: "640x1136", url: "/ios-splash/640x1136.png" },
+      {
+        media:
+          "(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)",
+        url: "/ios-splash/2048x2732.png",
+      },
+      {
+        media:
+          "(device-width: 1284px) and (device-height: 2778px) and (-webkit-device-pixel-ratio: 3)",
+        url: "/ios-splash/2048x2732.png",
+      },
+      {
+        media:
+          "(device-width: 1170px) and (device-height: 2532px) and (-webkit-device-pixel-ratio: 3)",
+        url: "/ios-splash/2048x2732.png",
+      },
+      {
+        media:
+          "(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)",
+        url: "/ios-splash/1668x2388.png",
+      },
+      {
+        media:
+          "(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)",
+        url: "/ios-splash/1668x2224.png",
+      },
+      {
+        media:
+          "(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)",
+        url: "/ios-splash/1536x2048.png",
+      },
+      {
+        media:
+          "(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)",
+        url: "/ios-splash/1242x2688.png",
+      },
+      {
+        media:
+          "(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)",
+        url: "/ios-splash/828x1792.png",
+      },
+      {
+        media:
+          "(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)",
+        url: "/ios-splash/1125x2436.png",
+      },
+      {
+        media:
+          "(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3)",
+        url: "/ios-splash/1242x2208.png",
+      },
+      {
+        media:
+          "(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)",
+        url: "/ios-splash/750x1334.png",
+      },
+      {
+        media:
+          "(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)",
+        url: "/ios-splash/640x1136.png",
+      },
+      {
+        media:
+          "(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)",
+        url: "/ios-splash/640x1136.png",
+      },
     ],
   },
 };
