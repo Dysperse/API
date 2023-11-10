49c810fe0c85bcc5cc0b04679258a2cbbfd0c9d1
Manu G <manusvathgurudath@gmail.com>
2023-11-10 06:03:44 +0000

    W

--- app/api/cron/notifications/plan/route.ts
+++ app/api/cron/notifications/plan/route.ts
@@ -1,13 +1,22 @@
 import { DispatchNotification } from "@/lib/server/notification";
 import { prisma } from "@/lib/server/prisma";
+import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
 import { NextRequest } from "next/server";
+dayjs.extend(timezone);
+dayjs.extend(utc);
 
 export async function POST(req: NextRequest) {
   const _subscriptions = await prisma.notificationSettings.findMany({
     where: {
       planDay: true,
     },
-    include: { user: { select: { email: true, timeZone: true } } },
+    select: {
+      pushSubscription: true,
+      planDayHour: true,
+      user: { select: { email: true, timeZone: true } },
+    },
   });
 
   const subscriptions = _subscriptions
@@ -22,21 +31,38 @@ export async function POST(req: NextRequest) {
         return true;
       }
     });
+
   const prompts = [
     "What will you do to make your day impactful?",
     "What are your top three goals for today!?",
     "Let's plan your day!",
     "How will you manage and prioritize your time efficiently today?",
   ];
+  const prompt = prompts[Math.floor(Math.random() * prompts.length)];
 
-  for (const subscription of subscriptions) {
-    await DispatchNotification({
-      actions: [{ action: "plan", title: "👉 Let's go" }],
-      subscription: JSON.stringify(subscription.pushSubscription),
-      title: "Good morning!",
-      body: prompts[Math.floor(Math.random() * prompts.length)],
-    });
-  }
+  const notificationPromises = subscriptions.map(
+    (subscription) =>
+      new Promise((resolve, reject) => {
+        try {
+          if (
+            subscription.planDayHour ===
+            dayjs().tz(subscription.user.timeZone).hour()
+          )
+            resolve(
+              DispatchNotification({
+                actions: [{ action: "plan", title: "👉 Let's go" }],
+                subscription: JSON.stringify(subscription.pushSubscription),
+                title: "Good morning!",
+                body: prompt,
+              })
+            );
+          else resolve("");
+        } catch (e) {
+          reject(e);
+        }
+      })
+  );
 
+  await Promise.allSettled(notificationPromises);
   return Response.json({ success: true, subscriptions: subscriptions });
 }
