1a834fbb4fb3a52d172f5d7a1a2191b27a64c0ba
Manu G <manusvathgurudath@gmail.com>
2023-11-16 08:18:36 +0000

    W

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -43,6 +43,19 @@ export function getAirQualityInfo(index) {
   return result || null; // Return null if no matching category is found
 }
 
+function hslToHex(h: any, s: any, l: any) {
+  l /= 100;
+  const a = (s * Math.min(l, 1 - l)) / 100;
+  const f = (n) => {
+    const k = (n + h / 30) % 12;
+    const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+    return Math.round(255 * color)
+      .toString(16)
+      .padStart(2, "0"); // convert to Hex and prefix "0" if needed
+  };
+  return `#${f(0)}${f(8)}${f(4)}`;
+}
+
 export function Time() {
   const { session } = useSession();
   const isDark = useDarkMode(session.darkMode);
@@ -190,9 +203,19 @@ function Home() {
           ? {
               transition: "background 2s",
               backgroundPosition: "center",
-              background: `url("${patterns[
-                session.user.homePagePattern
-              ]?.replace("[FILL_COLOR]", encodeURIComponent(palette[3]))}")`,
+              background: `url("/api/user/homePagePattern?${new URLSearchParams(
+                {
+                  color: hslToHex(
+                    ...([
+                      ...palette[3]
+                        .replaceAll(/hsl\(|\)|%/g, "")
+                        .split(",")
+                        .map((e) => +e),
+                    ] as any)
+                  ),
+                  pattern: session.user.homePagePattern,
+                }
+              )}")`,
             }
           : undefined
       }
