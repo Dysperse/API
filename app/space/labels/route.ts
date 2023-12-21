bd5fffad2794cf14f1c2fa6d1e53af4281a5e9ec
Manu G <manusvathgurudath@gmail.com>
2023-12-21 02:25:47 +0000

    Refactor GET function in route.ts to fetch inbox items by property ID

--- app/space/labels/route.ts
+++ app/space/labels/route.ts
@@ -1,3 +1,20 @@
-export function GET() {
-  return Response.json({ success: true });
+import { getApiParam, handleApiError } from "@/lib/server/helpers";
+import { prisma } from "@/lib/server/prisma";
+import { NextRequest } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const property = await getApiParam(req, "propertyId", true);
+
+  try {
+    const data = await prisma.inboxItem.findMany({
+      where: {
+        property: { id: property },
+      },
+      orderBy: { id: "desc" },
+    });
+
+    return Response.json(data);
+  } catch (e) {
+    return handleApiError(e);
+  }
 }
