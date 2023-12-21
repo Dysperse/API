e600fe31875ab8275d89079633663c0363994b62
Manu G <manusvathgurudath@gmail.com>
2023-12-21 07:33:37 +0000

    W

---
+++ app/user/spaces/route.ts
@@ -0,0 +1,17 @@
+import { getIdentifiers } from "@/lib/getIdentifiers";
+import { handleApiError } from "@/lib/handleApiError";
+import { prisma } from "@/lib/prisma";
+import { NextRequest } from "next/server";
+
+export async function GET(req: NextRequest) {
+  try {
+    const { userId } = await getIdentifiers(req);
+    const tabs = await prisma.spaceInvite.findMany({
+      where: { userId },
+      include: { space: true },
+    });
+    return Response.json(tabs);
+  } catch (e) {
+    return handleApiError(e);
+  }
+}
