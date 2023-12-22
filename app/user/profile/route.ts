df5f6c085b60e955207f55d6366a443af684ef5b
Manu G <manusvathgurudath@gmail.com>
2023-12-22 02:19:37 +0000

    Update user authentication logic

---
+++ app/user/profile/route.ts
@@ -0,0 +1,24 @@
+import { getApiParams } from "@/lib/getApiParams";
+import { handleApiError } from "@/lib/handleApiError";
+import { prisma } from "@/lib/prisma";
+import { NextRequest } from "next/server";
+
+export async function GET(req: NextRequest) {
+  try {
+    // get body
+    const params = await getApiParams(req, [{ name: "email", required: true }]);
+    const data = await prisma.user.findFirstOrThrow({
+      where: {
+        email: params.email,
+      },
+      select: {
+        followers: true,
+        following: true,
+        profile: true,
+      },
+    });
+    return Response.json(data);
+  } catch (e) {
+    return handleApiError(e);
+  }
+}
