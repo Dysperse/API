a6b45f35d098c074d2fd98d4324f3e31b6e42ce3
Manu G <manusvathgurudath@gmail.com>
2023-12-23 09:23:34 +0000

    commit

--- app/space/entity/route.ts
+++ app/space/entity/route.ts
@@ -1,6 +1,5 @@
 import { getApiParams } from "@/lib/getApiParams";
 import { getIdentifiers } from "@/lib/getIdentifiers";
-import { handleApiError } from "@/lib/handleApiError";
 import { prisma } from "@/lib/prisma";
 import { NextRequest } from "next/server";
 
@@ -14,15 +13,25 @@ export async function POST(req: NextRequest) {
         { name: "name", required: true },
         { name: "type", required: true },
         { name: "note", required: false },
+        { name: "due", required: false },
+        { name: "dateOnly", required: false },
+        { name: "pinned", required: false },
+        { name: "notifications", required: false },
       ],
       { type: "BODY" }
     );
+
+    console.log(params);
 
     const space = await prisma.entity.create({
       data: {
         name: params.name,
         type: params.type,
         note: params.note,
+        due: params.due ? new Date(params.due) : undefined,
+        dateOnly: Boolean(params.dateOnly ?? true),
+        pinned: Boolean(params.pinned ?? false),
+        notifications: params.notifications,
 
         createdBy: {
           connect: {
@@ -37,7 +46,5 @@ export async function POST(req: NextRequest) {
       },
     });
     return Response.json(space);
-  } catch (e) {
-    return handleApiError(e);
-  }
+  } catch (e) {}
 }
