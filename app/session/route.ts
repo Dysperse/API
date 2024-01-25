e3a2c91c3169337636fca72efaf1ebd469cf1294
Manu G <manusvathgurudath@gmail.com>
2024-01-24 21:05:36 -0800

    W

--- app/session/route.ts
+++ app/session/route.ts
@@ -1,12 +1,19 @@
 import { getIdentifiers } from "@/lib/getIdentifiers";
 import { getSessionData } from "@/lib/getSessionData";
 import { handleApiError } from "@/lib/handleApiError";
+import { prisma } from "@/lib/prisma";
 import { NextRequest } from "next/server";
 
 export async function GET(req: NextRequest) {
   try {
     // get body
-    const { sessionId } = await getIdentifiers(req);
+    const { sessionId, userId } = await getIdentifiers(req);
+    prisma.profile.update({
+      where: { userId },
+      data: {
+        lastActive: new Date(),
+      },
+    });
     const user = await getSessionData(sessionId);
 
     return Response.json(user);
