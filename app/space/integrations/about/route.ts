9eda87163176505acad78b5f5193cb900f948bd0
Manu G <manusvathgurudath@gmail.com>
2024-02-11 10:55:37 +0530

    br

---
+++ app/space/integrations/about/route.ts
@@ -0,0 +1,11 @@
+import { getApiParams } from "@/lib/getApiParams";
+import { NextRequest } from "next/server";
+import integrations from "../integrations.json";
+
+export async function GET(req: NextRequest) {
+  const params = await getApiParams(req, [{ name: "id", required: false }]);
+
+  return Response.json(
+    params.id ? integrations.find((i) => i.slug === params.id) : integrations
+  );
+}
