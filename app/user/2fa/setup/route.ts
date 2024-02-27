215ecde10a7462dbb831c0238ff2513f9fc0cb2e
Manu G <manusvathgurudath@gmail.com>
2024-02-26 23:33:39 -0800

    W

---
+++ app/user/2fa/setup/route.ts
@@ -0,0 +1,49 @@
+import { getApiParams } from "@/lib/getApiParams";
+import { getIdentifiers } from "@/lib/getIdentifiers";
+import { handleApiError } from "@/lib/handleApiError";
+import { prisma } from "@/lib/prisma";
+import { NextRequest } from "next/server";
+const twofactor = require("node-2fa");
+
+export const dynamic = "force-dynamic";
+
+export async function GET(req: NextRequest) {
+  try {
+    const { userId } = await getIdentifiers();
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+      select: {
+        email: true,
+        profile: { select: { name: true } },
+      },
+    });
+    const data = twofactor.generateSecret({
+      name: "Dysperse",
+      account: `${user?.profile?.name}`,
+    });
+    return Response.json(data);
+  } catch (e) {
+    return handleApiError(e);
+  }
+}
+
+export async function POST(req: NextRequest) {
+  try {
+    const { userId } = await getIdentifiers();
+    const params = await getApiParams(req, [
+      { name: "secret", required: true },
+      { name: "code", required: true },
+    ]);
+    if (!twofactor.verifyToken(params.secret, params.code)) {
+      throw new Error("Invalid code");
+    }
+    const data = await prisma.user.update({
+      where: { id: userId },
+      data: { twoFactorSecret: params.secret },
+    });
+
+    return Response.json(data);
+  } catch (e) {
+    return handleApiError(e);
+  }
+}
