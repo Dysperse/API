30911ee202966aca94bf0e4af892434005752229
Manu G <manusvathgurudath@gmail.com>
2024-03-20 18:29:15 +0000

    W

---
+++ app/user/account/email/route.ts
@@ -0,0 +1,26 @@
+import { getApiParams } from "@/lib/getApiParams";
+import { getIdentifiers } from "@/lib/getIdentifiers";
+import { handleApiError } from "@/lib/handleApiError";
+import { prisma } from "@/lib/prisma";
+import { NextRequest } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    // get body
+    const { userId } = await getIdentifiers();
+    const params = await getApiParams(
+      req,
+      [{ name: "lastReleaseVersionViewed", required: false }],
+      { type: "BODY" }
+    );
+    const data = await prisma.user.update({
+      where: { id: userId },
+      data: {
+        lastReleaseVersionViewed: params.lastReleaseVersionViewed || undefined,
+      },
+    });
+    return Response.json(data);
+  } catch (e) {
+    return handleApiError(e);
+  }
+}
