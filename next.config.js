9fd58d680fd63a4839038e6f0bf5c555b1f1e4be
Manu G <manusvathgurudath@gmail.com>
2024-05-05 22:32:02 -0700

    pls dont break

--- next.config.js
+++ next.config.js
@@ -21,8 +21,7 @@ const moduleExports = {
           },
           {
             key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+            value: "*",
           },
         ],
       },
