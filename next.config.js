9840c3982417ad710ebb48748047d70153d39c74
Manu G <manusvathgurudath@gmail.com>
2024-05-07 00:42:24 -0700

    W

--- next.config.js
+++ next.config.js
@@ -10,19 +10,19 @@ const moduleExports = {
   async headers() {
     return [
       {
-        source: "/:path*",
+        // all paths
+        source: "/(.*)",
         headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*", // Set your origin
-          },
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
           {
             key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
+            value: "GET,DELETE,PATCH,POST,PUT",
           },
           {
             key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
           },
         ],
       },
