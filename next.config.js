d2c4715c83b7b93b9b779fa2c010e78548cbe803
Manu G <manusvathgurudath@gmail.com>
2024-05-07 01:04:48 -0700

    W

--- next.config.js
+++ next.config.js
@@ -1,33 +1,40 @@
 const { withSentryConfig } = require("@sentry/nextjs");
 
 const moduleExports = {
+  async headers() {
+    return [
+      {
+        // Routes this applies to
+        source: "/(.*)",
+        // Headers
+        headers: [
+          // Allow for specific domains to have access or * for all
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+            // DOES NOT WORK
+            // value: process.env.ALLOWED_ORIGIN,
+          },
+          // Allows for specific methods accepted
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          // Allows for specific headers accepted (These are a few standard ones)
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     unoptimized: true,
   },
   reactStrictMode: true,
   swcMinify: true,
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        // all paths
-        source: "/(.*)",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ];
-  },
   async redirects() {
     return [
       {
