a970d1cc1561e941859c8a63feaa3216ea677703
Manu G <manusvathgurudath@gmail.com>
2024-06-17 21:55:36 -0700

    W

--- app/user/notifications/route.ts
+++ app/user/notifications/route.ts
@@ -13,6 +13,37 @@ export const OPTIONS = async () => {
   });
 };
 
+export async function GET(req: NextRequest) {
+  try {
+    const { userId } = await getIdentifiers();
+
+    const subscriptions = await prisma.notificationSubscription.findMany({
+      where: { userId },
+    });
+
+    return Response.json({ subscriptions });
+  } catch (e) {
+    return handleApiError(e);
+  }
+}
+
+export async function DELETE(req: NextRequest) {
+  try {
+    const { userId } = await getIdentifiers();
+    const params = await getApiParams(req, [{ name: "id", required: true }]);
+
+    await prisma.notificationSubscription.deleteMany({
+      where: {
+        AND: [{ id: params.id }, { userId }],
+      },
+    });
+
+    return Response.json({ success: true });
+  } catch (e) {
+    return handleApiError(e);
+  }
+}
+
 export async function POST(req: NextRequest) {
   try {
     const { userId } = await getIdentifiers();
@@ -20,6 +51,8 @@ export async function POST(req: NextRequest) {
       req,
       [
         { name: "type", required: true },
+        { name: "deviceType", required: true },
+        { name: "deviceName", required: true },
         { name: "tokens", required: true },
       ],
       { type: "BODY" }
@@ -29,6 +62,8 @@ export async function POST(req: NextRequest) {
       data: {
         user: { connect: { id: userId } },
         type: params.type,
+        deviceType: params.deviceType,
+        deviceName: params.deviceName,
         tokens: params.tokens,
       },
     });
