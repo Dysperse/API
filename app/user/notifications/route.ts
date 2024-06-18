818831234cae313a57613e7546ec319a49e26c8c
Manu G <manusvathgurudath@gmail.com>
2024-06-17 21:46:38 -0700

    W

---
+++ app/user/notifications/route.ts
@@ -0,0 +1,36 @@
+import { getApiParams } from "@/lib/getApiParams";
+import { getIdentifiers } from "@/lib/getIdentifiers";
+import { handleApiError } from "@/lib/handleApiError";
+import { prisma } from "@/lib/prisma";
+import { NextRequest } from "next/server";
+
+export const dynamic = "force-dynamic";
+
+export const OPTIONS = async () => {
+  return new Response("", {
+    status: 200,
+    headers: { "Access-Control-Allow-Headers": "*" },
+  });
+};
+
+export async function POST(req: NextRequest) {
+  try {
+    const { userId } = await getIdentifiers();
+    const params = await getApiParams(req, [
+      { name: "type", required: true },
+      { name: "tokens", required: true },
+    ]);
+
+    const subscription = await prisma.notificationSubscription.create({
+      data: {
+        user: { connect: { id: userId } },
+        type: params.type,
+        tokens: params.tokens,
+      },
+    });
+
+    return Response.json({ subscription });
+  } catch (e) {
+    return handleApiError(e);
+  }
+}
