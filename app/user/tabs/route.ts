671af4c8d46ad1f2ffabd7e170de198e5005ba0b
Manu G <manusvathgurudath@gmail.com>
2024-06-22 11:15:15 -0700

    idk

--- app/user/tabs/route.ts
+++ app/user/tabs/route.ts
@@ -20,7 +20,7 @@ export async function GET(req: NextRequest) {
       where: {
         userId,
       },
-      orderBy: { order: "asc" },
+      // orderBy: { order: "asc" },
       include: {
         collection: {
           select: { name: true, emoji: true },
