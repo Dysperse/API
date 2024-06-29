ab37e4548c43d0178469d142ea6ac6c7ca9cd87c
Manu G <manusvathgurudath@gmail.com>
2024-06-29 10:30:18 -0700

    W

--- app/upload/route.ts
+++ app/upload/route.ts
@@ -3,6 +3,15 @@ import { handleApiError } from "@/lib/handleApiError";
 import { Formidable } from "formidable";
 import fs from "fs";
 
+export const dynamic = "force-dynamic";
+
+export const OPTIONS = async () => {
+  return new Response("", {
+    status: 200,
+    headers: { "Access-Control-Allow-Headers": "*" },
+  });
+};
+
 export async function POST(req) {
   try {
     const formData: any = await new Promise((resolve, reject) => {
