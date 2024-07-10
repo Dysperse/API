fafaceaf52818a6e876daafc3d0a7718d0256720
Manu G <manusvathgurudath@gmail.com>
2024-07-10 11:27:16 -0700

    W

--- app/dysverse/route.ts
+++ app/dysverse/route.ts
@@ -1,4 +1,5 @@
 import { getApiParams } from "@/lib/getApiParams";
+import { getIdentifiers } from "@/lib/getIdentifiers";
 import { handleApiError } from "@/lib/handleApiError";
 import { prisma } from "@/lib/prisma";
 import { NextRequest } from "next/server";
@@ -10,6 +11,51 @@ export const OPTIONS = async () => {
     headers: { "Access-Control-Allow-Headers": "*" },
   });
 };
+
+// copy collection
+export async function POST(req: NextRequest) {
+  try {
+    const { userId, spaceId } = await getIdentifiers();
+    const params = await getApiParams(req, [{ name: "id", required: true }]);
+
+    const copy = await prisma.collection.findFirstOrThrow({
+      where: { id: params.id },
+      include: { labels: true },
+    });
+
+    const data = await prisma.collection.create({
+      data: {
+        name: copy.name,
+        description: copy.description,
+        defaultView: copy.defaultView,
+        category: copy.category,
+        emoji: copy.emoji,
+        kanbanOrder: copy.kanbanOrder || undefined,
+        gridOrder: copy.gridOrder || undefined,
+        createdBy: { connect: { id: userId } },
+        labels: {
+          create: copy.labels.map((label) => ({
+            name: label.name,
+            emoji: label.emoji,
+            color: label.color,
+            createdBy: { connect: { id: userId } },
+            space: { connect: { id: spaceId } },
+          })),
+        },
+        space: { connect: { id: spaceId } },
+        shareItems: copy.shareItems,
+        keepAuthorAnonymous: copy.keepAuthorAnonymous,
+        showCompleted: copy.showCompleted,
+        originalCollectionTemplate: { connect: { id: copy.id } },
+        keepProfileAnonymous: copy.keepProfileAnonymous,
+      },
+    });
+
+    return Response.json(data);
+  } catch (e) {
+    return handleApiError(e);
+  }
+}
 
 export async function GET(req: NextRequest) {
   try {
