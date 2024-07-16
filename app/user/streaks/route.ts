ce25887291a116d2164690d635c50d4eb6c0e3cf
Manu G <manusvathgurudath@gmail.com>
2024-07-16 09:48:20 -0700

    W

--- app/user/streaks/route.ts
+++ app/user/streaks/route.ts
@@ -1,8 +1,10 @@
+import { getApiParams } from "@/lib/getApiParams";
 import { getIdentifiers } from "@/lib/getIdentifiers";
 import { handleApiError } from "@/lib/handleApiError";
 import { prisma } from "@/lib/prisma";
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
+import { NextRequest } from "next/server";
 dayjs.extend(utc);
 
 export const dynamic = "force-dynamic";
@@ -15,9 +17,13 @@ export const OPTIONS = async () => {
 };
 
 // Today's tasks
-export async function GET() {
+export async function GET(req: NextRequest) {
   try {
     const { userId } = await getIdentifiers();
+    const params = await getApiParams(req, [
+      { name: "dayStart", required: true },
+      { name: "weekStart", required: true },
+    ]);
     const data = await prisma.user.findFirstOrThrow({
       where: { id: userId },
     });
@@ -25,14 +31,14 @@ export async function GET() {
     const tasks = await prisma.completionInstance.findMany({
       where: {
         completedAt: {
-          gte: dayjs().startOf("week").utc().toDate(),
+          gte: dayjs(params.weekStart).utc().toDate(),
         },
       },
     });
     return Response.json({
       user: data,
       dayTasks: tasks.filter((task) => {
-        return dayjs(task.completedAt).isSame(dayjs(), "day");
+        return dayjs(task.completedAt).isSame(dayjs(params.dayStart), "day");
       }).length,
       weekTasks: tasks.length,
     });
