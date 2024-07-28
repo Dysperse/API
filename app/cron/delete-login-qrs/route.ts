f973cbe41ab945d9ac324dcaf6795697786d9e8c
Manu G <manusvathgurudath@gmail.com>
2024-07-28 14:01:10 -0700

    add

---
+++ app/cron/delete-login-qrs/route.ts
@@ -0,0 +1,27 @@
+import { prisma } from "@/lib/prisma";
+import { NextRequest } from "next/server";
+
+export const dynamic = "force-dynamic";
+
+export const OPTIONS = async () => {
+  return new Response("", {
+    status: 200,
+    headers: { "Access-Control-Allow-Headers": "*" },
+  });
+};
+
+export async function GET(req: NextRequest) {
+  if (
+    process.env.NODE_ENV !== "development" &&
+    req.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`
+  )
+    throw new Error("Access denied");
+
+  await prisma.qrToken.deleteMany({
+    where: {
+      expiresAt: {
+        lte: new Date(),
+      },
+    },
+  });
+}
