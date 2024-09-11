82c220fa3302b6d39984492b6c76f6fa423cf49e
Manu G <manusvathgurudath@gmail.com>
2024-09-10 19:59:05 -0700

    fix

--- app/collections/getLabelOrder.ts
+++ app/collections/getLabelOrder.ts
@@ -3,15 +3,12 @@ export function getLabelOrder(
   key: "kanbanOrder" | "gridOrder" = "kanbanOrder"
 ) {
   const labelIds = new Set(collection.labels.map((label) => label.id));
+  const t = (collection as any)?.[key] || [];
 
   return [
     ...new Set([
-      ...((collection as any)?.[key] || []).filter((id: string) =>
-        labelIds.has(id)
-      ),
-      ...Array.from(labelIds).filter(
-        (id) => !(collection as any)[key].includes(id)
-      ),
+      ...t.filter((id: string) => labelIds.has(id)),
+      ...Array.from(labelIds).filter((id) => !t.includes(id)),
     ]),
   ];
 }
