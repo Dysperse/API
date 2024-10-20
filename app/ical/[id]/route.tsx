19f72d2c70b34588cdf6ba834cc7a543d795b9a7
Manu G <manusvathgurudath@gmail.com>
2024-10-20 01:23:29 -0700

    W

--- app/ical/[id]/route.tsx
+++ app/ical/[id]/route.tsx
@@ -3,6 +3,7 @@ import { prisma } from "@/lib/prisma";
 import { Prisma } from "@prisma/client";
 import ical from "ical-generator";
 import { NextRequest } from "next/server";
+import { RRule } from "rrule";
 
 export const dynamic = "force-dynamic";
 export const OPTIONS = async () => {
@@ -16,12 +17,20 @@ export async function GET(req: NextRequest, { params }) {
   try {
     if (!params.id) throw new Error("Missing id");
     const user = await prisma?.spaceInvite.findFirstOrThrow({
-      where: { spaceId: params.id },
-      select: { user: { select: { email: true } } },
+      where: { id: params.id },
+      select: { user: { select: { email: true } }, spaceId: true },
     });
     const entities = await prisma?.entity.findMany({
       where: {
-        AND: [{ spaceId: params.id }, { start: { not: Prisma.AnyNull } }],
+        AND: [
+          { spaceId: user?.spaceId },
+          {
+            OR: [
+              { start: { not: null } },
+              { recurrenceRule: { not: Prisma.AnyNull } },
+            ],
+          },
+        ],
       },
     });
 
@@ -37,16 +46,31 @@ export async function GET(req: NextRequest, { params }) {
         allDay: entity.dateOnly,
         attachments:
           entity.attachments &&
-          entity.attachments.map((attachment) => attachment),
+          (entity.attachments as any).map((attachment) => attachment),
         priority: entity.pinned ? 1 : 0,
         url: entity.published
           ? `https://dys.us.to/${entity.shortId}`
           : undefined,
         summary: entity.name,
+        // location:
+        //   entity.attachments &&
+        //   (entity.attachments as any).find((e) => e.type === "LOCATION")
+        //     ? (entity.attachments as any).find((e) => e.type === "LOCATION")
+        //         .data
+        //     : undefined,
+        repeating: entity.recurrenceRule
+          ? new RRule(entity.recurrenceRule as any).toString()
+          : undefined,
         description: entity.note,
       });
     }
-    return new Response(params.id);
+    return new Response(calendar.toString(), {
+      status: 200,
+      // headers: {
+      //   "Content-Type": "text/calendar",
+      //   "Content-Disposition": `attachment; filename="${user?.user?.email}.ics"`,
+      // },
+    });
   } catch (e) {
     return handleApiError(e);
   }
