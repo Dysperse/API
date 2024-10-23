9e7845c5a543b1524b4891906f769a0d1d079e67
Manu G <manusvathgurudath@gmail.com>
2024-10-22 21:53:46 -0700

    bW

--- app/user/delete-account/route.ts
+++ app/user/delete-account/route.ts
@@ -1,6 +1,8 @@
+import { getApiParams } from "@/lib/getApiParams";
 import { getIdentifiers } from "@/lib/getIdentifiers";
 import { handleApiError } from "@/lib/handleApiError";
 import { prisma } from "@/lib/prisma";
+import { verifyTurnstileToken } from "@/lib/verifyTurnstileToken";
 import { NextRequest } from "next/server";
 
 export const dynamic = "force-dynamic";
@@ -25,6 +27,30 @@ export const OPTIONS = async () => {
 export async function DELETE(req: NextRequest) {
   try {
     const { spaceId, userId } = await getIdentifiers();
+    const params = await getApiParams(
+      req,
+      [
+        { name: "reason", required: true },
+        { name: "feedback", required: true },
+        { name: "alternative", required: true },
+        { name: "stay", required: true },
+        { name: "captcha", required: true },
+      ],
+      { type: "BODY" }
+    );
+
+    await verifyTurnstileToken(params.captcha);
+
+    await fetch(
+      "https://script.google.com/macros/s/AKfycbwuTc5P6NZ8RYMGxarngng4Itr6qGmgXmihukJOiMWfKUg81mfPYlWjlhTbhLIB8M4/exec",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(params),
+      }
+    );
 
     // Find the user and include related data
     await prisma.space.delete({ where: { id: spaceId } });
