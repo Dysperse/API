3bfdf0b13a3d95cf4114898c2415fe0a16820f96
Manu G <manusvathgurudath@gmail.com>
2024-11-25 23:17:36 -0800

    asfasf

---
+++ app/space/collections/collection/reset-pin/route.ts
@@ -0,0 +1,49 @@
+import { getApiParams } from "@/lib/getApiParams";
+import { getIdentifiers } from "@/lib/getIdentifiers";
+import { handleApiError } from "@/lib/handleApiError";
+import { prisma } from "@/lib/prisma";
+import argon2 from "argon2";
+import { NextRequest } from "next/server";
+import { v4 as uuidv4 } from "uuid";
+
+export const dynamic = "force-dynamic";
+export const OPTIONS = async () => {
+  return new Response("", {
+    status: 200,
+    headers: { "Access-Control-Allow-Headers": "*" },
+  });
+};
+
+export async function POST(req: NextRequest) {
+  try {
+    const { userId } = await getIdentifiers();
+    const params = await getApiParams(
+      req,
+      [
+        { name: "id", required: true },
+        { name: "password", required: true },
+      ],
+      { type: "BODY" }
+    );
+
+    const user = await prisma.user.findFirstOrThrow({
+      where: { id: userId },
+      select: { password: true },
+    });
+
+    if (!argon2.verify(user.password, params.password)) {
+      throw new Error("Invalid password");
+    }
+
+    const data = await prisma.collection.updateMany({
+      data: {
+        id: params.refreshId ? uuidv4() : undefined,
+        pinCode: null,
+      },
+    });
+
+    return Response.json(data);
+  } catch (e) {
+    return handleApiError(e);
+  }
+}
