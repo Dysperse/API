308e00cea9918273b8ee7ab83d228a2790b21965
Manu G <manusvathgurudath@gmail.com>
2024-12-15 22:48:31 -0800

    asdfadsf

---
+++ app/ai/image-caption/route.ts
@@ -0,0 +1,23 @@
+import { getIdentifiers } from "@/lib/getIdentifiers";
+import { handleApiError } from "@/lib/handleApiError";
+import { NextRequest } from "next/server";
+
+export const dynamic = "force-dynamic";
+
+export const OPTIONS = async () => {
+  return new Response("", {
+    status: 200,
+    headers: { "Access-Control-Allow-Headers": "*" },
+  });
+};
+
+export async function POST(req: NextRequest) {
+  try {
+    // idk why i'm even protecting this endpoint
+    await getIdentifiers();
+    // take it - i don't care, it's a free api anyways
+    return Response.json(process.env.IMAGE_CAPTIONING_TOKEN);
+  } catch (e) {
+    return handleApiError({ error: "Please log in" });
+  }
+}
