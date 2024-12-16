cfe5d8ef456cb2c8788d1eb10e6a39c27d20ff65
Manu G <manusvathgurudath@gmail.com>
2024-12-15 22:48:57 -0800

    asdf

--- app/ai/image-caption/route.ts
+++ app/ai/image-caption/route.ts
@@ -16,6 +16,7 @@ export async function POST(req: NextRequest) {
     // idk why i'm even protecting this endpoint
     await getIdentifiers();
     // take it - i don't care, it's a free api anyways
+    // huggingface scans for leaked tokens and revokes them.
     return Response.json(process.env.IMAGE_CAPTIONING_TOKEN);
   } catch (e) {
     return handleApiError({ error: "Please log in" });
