88c4a0fe64af812c786739a44a760db551d14570
Manu G <manusvathgurudath@gmail.com>
2025-01-25 14:05:49 -0800

    add

---
+++ app/user/notifications/badge/route.ts
@@ -0,0 +1,70 @@
+import { getIdentifiers } from "@/lib/getIdentifiers";
+import { handleApiError } from "@/lib/handleApiError";
+import { prisma } from "@/lib/prisma";
+import { NextRequest } from "next/server";
+
+export const dynamic = "force-dynamic";
+
+export const OPTIONS = async () => {
+  return new Response("", {
+    status: 200,
+    headers: { "Access-Control-Allow-Headers": "*" },
+  });
+};
+
+export async function GET(req: NextRequest) {
+  try {
+    const { userId } = await getIdentifiers();
+    const badges = await prisma.notificationSettings.findFirstOrThrow({
+      where: { userId },
+      select: { badgedCollections: { select: { id: true } } },
+    });
+
+    const entities = await prisma.collection.findMany({
+      where: { id: { in: badges.badgedCollections.map((t) => t.id) } },
+      select: {
+        _count: {
+          select: {
+            entities: {
+              where: {
+                trash: false,
+                completionInstances: {
+                  none: { taskId: { contains: "-" } },
+                },
+              },
+            },
+          },
+        },
+        labels: {
+          select: {
+            name: true,
+            _count: {
+              select: {
+                entities: {
+                  where: {
+                    trash: false,
+                    completionInstances: {
+                      none: { taskId: { contains: "-" } },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    });
+
+    return Response.json({
+      count: entities.reduce((acc, entity) => {
+        const labelCount = entity.labels.reduce(
+          (labelAcc, label) => labelAcc + label._count.entities,
+          0
+        );
+        return acc + entity._count.entities + labelCount;
+      }, 0),
+    });
+  } catch (e) {
+    return handleApiError(e);
+  }
+}
