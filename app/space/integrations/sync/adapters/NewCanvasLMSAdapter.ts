63e332b9e1d1242afe8928b85fb8731c27df5162
Manu G <manusvathgurudath@gmail.com>
2025-01-30 22:24:34 -0800

    W

--- app/space/integrations/sync/adapters/NewCanvasLMSAdapter.ts
+++ app/space/integrations/sync/adapters/NewCanvasLMSAdapter.ts
@@ -38,6 +38,7 @@ export class NewCanvasLMSAdapter extends Integration {
         per_page: "100",
         access_token: i.accessToken,
         "include[]": "submission",
+        bucket: "upcoming",
       })}`;
 
       while (nextUrl) {
