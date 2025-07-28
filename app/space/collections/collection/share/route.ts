23caa6be160bd52653d9af77c4c403fb58561141
Manu G <manusvathgurudath@gmail.com>
2025-07-27 21:15:57 -0700

    Removes extraneous line break for code clarity
    Cleans up formatting to improve readability and maintain consistency.
    No functional changes made.

--- app/space/collections/collection/share/route.ts
+++ app/space/collections/collection/share/route.ts
@@ -6,6 +6,7 @@ import { prisma } from "@/lib/prisma";
 import { NextRequest } from "next/server";
 
 export const dynamic = "force-dynamic";
+
 export const OPTIONS = async () => {
   return new Response("", {
     status: 200,
